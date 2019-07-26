1ae2e389128cc2344e2d53f88243b2caf861b05d
Yevgeniy <zleonenkov@gmail.com>
2019-07-26 16:34:31 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'react-tabs/style/react-tabs.css';
+import {
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+} from 'react-tabs';
 
 class App extends React.Component {
   state = {
@@ -10,6 +17,27 @@ class App extends React.Component {
     ],
   };
 
+  title = tabs => tabs.map(tabItem => (
+    <Tab>{tabItem.title}</Tab>
+  ));
+
+  content = tabs => tabs.map(tabItem => (
+    <TabPanel>{tabItem.content}</TabPanel>
+  ));
+
+  setIndex = tabs => tabs.map((tabItem, i) => (
+    {
+      ...tabItem,
+      index: i,
+    }
+  ));
+
+  onTabSelected = () => {
+    this.setState(prevState => ({
+      tabs: this.setIndex(prevState.tabs),
+    }));
+  };
+
   render() {
     const { tabs } = this.state;
 
@@ -17,6 +45,13 @@ class App extends React.Component {
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>{tabs.length} tabs</h1>
+        <Tabs>
+          <TabList onClick={this.onTabSelected}>
+            {this.title(tabs)}
+          </TabList>
+
+          {this.content(tabs)}
+        </Tabs>
       </div>
     );
   }
