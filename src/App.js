06ea5a09899c478f6235db3e0178cc80904d8ebe
Serhii <davydko.serhiy@gmail.com>
2019-07-26 15:06:16 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -8,15 +8,60 @@ class App extends React.Component {
       { title: 'Tab 2', content: 'Some text 2' },
       { title: 'Tab 3', content: 'Some text 3' },
     ],
+    activeId: 1,
   };
 
+  componentDidMount() {
+    let tabId = 0;
+
+    this.setState(prevState => ({
+      tabs: prevState.tabs.map((tab) => {
+        tabId++;
+
+        return (
+          { ...tab, id: tabId }
+        );
+      }),
+    }));
+  }
+
+  clickTab(id) {
+    this.forceUpdate();
+    this.setState({
+      activeId: id,
+    });
+  }
+
   render() {
-    const { tabs } = this.state;
+    const { tabs, activeId } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+
+        <nav>
+          <ul>
+            {tabs.map(tab => (
+              <li className={(tab.id === activeId) && 'active-tab'}>
+                <a
+                  href={`#${tab.title}`}
+                  onClick={() => this.clickTab(tab.id)}
+                  className={(tab.id === activeId) && 'active-link'}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <main>
+          {tabs.map(article => (
+            <article className={(article.id === activeId) && 'active-article'}>
+              <a name={article.title}></a>
+              {article.content}
+            </article>
+          ))}
+        </main>
       </div>
     );
   }
