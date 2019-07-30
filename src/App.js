2cce7d781ac3b9a4a477cb3cebec3e1b639b5565
Пользователь <Sviatoslv>
2019-07-30 17:30:31 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import Tabs from './components/Tabs';
+import PropTypes from 'prop-types';
+
+const TabsBlockContext = React.createContext();
 
 const tabsOrigin = [
   { title: 'Tab 1', content: 'Some text 1' },
@@ -13,6 +15,7 @@ const tabsForWord = tabsOrigin.map((tab, index) => ({ ...tab, id: index }));
 class App extends React.Component {
   state = {
     tabs: tabsForWord,
+    index: 0,
   };
 
   changeTabOrder = (event) => {
@@ -31,21 +34,84 @@ class App extends React.Component {
     }
   }
 
+  onTabSelected = (event) => {
+    this.setState({ index: +event.target.name });
+  }
+
   render() {
-    const { tabs } = this.state;
+    const { tabs, index } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>{tabs.length} tabs</h1>
 
-        <Tabs
-          tabs={tabs}
-          changeTabOrder={this.changeTabOrder}
-        />
+        <TabsBlockContext.Provider
+          value={tabs.find(tab => tab.id === index).content}
+        >
+          <TabsBlock>
+            {tabs.map(tab => (
+              <TabsBlock.Tab
+                title={tab.title}
+                id={tab.id}
+                onTabSelected={this.onTabSelected}
+                index={index}
+                key={tab.id}
+              >
+              </TabsBlock.Tab>
+            ))}
+          </TabsBlock>
+        </TabsBlockContext.Provider>
       </div>
     );
   }
 }
+
+const TabsBlock = ({ children }) => (
+  <TabsBlockContext.Consumer>
+    {value => (
+      <div>
+        <ul className="Tabs__list">
+          {children}
+        </ul>
+
+        <div
+          className="Tabs-content"
+        >
+          {value}
+        </div>
+      </div>
+    )}
+  </TabsBlockContext.Consumer>
+);
+
+TabsBlock.Tab = ({
+  onTabSelected, title, id, index,
+}) => (
+  <li className="Tabs__items">
+    <button
+      onClick={onTabSelected}
+      name={id}
+      type="button"
+      className={
+        index === id ? 'button button--active' : 'button'
+      }
+      key={id}
+    >
+      {title}
+    </button>
+  </li>
+);
+
+TabsBlock.propTypes = ({
+  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+});
+
+TabsBlock.Tab.propTypes = ({
+  id: PropTypes.number.isRequired,
+  onTabSelected: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+});
 
 export default App;
