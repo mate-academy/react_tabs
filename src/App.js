36aeb10ea69533ca0e7ba8de38be46c8956ddd2e
Пользователь <Sviatoslv>
2019-07-30 18:06:11 +0300

    Fixed Solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
-import TabsCaruselControl from './components/TabsCaruselControl';
 
-const TabsBlockContext = React.createContext();
+import TabsCaruselControl from './components/TabsCaruselControl';
+import TabsBlock from './components/TabsBlock';
+
+import TabsBlockContext from './TabsBlockContext';
 
 const tabsOrigin = [
   { title: 'Tab 1', content: 'Some text 1' },
@@ -69,52 +70,5 @@ class App extends React.Component {
     );
   }
 }
-
-const TabsBlock = ({ children }) => (
-  <TabsBlockContext.Consumer>
-    {value => (
-      <div>
-        <ul className="Tabs__list">
-          {children}
-        </ul>
-
-        <div
-          className="Tabs-content"
-        >
-          {value}
-        </div>
-      </div>
-    )}
-  </TabsBlockContext.Consumer>
-);
-
-TabsBlock.Tab = ({
-  onTabSelected, title, id, index,
-}) => (
-  <li className="Tabs__items">
-    <button
-      onClick={onTabSelected}
-      name={id}
-      type="button"
-      className={
-        index === id ? 'button button--active' : 'button'
-      }
-      key={id}
-    >
-      {title}
-    </button>
-  </li>
-);
-
-TabsBlock.propTypes = ({
-  children: PropTypes.arrayOf(PropTypes.object).isRequired,
-});
-
-TabsBlock.Tab.propTypes = ({
-  id: PropTypes.number.isRequired,
-  onTabSelected: PropTypes.func.isRequired,
-  title: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
-});
 
 export default App;
