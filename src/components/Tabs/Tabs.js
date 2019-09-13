8b845ad617a54a4b411450601e91375c572baf58
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-13 22:31:16 +0300

    Solution

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Tabs.css';
 import Tab from '../Tab/Tab';
 
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
-    
     this.state = {
       activeTab: 0,
     };
@@ -20,18 +22,24 @@ class Tabs extends React.Component {
     const { tabs } = this.props;
     const { handleClick } = this;
     const { activeTab } = this.state;
+    let isTabActive = false;
 
     return (
-      <div className="tabs">
-        <ul className="tabs__name">
-          {tabs.map((tab, index) => (
-            <Tab
-              tab={tab}
-              index={index}
-              handleClick={handleClick}
-              key={`${tab.title}-key`}
-            />
-          ))}
+      <div>
+        <ul className="tabs">
+          {tabs.map((tab, index) => {
+            isTabActive = activeTab === index;
+
+            return (
+              <Tab
+                isTabActive={isTabActive}
+                tab={tab}
+                index={index}
+                handleClick={handleClick}
+                key={`${tab.title}-key`}
+              />
+            );
+          })}
         </ul>
         <div className="tabs__content">
           {tabs[activeTab].content}
@@ -40,5 +48,22 @@ class Tabs extends React.Component {
     );
   }
 }
+
+Tabs.defaultProps = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      content: 'No content!',
+    })
+  ),
+};
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      content: PropTypes.string,
+    }).isRequired
+  ),
+};
 
 export default Tabs;
