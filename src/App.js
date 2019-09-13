047063734a1a49d2074927ada1b57f7c7cd4e725
Rafler <amuronyk@gmail.com>
2019-09-13 10:24:02 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Tabs from './components/Tabs/Tabs';
 
 class App extends React.Component {
   state = {
@@ -8,15 +9,21 @@ class App extends React.Component {
       { title: 'Tab 2', content: 'Some text 2' },
       { title: 'Tab 3', content: 'Some text 3' },
     ],
+    indexEl: 0,
+  };
+
+  clickTab = (index) => {
+    this.setState({
+      indexEl: index,
+    });
   };
 
   render() {
-    const { tabs } = this.state;
+    const { tabs, indexEl } = this.state;
 
     return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+      <div>
+        <Tabs tabs={tabs} indexItem={indexEl} clickTab={this.clickTab} />
       </div>
     );
   }
