df99dcc539c587efa806f39467b4e5944768fbf8
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-09-15 22:35:28 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import './App.css';
+import Tabs from './components/Tabs/Tabs';
 
 class App extends React.Component {
   state = {
     tabs: [
-      { title: 'Tab 1', content: 'Some text 1' },
-      { title: 'Tab 2', content: 'Some text 2' },
-      { title: 'Tab 3', content: 'Some text 3' },
+      { title: 'Tab 1', content: 'Some very interesting text 1' },
+      { title: 'Tab 2', content: 'Some very interesting text 2' },
+      { title: 'Tab 3', content: 'Some very interesting text 3' },
     ],
+    activeTabIndex: 0,
+  };
+
+  onTabSelected = (index) => {
+    this.setState({
+      activeTabIndex: index,
+    });
   };
 
   render() {
-    const { tabs } = this.state;
+    const { tabs, activeTabIndex } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+        <Tabs
+          tabs={tabs}
+          onTabSelected={this.onTabSelected}
+          activeTab={activeTabIndex}
+        />
       </div>
     );
   }
