9a5f3f80ccd81b7a1a82ba9b51fde3de732ab5fb
VadimK <djvadya16@gmail.com>
2019-10-29 14:33:48 +0200

    done

--- src/comsponents/tabs/Tabs.js
+++ src/comsponents/tabs/Tabs.js
@@ -1,63 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tab from '../tab/Tab';
 
-import {
-  BrowserRouter as Router, Switch, Route, Link,
-} from 'react-router-dom';
 
-import Tab1 from './Tab1';
-import Tab2 from './Tab2';
-import Tab3 from './Tab3';
+
 
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      activeClasses: [false, false, false],
+      activeTab: 0,
     };
   }
 
-  addActiveClass(index) {
-    const activeClasses = [false, false, false];
-
-    activeClasses[index] = true;
-    this.setState({ activeClasses });
-  }
+  onTabSelected = (index) =>
+    this.setState(() => ({activeTab: index}));
 
   render() {
     const { tabs } = this.props;
-    const checkActiveLink1 = this.state.activeClasses[0]
-      ? 'active item' : 'item';
-    const checkActiveLink2 = this.state.activeClasses[1]
-      ? 'active item' : 'item';
-    const checkActiveLink3 = this.state.activeClasses[2]
-      ? 'active item' : 'item';
 
     return (
-      <Router>
-        <div>
-          <div className="ui attached tabular menu">
-            <Link to="/1">
-              <a className={checkActiveLink1} onClick={() => this.addActiveClass(0)}>
-                {tabs[0].title}</a>
-            </Link>
-            <Link to="/2">
-              <a className={checkActiveLink2} onClick={() => this.addActiveClass(1)}>
-                {tabs[1].title}</a>
-            </Link>
-            <Link to="/3">
-            <a className={checkActiveLink3} onClick={() => this.addActiveClass(2)}>
-                {tabs[2].title}</a>
-            </Link>
-          </div>
-          <Switch>
-            <Route path="/1" render={(props) => <Tab1 content={tabs[0].content} />} />
-            <Route path="/2" render={(props) => <Tab2 content={tabs[1].content} />} />
-            <Route path="/3" render={(props) => <Tab3 content={tabs[2].content} />} />
-          </Switch>
+      <>
+        <div className="ui attached tabular menu">
+          {
+            tabs.map((tab, index) => (
+              <Tab
+                key={tab.title}
+                index={index}
+                tab={tab}
+                tabActivated={() => this.onTabSelected(index)}
+                activeTab={this.state.activeTab}
+              />
+            ))
+          }
         </div>
-      </Router>
+        <div>
+          <div class="ui bottom attached segment active tab">{tabs[this.state.activeTab].content}</div>
+        </div>
+      </>
     )
   }
 }
