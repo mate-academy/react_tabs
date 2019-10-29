4af7edad8360dfe2cdf8eed712ba02e064e1bf46
Dmitriymush <powder322@gmail.com>
2019-10-29 18:33:59 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Tabs from "./components/Tabs";
 
 class App extends React.Component {
   state = {
@@ -8,15 +9,27 @@ class App extends React.Component {
       { title: 'Tab 2', content: 'Some text 2' },
       { title: 'Tab 3', content: 'Some text 3' },
     ],
+    currentTabIndex: 0,
   };
 
   render() {
     const { tabs } = this.state;
 
     return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+      <div className="container">
+        <Tabs
+          tabs={tabs}
+          onTabSelected={(tabData) => {
+            this.setState(state => ({
+              ...state,
+              currentTabIndex: tabData,
+            }));
+          }
+          }
+        />
+        <div className="content_container">
+          {this.state.tabs[this.state.currentTabIndex].title}
+        </div>
       </div>
     );
   }
