21d18728762e7da93697e2ded227d60bef602111
YuraBobeshko <gorobech55@gmail.com>
2019-10-31 22:15:56 +0200

    sol

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 class App extends React.Component {
   state = {
     tabs: [
-      { title: 'Tab 1', content: 'Some text 1', key: 0 },
-      { title: 'Tab 2', content: 'Some text 2', key: 1 },
-      { title: 'Tab 3', content: 'Some text 3', key: 2 },
+      { title: 'Tab 1', content: 'Some text 1', key: 0, },
+      { title: 'Tab 2', content: 'Some text 2', key: 1, },
+      { title: 'Tab 3', content: 'Some text 3', key: 2, },
     ],
     activ: 'Some text 1',
     buttonIndex: 0,
-    };
+  };
 
   buttonCliked(isActiv, buttonIndex){
     this.setState ({
       activ: isActiv,
-      buttonIndex: buttonIndex
+      buttonIndex: buttonIndex,
     });
   }
 
@@ -23,13 +23,12 @@ class App extends React.Component {
     const button = [];
     const { tabs } = this.state;
     for (const tab of tabs) {
-      button.push(<button disabled={tab.key === this.state.buttonIndex}
-                          className={"button"}
+      button.push(<button className={tab.key !== this.state.buttonIndex ? "button" : "isActiv " + "button"}
                           onClick={ () => this.buttonCliked(tab.content, tab.key)}
                           key={tab.key}>{tab.title}
                   </button>)
     }
-
+    // {tab.activ ? "" : "isActiv"
     return (
       <div className="App">
         <div className={"content"}>
