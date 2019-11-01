010b957886f2f2fdccd40452e111416ebd98442f
Anna Kazantseva <annetastara@gmail.com>
2019-11-01 12:00:26 +0200

    Fixed

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -9,13 +9,14 @@ class Tabs extends Component {
   render() {
     return (
       <>
-        <button onClick={() => this.props.onTabSelected(0)}
-          class="ui positive button">Tab 1</button>
-        <button onClick={() => this.props.onTabSelected(1)}
-          class="ui positive button">Tab 2</button>
-        <button onClick={() => this.props.onTabSelected(2)}
-          class="ui positive button">Tab 3</button>
-        </>
+        {this.props.tabs.map(el => (
+          <button
+            onClick={() => this.props.onTabSelected(el.content)}
+            class="ui positive button">
+            {el.title}
+          </button>
+        ))}
+      </>
     );
   }
 }
