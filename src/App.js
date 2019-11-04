f91a4d2842429a10790fffb07110eca046fe0c74
Veronika D <donets.veronika@yahoo.com>
2019-11-04 11:12:26 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 import './App.css';
 import Tabs from './components/Tabs';
 
+const tabs = [
+  { title: 'Home', content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.' },
+  { title: 'Profile', content: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.' },
+  { title: 'Contacts', content: 'Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.' },
+];
+
+function addId(tabsAll) {
+  let counter = 0;
+
+  return tabsAll.map((tab) => {
+    const tabWithId = { ...tab, id: counter };
+    counter++;
+
+    return tabWithId;
+  });
+}
+
 function App() {
-  return <Tabs />;
+  return <Tabs tabs={addId(tabs)} />;
 }
 
 export default App;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-
-const tabs = [
-  { title: 'Ноme', content: 'Some text 1' },
-  { title: 'Profile', content: 'Some text 2' },
-  { title: 'Contacts', content: 'Some text 3' },
-];
+import SingleTab from './SingleTab';
+import SingleContent from './SingleContent';
 
 class Tabs extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       activeTab: 0,
     };
@@ -22,39 +17,31 @@ class Tabs extends Component {
     }));
   };
 
-  updateTabClassName = (tab) => {
-    if (this.state.activeTab === tab) {
-      return 'item active';
-    }
-
-    return 'item';
-  }
-
-  updateTextClassName = (tab) => {
-    if (this.state.activeTab === tab) {
-      return 'ui bottom attached active tab segment';
-    }
-
-    return 'ui bottom attached tab segment';
-  }
-
   render() {
+    const allTabs = this.props.tabs;
+
     return (
       <div className="wrapper">
         <div className="ui top attached tabular menu">
-          <div onClick={() => this.onTabSelected(0)} className={this.updateTabClassName(0)}>{tabs[0].title}</div>
-          <div onClick={() => this.onTabSelected(1)} className={this.updateTabClassName(1)}>{tabs[1].title}</div>
-          <div onClick={() => this.onTabSelected(2)} className={this.updateTabClassName(2)}>{tabs[2].title}</div>
+          {allTabs.map((tab, index) => (
+            <SingleTab
+              onTabSelected={() => this.onTabSelected(tab.id)}
+              active={this.state.activeTab === index}
+              title={tab.title}
+              key={tab.id}
+            />
+          ))
+          }
         </div>
-        <div onClick={() => this.onTabSelected(0)} className={this.updateTextClassName(0)}>
-          <h1>{tabs[0].content}</h1>
-        </div>
-        <div onClick={() => this.onTabSelected(1)} className={this.updateTextClassName(1)}>
-          <h1>{tabs[1].content}</h1>
-        </div>
-        <div onClick={() => this.onTabSelected(2)} className={this.updateTextClassName(2)}>
-          <h1>{tabs[2].content}</h1>
-        </div>
+        {allTabs.map((tab, index) => (
+          <SingleContent
+            onTabSelected={() => this.onTabSelected(tab.id)}
+            content={tab.content}
+            active={this.state.activeTab === index}
+            key={tab.id}
+          />
+        ))
+        }
       </div>
     );
   }
