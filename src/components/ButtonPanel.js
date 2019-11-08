46a96aed3f166c7d917ec456c9d516415b24648d
lubremets <lubremets@gmail.com>
2019-11-08 10:30:48 +0200

    Solution

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+function ButtonPanel({ tabs, selectedTab, activeTab }) {
+  return (
+    <>
+      <div className="tabs">
+        {tabs.map((tab, index) => (
+          <Button
+            active={activeTab === index}
+            selectedTab={selectedTab}
+            key={tab.title}
+            index={index}
+            title={tab.title}
+          />
+        ))}
+      </div>
+      <div className="line" />
+      <br />
+      <div>
+        {tabs[activeTab].content}
+      </div>
+    </>
+  );
+}
+
+ButtonPanel.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  selectedTab: PropTypes.func.isRequired,
+  activeTab: PropTypes.number.isRequired,
+};
+
+export default ButtonPanel;
