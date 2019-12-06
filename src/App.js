d6577dcfdcdac3c3dc0879843527ff4deccd50a0
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-06 22:38:54 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import './App.css';
 import Tabs from './components/Tabs';
 
-const AuthorName = `Charles Lutwidge Dodgson —
-                    [ Lewis Carroll ]
-                    27 January 1832 – 14 January 1898`;
-const BookTitle = `"Alice's Adventures in Wonderland"`;
-const BookQuote = `“Would you tell me,
-  please, which way I ought to go from here?”
-  That depends a good deal on where you want to get to,” said the Cat.
-  I don’t much care where—” said Alice.
-  “Then it doesn’t matter which way you go,” said the Cat.
-  “—so long as I get somewhere,” Alice added as an explanation.
-  — Oh, you’re sure to do that,” said the Cat,
-  “if you only walk long enough.” `;
+const AuthorName = `Charles Lutwidge Dodgson ~
+                    Lewis Carroll ~
+                    [27 January 1832 – 14 January 1898]`;
+const BookTitle = `|| Alice's Adventures in Wonderland ||`;
+const BookQuote = `*It takes all the running you can do, to
+  keep in the same place. If you want to get somewhere else,
+  you must run at least twice as fast as that!`;
 
 class App extends React.Component {
   state = {
