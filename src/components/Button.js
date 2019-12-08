45a90bf8fb19fcf32c75a8189508707c4b6ada3c
Vivych Oksana <vivichokcana@gmail.com>
2019-12-08 23:31:34 +0200

    'Solution'

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button({ title, selectedTab, index, active }) {
+  return (
+    <button
+      type="button"
+      className={active ? 'active' : 'inactive'}
+      onClick={() => selectedTab(index)}
+    >
+      {title}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
+  selectedTab: PropTypes.func.isRequired,
+};
+
+export default Button;
