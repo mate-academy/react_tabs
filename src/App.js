0a37d47d0da18b49e8b8b57e1e29c4fb453362e3
Vvxforce <vvxforce@gmail.com>
2019-12-09 17:22:32 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './App.css';
+import Tabs from './Tabs';
 
 class App extends React.Component {
-  state = {
-    tabs: [
-      { title: 'Tab 1', content: 'Some text 1' },
-      { title: 'Tab 2', content: 'Some text 2' },
-      { title: 'Tab 3', content: 'Some text 3' },
-    ],
-  };
+    state = {
+      tabs: [
+        { title: 'Tab 1', content: 'Some text 1' },
+        { title: 'Tab 2', content: 'Some text 2' },
+        { title: 'Tab 3', content: 'Some text 3' },
+      ],
+    };
 
-  render() {
-    const { tabs } = this.state;
+    render() {
+      const { tabs } = this.state;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
-      </div>
-    );
-  }
+      return (
+        <div className="App">
+          <Tabs tabs={tabs} />
+        </div>
+      );
+    }
 }
 
 export default App;
