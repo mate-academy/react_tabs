d808ff4c32b4e849894f04512f4b5b558d565d2a
mgrinko <glukym@gmail.com>
2020-01-17 15:08:44 +0200

    Basic markup was simplified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    tabs: [
-      { title: 'Tab 1', content: 'Some text 1' },
-      { title: 'Tab 2', content: 'Some text 2' },
-      { title: 'Tab 3', content: 'Some text 3' },
-    ],
-  };
+// eslint-disable-next-line no-unused-vars
+const tabs = [
+  {
+    title: 'Tab 1',
+    content: 'Some text 1',
+  },
+  {
+    title: 'Tab 2',
+    content: 'Some text 2',
+  },
+  {
+    title: 'Tab 3',
+    content: 'Some text 3',
+  },
+];
 
-  render() {
-    const { tabs } = this.state;
-
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <h1>React tabs</h1>
+);
 
 export default App;
