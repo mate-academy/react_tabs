de4d25640e1d8bb26b7981da48e26896bc3adc68
Olexander <kravchenko.sania@gmail.com>
2020-02-16 17:32:15 +0200

    attempt 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import { Tabs } from './components/Tabs/Tabs';
+
 // eslint-disable-next-line no-unused-vars
 const tabs = [
   {
@@ -12,13 +14,25 @@ const tabs = [
     content: 'Some text 2',
   },
   {
-    title: 'Tab 3',
+    title: 'Tab 4',
     content: 'Some text 3',
+  },
+  {
+    title: 'Tab 4',
+    content: 'Some text 4',
   },
 ];
 
+const preparedTabs = tabs.map((tab, i) => {
+  const newTab = {
+    ...tab, id: i,
+  };
+
+  return newTab;
+});
+
 const App = () => (
-  <h1>React tabs</h1>
+  <Tabs tabs={preparedTabs} />
 );
 
 export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Tabs.css';
+import PropTypes from 'prop-types';
+
+import { Tab } from '../Tab/Tab';
+
+export class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      index: 0,
+    };
+  }
+
+  handlerClick = (event) => {
+    this.setState({ index: +event.target.id });
+  }
+
+  render() {
+    const { tabs } = this.props;
+
+    return (
+      <>
+        <ul className="tabs">
+          {tabs.map((tab, i) => (
+            <Tab
+              key={tab.id}
+              id={i}
+              activeClass={i === this.state.index
+                ? 'tabs__element--active'
+                : ''}
+              toggle={this.handlerClick}
+            >
+              {tab.title}
+            </Tab>
+          ))}
+          <li className="tabs__empty-element" />
+        </ul>
+        <p className="tabs__text">{tabs[this.state.index].content}</p>
+      </>
+    );
+  }
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+};
