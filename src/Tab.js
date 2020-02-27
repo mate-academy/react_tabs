afc0dc99f5835e5efd356a8051878b5bdda49a42
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-27 14:27:48 +0200

    27_02_20_1

---
+++ src/Tab.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Tabs.css';
+
+const Tab = ({ tabs, state }) => (
+  <div className="content">
+    { tabs[state].content }
+  </div>
+);
+
+Tab.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      content: PropTypes.string,
+    }),
+  ).isRequired,
+  state: PropTypes.number.isRequired,
+};
+
+export default Tab;
