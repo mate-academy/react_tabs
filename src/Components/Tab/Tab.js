fa104d89d5489633c43dfc03f83d672786dbaeb9
AnnieMarkina <anniemurkina@gmail.com>
2020-03-12 12:07:03 +0200

    classnames fixed

--- src/Components/Tab/Tab.js
+++ src/Components/Tab/Tab.js
@@ -4,8 +4,7 @@ import classNames from 'classnames';
 
 function Tab({ title, index, selectTab, isActive }) {
   const classCondition = classNames(
-    `tabs__button 
-        ${isActive ? 'tabs__button--active' : ''}`,
+    'tabs__button', { isActive: 'tabs__button--active' },
   );
 
   return (
