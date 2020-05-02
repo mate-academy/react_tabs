21b2567909a68d7325b4c057981620a11f3c1cf9
Rustam Khananov <antmine2017@gmail.com>
2020-05-02 23:18:09 +0300

    2048

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-document.querySelector('.tabs__tab').focus();
+document.querySelector('.game2048__container').focus();
