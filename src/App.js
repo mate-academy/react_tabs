f51a9e1ca65566af762691253ce5e6b1b66542bc
VlaDinter <vladyslavdinter@gmail.com>
2020-06-26 05:07:46 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import Tabs from './components/Tabs/Tabs';
+import Tab from './components/Tab/Tab';
 import './App.css';
 
 // eslint-disable-next-line no-unused-vars
@@ -17,8 +20,35 @@ const tabs = [
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+class App extends React.Component {
+  state = {
+    tabIndex: 0,
+  }
+
+  changeTabIndex = (index) => {
+    this.setState({ tabIndex: index });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>React tabs</h1>
+        <Tabs>
+          {tabs.map((tab, index) => (
+            <Tab
+              onTabSelected={() => this.changeTabIndex(index)}
+              title={tab.title}
+              activeIndex={this.state.tabIndex === index}
+              key={tab.title}
+            >
+              {tab.content}
+            </Tab>
+          ))}
+        </Tabs>
+        <p>{tabs[this.state.tabIndex].content}</p>
+      </>
+    );
+  }
+}
 
 export default App;
