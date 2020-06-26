8120604d3ef0a23d36fd7fb37d897b5a07de8fd9
Glib Rulev <velzepooz@gmail.com>
2020-06-26 17:05:14 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import './App.css';
+import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './components/Tab/Tab';
 
 // eslint-disable-next-line no-unused-vars
-const tabs = [
+const tabsFromServer = [
   {
+    tabId: 0,
     title: 'Tab 1',
     content: 'Some text 1',
   },
   {
+    tabId: 1,
     title: 'Tab 2',
     content: 'Some text 2',
   },
   {
+    tabId: 2,
     title: 'Tab 3',
     content: 'Some text 3',
   },
 ];
 
 const App = () => (
-  <h1>React tabs</h1>
+  <>
+    <h1>React tabs</h1>
+    {/* <Tabs tabs={tabsFromServer} /> */}
+    <Tabs>
+      <Tab title="Tab 2">
+        It could be a text
+      </Tab>
+
+      <Tab title="Tab 3">
+        <div>1</div>
+        <div>2</div>
+      </Tab>
+
+      <Tab title="Tab 1">
+        Or even any JSX
+      </Tab>
+    </Tabs>
+  </>
 );
 
 export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes, { shape } from 'prop-types';
+import { Tab } from '../Tab/Tab';
+
+export class Tabs extends React.Component {
+  state = {
+    activeTab: 0,
+  }
+
+  onTabSelected = selectedTab => () => {
+    this.setState({
+      activeTab: selectedTab,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <ul className="nav nav-tabs">
+          {this.props.children
+            ? this.props.children.map((tab, index) => (
+              <Tab
+                key={tab.props.title}
+                activeTab={this.state.activeTab === index}
+                title={tab.props.title}
+                click={this.onTabSelected(index)}
+              />
+            ))
+            : this.props.tabs.map((tab, index) => (
+              <Tab
+                key={tab.title}
+                activeTab={this.state.activeTab === index}
+                title={tab.title}
+                click={this.onTabSelected(index)}
+              />
+            ))
+          }
+        </ul>
+        <div id="myTabContent" className="tab-content">
+          {this.props.children
+            ? <>{this.props.children[this.state.activeTab].props.children}</>
+            : <>{this.props.tabs[this.state.activeTab].content}</>
+          }
+        </div>
+      </>
+    );
+  }
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  })),
+  children: PropTypes.arrayOf(shape({
+    props: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  })),
+};
+
+Tabs.defaultProps = {
+  tabs: Tabs.children,
+  children: Tab.tabs,
+};
