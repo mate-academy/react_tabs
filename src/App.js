2a2af28de300ee5b93e4fd7548715dce9a0f937f
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-28 11:53:00 +0300

    move tabs array to TabsData.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Tabs } from './components/Tabs/Tabs';
-
-// eslint-disable-next-line no-unused-vars
-const tabs = [
-  {
-    title: 'Tab 1',
-    content: 'Some text 1',
-  },
-  {
-    title: 'Tab 2',
-    content: 'Some text 2',
-  },
-  {
-    title: 'Tab 3',
-    content: 'Some text 3',
-  },
-];
+import { tabs } from './components/Tabs/TabsData';
 
 const App = () => (
   <Tabs tabs={tabs} />
