f30e466f656897f6faa2bbe07a2a99e492f00b57
nazmeln <nazik100378@gmail.com>
2020-06-28 12:09:43 +0300

    solved

---
+++ src/components/Tabs.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Tabs extends React.Component {
+  state = {
+    selectedIndex: 0,
+  }
+
+  render() {
+    const { tabs } = this.props;
+    const { selectedIndex } = this.state;
+
+    return (
+      <div className="tabs">
+        {tabs.map((tab, i) => (
+          <>
+            <button
+              type="button"
+              key={tab.id}
+              onClick={() => {
+                this.setState({ selectedIndex: i });
+              }}
+              className={(
+                i === selectedIndex ? 'button button--active' : 'button'
+              )}
+            >
+              { tab.title }
+            </button>
+          </>
+        )) }
+        <hr />
+        <div className="tab">{ tabs[selectedIndex].content }</div>
+      </div>
+    );
+  }
+}
+
+const TabShape = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  is: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+});
+
+Tabs.propTypes = {
+  tabs: TabShape.isRequired,
+};
+
+export { Tabs };
