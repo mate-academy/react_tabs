c573d938c9bf5ad515a5ce160cfdd28ac2a27713
Vladyslav Halushkin <v.halushkin@gmail.com>
2020-06-28 15:36:32 +0300

    task ready, lint-checked

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropType from 'prop-types';
+import { Tab } from '../Tab/Tab';
+import './Tabs.css';
+import { tabShape } from './tabShape';
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: this.props.tabs[this.props.index].content,
+    };
+  }
+
+  onTabSelected = (index) => {
+    this.setState({
+      content: this.props.tabs[index].content,
+    });
+  };
+
+  render() {
+    return (
+      <div className="tabs">
+        <div className="tabs__list">
+          {this.props.tabs.map((tab, index) => (
+            <Tab key={tab.title} onSelected={() => this.onTabSelected(index)}>
+              {tab.title}
+            </Tab>
+          ))}
+        </div>
+        <p className="tabs__content">{this.state.content}</p>
+      </div>
+    );
+  }
+}
+
+export { Tabs };
+
+Tabs.propTypes = {
+  tabs: PropType.arrayOf(tabShape).isRequired,
+  index: PropType.number.isRequired,
+};
