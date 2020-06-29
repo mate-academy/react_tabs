64f386807d5e5f41e2e96284b20a4e8586728631
bran4u <s.oselskiy@gmail.com>
2020-06-29 22:05:18 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './App.css';
+import Tabs from './Components/Tabs/Tabs';
 
 // eslint-disable-next-line no-unused-vars
 const tabs = [
   {
     title: 'Tab 1',
     content: 'Some text 1',
+    id: 1,
   },
   {
     title: 'Tab 2',
     content: 'Some text 2',
+    id: 2,
   },
   {
     title: 'Tab 3',
     content: 'Some text 3',
+    id: 3,
   },
 ];
 
 const App = () => (
-  <h1>React tabs</h1>
+  <Tabs tabs={tabs} />
 );
 
 export default App;
---
+++ src/Components/Tabs/Tabs.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Tabs.css';
+
+export default class Tabs extends React.Component {
+  state = {
+    tabIndex: 0,
+  };
+
+  clickHandler = (index) => {
+    this.setState({ tabIndex: index });
+  }
+
+  render() {
+    const { tabs } = this.props;
+
+    return (
+      <div className="tabs__container">
+        <div className="tabs__nav">
+          {
+            tabs.map((tab, index) => (
+              <button
+                type="button"
+                onClick={() => this.clickHandler(index)}
+              >
+                {tab.title}
+              </button>
+            ))
+          }
+        </div>
+        <div className="tabs__content">
+          {this.props.tabs[this.state.tabIndex].content}
+        </div>
+      </div>
+    );
+  }
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    content: PropTypes.string,
+  })).isRequired,
+};
