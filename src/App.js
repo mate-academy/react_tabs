aead60ed8e13cf21e079cd54ccd16eef03754872
DTR <t.terry.rice@gmail.com>
2020-06-30 22:25:54 +0300

    final 1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import './App.css';
-import Tab from './components/Tab/Tab';
+import tabs from './box/tabs';
 
-const App = () => (
-  <>
-    <Tab />
-  </>
-);
+class App extends React.PureComponent {
+  state = {
+    id: 0,
+  }
+
+  switch = (event) => {
+    event.preventDefault();
+    const number = event.target.getAttribute('href');
+
+    this.setState(() => ({
+      id: number,
+    }));
+  }
+
+  blur = event => event.target.classList.remove('active');
+
+  focus = event => event.target.classList.add('active');
+
+  render() {
+    return (
+      <>
+        <section className="tab__tittle">
+
+          {tabs.map((tab, index) => (
+            <React.Fragment key={tab.title}>
+              <a
+                href={`${index}`}
+                className="tab__element"
+                onClick={this.switch}
+                onBlur={this.blur}
+                onFocus={this.focus}
+              >
+                {tab.title}
+              </a>
+            </React.Fragment>
+          ))}
+        </section>
+
+        <section className="tab__content">
+          <p>{tabs[this.state.id].content}</p>
+        </section>
+      </>
+    );
+  }
+}
+
+const tab = (event) => {
+  if (event.key === 'Tab' || event.target.className === 'tab__element') {
+    event.target.click();
+  }
+};
+
+document.addEventListener('keyup', tab);
 
 export default App;
