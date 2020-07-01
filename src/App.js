3b7417c5a93a73e7f9d8887eece51044ceec5607
Andrew Nickyforov <nik_andrey@ukr.net>
2020-07-01 11:12:03 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line no-unused-vars
 const tabs = [
   {
-    title: 'Tab 1',
+    title: 'Home',
     content: 'Some text 1',
   },
   {
-    title: 'Tab 2',
+    title: 'Profile',
     content: 'Some text 2',
   },
   {
-    title: 'Tab 3',
+    title: 'Contact',
     content: 'Some text 3',
   },
 ];
 
+class Tabs extends React.Component {
+  state = {
+    activeTab: 0,
+  }
+
+  changeActiveIndex = (index) => {
+    this.setState({
+      activeTab: index,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="tabs">
+          {
+            this.props.tabs.map((tab, index) => (
+              <button
+                type="button"
+                onClick={() => this.changeActiveIndex(index)}
+                className={
+                  this.state.activeTab === index
+                    ? 'tab tab--active'
+                    : 'tab'}
+              >
+                {tab.title}
+              </button>
+            ))
+          }
+        </div>
+        <p>{tabs[this.state.activeTab].content}</p>
+      </>
+    );
+  }
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
 const App = () => (
-  <h1>React tabs</h1>
+  <Tabs tabs={tabs} />
 );
 
 export default App;
