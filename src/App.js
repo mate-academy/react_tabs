a26a3b2106b05f90c17f0f0f0fee544c25e16845
Andrew Nickyforov <nik_andrey@ukr.net>
2020-07-01 13:44:12 +0300

    added component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
+import { Tabs } from './Tabs';
 
 // eslint-disable-next-line no-unused-vars
 const tabs = [
@@ -17,52 +17,6 @@ const tabs = [
     content: 'Some text 3',
   },
 ];
-
-class Tabs extends React.Component {
-  state = {
-    activeTab: 0,
-  }
-
-  changeActiveIndex = (index) => {
-    this.setState({
-      activeTab: index,
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <div className="tabs">
-          {
-            this.props.tabs.map((tab, index) => (
-              <button
-                type="button"
-                key={tab.title}
-                onClick={() => this.changeActiveIndex(index)}
-                className={
-                  this.state.activeTab === index
-                    ? 'tab tab--active'
-                    : 'tab'}
-              >
-                {tab.title}
-              </button>
-            ))
-          }
-        </div>
-        <p>{tabs[this.state.activeTab].content}</p>
-      </>
-    );
-  }
-}
-
-Tabs.propTypes = {
-  tabs: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-    }).isRequired,
-  ).isRequired,
-};
 
 const App = () => (
   <Tabs tabs={tabs} />
