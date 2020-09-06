aa8cd090e27a481e80d6ae8a55e8fa04a1fb0857
nanccyy <lidiapriymak@gmail.com>
2020-09-06 10:57:14 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Tab from './Tab';
 
 // eslint-disable-next-line no-unused-vars
 const tabs = [
@@ -17,8 +18,35 @@ const tabs = [
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+class App extends React.Component {
+  state = {
+    number: 0,
+    content: tabs[0].content,
+  }
+
+  onTabSelected = (index) => {
+    this.setState({
+      content: tabs[index].content,
+      number: index,
+    });
+  }
+
+  render() {
+    const { content, number } = this.state;
+
+    return (
+      <div>
+        <h1>React tabs</h1>
+
+        <Tab
+          tabs={tabs}
+          onSelected={this.onTabSelected}
+          content={content}
+          activeNumber={number}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
