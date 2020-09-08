d5dbee7859eb8c2538b3b3d63fe913236100f2ba
artsashko <artyomsashko@gmail.com>
2020-09-08 15:08:46 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Tabs } from './Tabs';
 
 // eslint-disable-next-line no-unused-vars
 const tabs = [
@@ -17,8 +18,33 @@ const tabs = [
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+const preparedTabs = tabs.map((tab, i) => ({
+  id: i, ...tab,
+}));
+
+class App extends React.Component {
+  state = {
+    activeTabIndex: 0,
+  }
+
+  onTabSelected = (id) => {
+    this.setState({ activeTabIndex: id });
+  };
+
+  render() {
+    const { activeTabIndex } = this.state;
+
+    return (
+      <>
+        <h1>React tabs</h1>
+        <Tabs
+          tabs={preparedTabs}
+          activeTabIndex={activeTabIndex}
+          onSelect={this.onTabSelected}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
