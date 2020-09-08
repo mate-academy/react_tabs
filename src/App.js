236d0f3e0fee7d7364edc5dc876aaa949b542fa6
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-09-08 19:10:56 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Tabs } from './components/Tabs/index';
 
 // eslint-disable-next-line no-unused-vars
-const tabs = [
+const tabsFromServer = [
   {
     title: 'Tab 1',
     content: 'Some text 1',
@@ -18,12 +18,33 @@ const tabs = [
   },
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>React tabs</h1>
-    <Tabs tabs={tabs} />
-  </div>
+export class App extends React.Component {
+  state = {
+    tabs: tabsFromServer.map((tab, index) => ({
+      ...tab,
+      index,
+    })),
+    index: 0,
+  };
 
-);
+  onTabSelected = (index) => {
+    this.setState({
+      index,
+    });
+  }
 
-export default App;
+  render() {
+    const { tabs, index } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React tabs</h1>
+        <Tabs
+          tabs={tabs}
+          tabIndex={index}
+          handleTabSelect={this.onTabSelected}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,46 +1,34 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import './Tabs.scss';
 
-export class Tabs extends React.Component {
-  state = {
-    tabIndex: 0,
-  }
-
-  render() {
-    const { tabs } = this.props;
-    const { tabIndex } = this.state;
-
-    return (
-      <div className="tabs">
-        <div>
-          {tabs.map((tab, index) => (
-            <button
-              key={tab.title}
-              type="button"
-              className={classNames({
-                tabs__button: true,
-                selected: tabIndex === index,
-              })}
-              onClick={() => {
-                this.setState({
-                  tabIndex: index,
-                });
-              }
-              }
-            >
-              {tab.title}
-            </button>
-          ))}
-        </div>
-        <p className="tabs__content">
-          {tabs[tabIndex].content}
-        </p>
-      </div>
-    );
-  }
-}
+export const Tabs = ({
+  tabs,
+  tabIndex,
+  handleTabSelect,
+}) => (
+  <div className="tabs">
+    <div>
+      {tabs.map(tab => (
+        <button
+          key={tab.title}
+          type="button"
+          className={classNames({
+            tabs__button: true,
+            selected: tabIndex === tab.index,
+          })}
+          onClick={() => handleTabSelect(tab.index)}
+        >
+          {tab.title}
+        </button>
+      ))}
+    </div>
+    <p className="tabs__content">
+      {tabs[tabIndex].content}
+    </p>
+  </div>
+);
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(
@@ -49,4 +37,6 @@ Tabs.propTypes = {
       content: PropTypes.string,
     }),
   ).isRequired,
+  tabIndex: PropTypes.string.isRequired,
+  handleTabSelect: PropTypes.func.isRequired,
 };
