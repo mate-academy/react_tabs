ee16aaac98cf7d476462a4cd9576190a432742fd
yuriimovchko <movchko.yuriy@gmail.com>
2020-09-09 14:34:11 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
+import { Tabs } from './components/Tabs';
 
-// eslint-disable-next-line no-unused-vars
 const tabs = [
   {
     title: 'Tab 1',
@@ -17,8 +17,27 @@ const tabs = [
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+class App extends React.Component {
+  state = {
+    activeIndex: 0,
+  }
+
+  changeTab = (event) => {
+    this.setState({ activeIndex: +event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>React tabs</h1>
+        <Tabs
+          tabs={tabs}
+          changeTab={this.changeTab}
+          activeIndex={this.state.activeIndex}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
