ef96195e19b36b019e52b5c4054c0e64840156d8
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-11-03 17:12:24 +0200

    solution

---
+++ src/components/Tab.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Tab extends React.PureComponent {
+  static propTypes = {
+    activeTab: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+
+  onClick = () => {
+    const { label, onClick } = this.props;
+
+    onClick(label);
+  }
+
+  render() {
+    const {
+      onClick,
+      props: {
+        activeTab,
+        label,
+      },
+    } = this;
+
+    let className = 'tab-list-item';
+
+    if (activeTab === label) {
+      className += ' tab-list-active';
+    }
+
+    return (
+      <li
+        className={className}
+        onClick={onClick}
+        role="presentation" // ?
+      >
+        {label}
+      </li>
+    );
+  }
+}
+
+export default Tab;
