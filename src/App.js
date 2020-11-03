bfdfb89a4fd7d9aefa28f0541ca9826e4146112a
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-03 15:52:58 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import './App.css';
+import Tabs from './components/Tabs/Tabs';
 
-// eslint-disable-next-line no-unused-vars
 const tabs = [
   {
     title: 'Tab 1',
-    content: 'Some text 1',
+    content: 'Content 1',
   },
   {
     title: 'Tab 2',
-    content: 'Some text 2',
+    content: 'Content 2',
   },
   {
     title: 'Tab 3',
-    content: 'Some text 3',
+    content: 'Content 3',
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+class App extends React.PureComponent {
+  state = {
+    currentIndex: 0,
+  }
+
+  onSelect = (index) => {
+    this.setState({
+      currentIndex: index,
+    });
+  }
+
+  render() {
+    const { currentIndex } = this.state;
+
+    return (
+      <div className="app">
+        <h1>React tabs</h1>
+        <Tabs
+          tabs={tabs}
+          index={currentIndex}
+          onSelect={this.onSelect}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
