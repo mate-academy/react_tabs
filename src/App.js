663824d4e394e43b0a030b93808475cb3828f291
Layrovell <marina95prodan@gmail.com>
2021-01-24 14:32:54 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import './App.css';
+import { Tab } from './components/Tab/Tab';
+import 'bulma/css/bulma.css';
 
-// eslint-disable-next-line no-unused-vars
 const tabs = [
   {
-    title: 'Tab 1',
-    content: 'Some text 1',
+    title: 'Tab 1', content: 'Some text 1',
   },
   {
-    title: 'Tab 2',
-    content: 'Some text 2',
+    title: 'Tab 2', content: 'Some text 2',
   },
   {
-    title: 'Tab 3',
-    content: 'Some text 3',
+    title: 'Tab 3', content: 'Some text 3',
   },
 ];
 
-const App = () => (
-  <h1>React tabs</h1>
-);
+class App extends React.Component {
+  state = {
+    active: 'Tab 1',
+  }
+
+  handlerOnClick = (e) => {
+    this.setState({
+      active: e,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React tabs</h1>
+        <Tab
+          tabs={tabs}
+          active={this.state.active}
+          handlerOnClick={this.handlerOnClick}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
