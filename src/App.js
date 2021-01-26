eb391d9de6c18f9ab0eb36d23fd853e56fcdf2fd
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-01-26 21:59:56 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import './App.css';
 // eslint-disable-next-line no-unused-vars
 const tabs = [
   {
+    id: 0,
     title: 'Tab 1',
     content: 'Some text 1',
   },
   {
+    id: 1,
     title: 'Tab 2',
     content: 'Some text 2',
   },
   {
+    id: 2,
     title: 'Tab 3',
     content: 'Some text 3',
   },
@@ -21,27 +24,42 @@ const tabs = [
 
 class App extends React.Component {
   state = {
-    tabsCopy: [...tabs].map(tab => ({ ...tab })),
-    activeTab: 0,
+    // tabsCopy: [...tabs].map(tab => ({ ...tab })),
+    activeTabIndex: 0,
   }
 
-  onTabSelected = (toggler) => {
-    const togglerIndex = tabs.findIndex(tab => tab.title === toggler.title);
+  // onTabSelected = (toggler) => {
+  //   const togglerIndex = tabs.findIndex(tab => tab.title === toggler.title);
 
-    this.setState({
-      activeTab: togglerIndex,
-    });
+  //   this.setState({
+  //     activeTabIndex: togglerIndex,
+  //   });
+  // }
+
+  onTabSelected = (selectedTabId) => {
+    this.setState({ activeTabIndex: selectedTabId });
   }
 
   render() {
-    const { activeTab, tabsCopy } = this.state;
+    const { activeTabIndex } = this.state;
 
     return (
-      <Tabs
-        tabs={tabsCopy}
-        activeTab={activeTab}
-        onTabSelected={this.onTabSelected}
-      />
+      <>
+        <Tabs
+          tabs={tabs}
+          activeTabIndex={activeTabIndex}
+          onTabSelected={this.onTabSelected}
+        />
+
+        {tabs
+          .filter((tab, index) => activeTabIndex === index)
+          .map(tab => (
+            <p key={tab.title}>
+              {tab.content}
+            </p>
+          ))
+        }
+      </>
     );
   }
 }
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Tabs = ({ tabs, activeTab, onTabSelected }) => (
-  <>
-    <div>
-      {tabs.map(tab => (
-        <button
-          key={tab.title}
-          type="button"
-          className="Tabs__button"
-          onClick={() => onTabSelected(tab)}
-        >
-          {tab.title}
-        </button>
-      ))}
-    </div>
-
-    <div>
-      {tabs
-        .filter((tab, index) => activeTab === index)
-        .map(tab => (
-          <p key={tab.title}>
-            {tab.content}
-          </p>
-        ))
-      }
-    </div>
-  </>
+export const Tabs = ({ tabs, activeTabIndex, onTabSelected }) => (
+  <div>
+    {tabs.map(tab => (
+      <button
+        key={tab.title}
+        type="button"
+        className="Tabs__button"
+        onClick={() => onTabSelected(tab.id)}
+      >
+        {tab.title}
+      </button>
+    ))}
+  </div>
 );
 
 Tabs.propTypes = {
-  activeTab: PropTypes.number.isRequired,
+  activeTabIndex: PropTypes.number.isRequired,
   onTabSelected: PropTypes.func.isRequired,
   tabs: PropTypes.arrayOf(
     PropTypes.shape({
