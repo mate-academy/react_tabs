28e925fae2fd7203f6ce6e79a05753a19b70d765
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-27 11:08:42 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import React from 'react';
 import './App.css';
-
 import { Tabs } from './Tabs';
 
 // eslint-disable-next-line no-unused-vars
-const tabsFromServer = [
+const tabs = [
   {
     title: 'Tab 1',
     content: 'Some text 1',
@@ -20,11 +18,39 @@ const tabsFromServer = [
   },
 ];
 
-const App = () => (
-  <>
-    <h1>React tabs</h1>
-    <Tabs items={tabsFromServer} />
-  </>
-);
+const tabsFromServer = tabs.map((tab, index) => ({
+  ...tab,
+  id: index,
+}));
+
+export class App extends React.Component {
+  state = {
+    arrayTabs: tabsFromServer,
+    content: '',
+  };
+
+  onTabSelected = (id) => {
+    this.setState(state => ({
+      content: state.arrayTabs.find(tab => tab.id === id).content,
+    }));
+  }
+
+  render() {
+    const { arrayTabs, content } = this.state;
+
+    return (
+      <div>
+        <h1>React tabs</h1>
+
+        <Tabs
+          tabs={arrayTabs}
+          onTabSelected={this.onTabSelected}
+        />
+
+        <p>{content}</p>
+      </div>
+    );
+  }
+}
 
 export default App;
