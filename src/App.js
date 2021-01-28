93df0f850b3d8044da6c644105d16f09b2a5a9e1
Dima Bond <neverpooh@gmail.com>
2021-01-28 16:06:09 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -2,24 +2,54 @@ import React from 'react';
 import './App.css';
 import { Tabs } from './Tabs';
 
-// eslint-disable-next-line no-unused-vars
-const tabs = [
+const tabsFromServer = [
   {
+    id: 1,
     title: 'Tab 1',
     content: 'Some text 1',
   },
   {
+    id: 2,
     title: 'Tab 2',
     content: 'Some text 2',
   },
   {
+    id: 3,
     title: 'Tab 3',
     content: 'Some text 3',
   },
 ];
 
-const App = () => (
-  <Tabs tabs={tabs} />
-);
+class App extends React.Component {
+  state = {
+    tabs: [...tabsFromServer],
+    selectedTab: 1,
+  }
+
+  onTabSelected = (id) => {
+    this.setState({
+      selectedTab: id,
+    });
+  }
+
+  render() {
+    const { tabs, selectedTab } = this.state;
+
+    const selectedTabContent = tabs.find(tab => tab.id === selectedTab);
+
+    return (
+      <>
+        <Tabs
+          onTabSelected={this.onTabSelected}
+          selectedTab={this.state.selectedTab}
+          tabs={tabs}
+        />
+        <div className="tab__content">
+          {selectedTabContent.content}
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/Tabs.js
+++ src/Tabs.js
@@ -2,49 +2,29 @@ import React from 'react';
 import ClassNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export class Tabs extends React.Component {
-  state = {
-    selectedTab: 'Tab 1',
-  }
-
-  onTabSelected = (title) => {
-    this.setState({
-      selectedTab: title,
-    });
-  }
-
-  render() {
-    const { selectedTab } = this.state;
-    const { tabs } = this.props;
-
-    const visibleTabText = tabs.find(tab => tab.title === selectedTab);
-
-    return (
-      <>
-        <div className="tab">
-          {tabs.map(tab => (
-            <button
-              className={ClassNames({ active: tab.title === selectedTab })}
-              key={tab.title}
-              type="button"
-              onClick={() => {
-                this.onTabSelected(tab.title);
-              }}
-            >
-              {tab.title}
-            </button>
-          ))}
-        </div>
-        <div className="tab__content">
-          {visibleTabText.content}
-        </div>
-      </>
-    );
-  }
-}
+export const Tabs = ({ tabs, onTabSelected, selectedTab }) => (
+  <>
+    <div className="tab">
+      {tabs.map(tab => (
+        <button
+          className={ClassNames({ active: tab.id === selectedTab })}
+          key={tab.id}
+          type="button"
+          onClick={() => {
+            onTabSelected(tab.id);
+          }}
+        >
+          {tab.title}
+        </button>
+      ))}
+    </div>
+  </>
+);
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.shape(
     PropTypes.string.isRequired,
   )).isRequired,
+  onTabSelected: PropTypes.func.isRequired,
+  selectedTab: PropTypes.number.isRequired,
 };
