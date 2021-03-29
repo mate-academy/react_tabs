07c79bbb8ed322799aa3596f069002c46837b52f
Ping <4ivrik@ukr.net>
2021-03-29 16:51:35 +0300

    solution

---
+++ src/Tabs.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './App.css';
+
+export const Tabs = ({ tabs, selectedTab, callback }) => (
+  <div>
+    <ul className="block">
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className="block__item"
+        >
+          <button
+            type="button"
+            onClick={() => callback(tab)}
+            className={tab.id === selectedTab.id
+              ? 'active'
+              : 'block__button'
+              }
+          >
+            {tab.title}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+
+  selectedTab: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+  callback: PropTypes.func.isRequired,
+};
