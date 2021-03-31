b38b2e996dc8345e4dfdb26f0940483ff4e75569
Smihotur Roman <smikhotur@gmail.com>
2021-03-31 22:14:09 +0300

    Solution task tabs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-
+import { Tabs } from './components/Tabs/Tabs';
 import './App.scss';
 
 /* eslint-disable */
 const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Home', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Profile', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Contact', content: 'Some text 3' },
 ];
 /* eslint-enable */
 
-const App = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    tabsCopy: tabs.map(tab => ({ ...tab })),
+    selectedTabIndex: 0,
+  }
+
+  onTabSelected = (tab) => {
+    const { tabsCopy } = this.state;
+    const findSelectedTabIndex = tabsCopy
+      .findIndex(el => el.title === tab.title);
+
+    this.setState({
+      selectedTabIndex: findSelectedTabIndex,
+    });
+  }
+
+  render() {
+    const { tabsCopy, selectedTabIndex } = this.state;
+
+    return (
+      <section className="app">
+        <h1 className="title">React tabs</h1>
+        <Tabs
+          tabsCopy={tabsCopy}
+          selectedTabIndex={selectedTabIndex}
+          onTabSelected={this.onTabSelected}
+        />
+      </section>
+    );
+  }
+}
 
 export default App;
