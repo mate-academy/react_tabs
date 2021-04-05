3aa3da77da957bc8c1ca6ed2685579c99ae7a293
Anna <garsinaanna32778@gmail.com>
2021-04-05 12:48:59 +0300

    added index verification

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends React.Component {
   };
 
   onTabSelected = (index) => {
+    if (index === this.state.currentIndex) {
+      return;
+    }
+
     this.setState({
       currentIndex: index,
     });
