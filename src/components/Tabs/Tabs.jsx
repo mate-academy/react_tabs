7bcf78ed334b2bae7cdd48a8683775798e4d020a
olena <melolena33@gmail.com>
2021-04-09 14:49:19 +0300

    Solution

---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './tabs.scss';
+
+export const Tabs = ({ tabs, selectedTabId = tabs[0].id, onTabSelected }) => (
+  <div>
+    <div className="tabs">
+      {tabs.map((tab, index) => (
+        <button
+          type="button"
+          key={tab.id}
+          className={`tab ${
+            (tab.id === selectedTabId)
+              ? 'selected'
+              : ''}`}
+          onClick={() => {
+            onTabSelected(tab.id);
+          }}
+          onKeyUp={() => {
+            onTabSelected(tab.id);
+          }}
+        >
+          {tab.title}
+        </button>
+      ))}
+    </div>
+    <textarea
+      value={tabs.find(tab => tab.id === selectedTabId).content}
+      className="content"
+      readOnly
+    />
+  </div>
+);
+
+const TabType = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+});
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(TabType).isRequired,
+  selectedTabId: PropTypes.string,
+  onTabSelected: PropTypes.func.isRequired,
+};
+
+Tabs.defaultProps = {
+  selectedTabId: 0,
+};
