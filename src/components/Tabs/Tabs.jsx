a8855bdcf995b69462dd13d6784941f23f3f8f0c
Alexander Levchenko <a.levchenko603@gmail.com>
2021-04-13 20:42:27 +0300

    first solution

---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import './tabs.scss';
+
+
+export class Tabs extends React.Component {
+
+  render() {
+    const { tabs, selectedTabId, onTabSelected } = this.props;
+
+    return (
+      <div className="tabs">
+        <ul className="tabs__list">
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className="tabs__list-item"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </li>
+          ))}
+        </ul>
+        <div className="tabs__list-content">
+          {tabs.find(tab => tab.id === selectedTabId).content}
+        </div>
+      </div>
+    )
+  }
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })).isRequired,
+    selectedTabId: PropTypes.string.isRequired,
+    onTabSelected: PropTypes.func.isRequired,
+};
