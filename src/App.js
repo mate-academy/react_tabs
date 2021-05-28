71dbe35a6d4c7ad1932fc31cc3efbd04b1301761
Vitalii Siedykh <siedykh.v@gmail.com>
2021-05-28 13:04:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,47 +2,48 @@ import React from 'react';
 
 import './App.css';
 import { Tabs } from './Tabs';
+import 'bulma/css/bulma.css';
 
 /* eslint-disable */
-const tabs = [
+
+const tabsFromServer = [
   { id: 'tab-1', title: 'Tab 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et varius nunc. Mauris imperdiet odio mi, eu ultricies urna molestie quis. Maecenas vitae suscipit turpis. Nunc fringilla diam sed ipsum facilisis facilisis. Suspendisse maximus at mi dictum consequat. Curabitur elementum.' },
   { id: 'tab-2', title: 'Tab 2', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur lectus odio, a iaculis dui sodales nec. Duis in posuere lectus, non consequat dui. Pellentesque eu orci massa. Fusce mauris mauris, fermentum a tincidunt eu, tincidunt vel diam. Sed in.' },
   { id: 'tab-3', title: 'Tab 3', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius fringilla lobortis. In gravida feugiat suscipit. Suspendisse sit amet blandit augue. Aliquam pellentesque id ipsum in tempus. Donec lobortis venenatis ornare. Donec ultricies interdum dolor, ut tincidunt velit malesuada in.' },
 ];
 /* eslint-enable */
 
-const tabsFromServer = tabs.map((tab, index) => ({
-  ...tab,
-  id: index,
-}));
-
 export class App extends React.Component {
   state = {
-    tabsArray: tabsFromServer,
-    content: '',
+    tabs: tabsFromServer.map(tab => ({ ...tab })),
+    selectedTab: tabsFromServer[0],
   }
 
-  onTabSelected = (id) => {
-    this.setState(state => ({
-      content: state.tabsArray.find(tab => tab.id === id).content,
-    }));
+  onTabSelected = (newTab) => {
+    this.setState({ selectedTab: newTab });
   }
 
   render() {
-    const { tabsArray, content } = this.state;
+    const { selectedTab, tabs } = this.state;
 
     return (
-      <div>
-        <h1>React tabs</h1>
+      <div className="columns is-centered">
+        <div className="column is-half box m-6">
+          <h1 className="has-text-centered is-size-2 mb-5">
+            Selected tab is:
+            {' '}
+            {selectedTab.title}
+          </h1>
 
-        <Tabs
-          tabs={tabsArray}
-          onTabSelected={this.onTabSelected}
-        />
-
-        <p>{content}</p>
+          <Tabs
+            tabs={tabs}
+            onTabSelected={this.onTabSelected}
+            selectedTabId={selectedTab.id}
+          />
+        </div>
       </div>
     );
   }
 }
+
 export default App;
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,30 +1,47 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-export const Tabs = ({ tabs, onTabSelected }) => (
-  <div className="wrapper">
-    {tabs.map(tab => (
-      <button
-        key={tab.id}
-        type="button"
-        className="button"
-        onClick={() => {
-          onTabSelected(tab.id);
-        }}
-      >
-        {tab.title}
-      </button>
-    ))
-    }
-  </div>
+export const Tabs = ({ tabs, onTabSelected, selectedTabId }) => (
+  <>
+    <div className="tabs is-centered is-medium is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={classNames('', {
+              'is-active': tab.id === selectedTabId,
+            })}
+          >
+            <a
+              href="#/"
+              onClick={() => {
+                onTabSelected(tab);
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <p className="has-text-centered">
+      {tabs.find(tab => tab.id === selectedTabId).content}
+    </p>
+  </>
 );
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
+
   onTabSelected: PropTypes.func.isRequired,
+
+  selectedTabId: PropTypes.string.isRequired,
 };
