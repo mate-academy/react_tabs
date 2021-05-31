9824cc90da391d6e354d79a95515ca6cade2e524
Ivan Kolodyazhnyi <80517717+IvanKolodiazhnyi@users.noreply.github.com>
2021-05-31 11:04:15 +0300

    v2

--- src/Components/Tabs/Tabs.jsx
+++ src/Components/Tabs/Tabs.jsx
@@ -4,38 +4,37 @@ import PropTypes from 'prop-types';
 
 import './Tabs.scss';
 
-export class Tabs extends React.Component {
-  render() {
-    const { tabs, id, onTabSelected } = this.props;
-    return (
-      <>
-        <ul className="list">
-          {tabs.map(tab => (
-            <li key={tab.id} className="item">
-              <span
-                className={classNames('item_title', {
-                  active: tab.id === id,
-                })}
-                onClick={() => onTabSelected(tab)}
-              >
-                {tab.title}
-              </span>
-            </li>
-          ))}
-        </ul>
+export const Tabs = ({ tabs, id, onTabSelected }) => (
+  <>
+    <ul className="list">
+      {tabs.map(tab => (
+        <li key={tab.id} className="item">
+          <div
+            className={classNames('item_title', {
+              active: tab.id === id,
+            })}
+            onClick={() => onTabSelected(tab)}
+          >
+            {tab.title}
+          </div>
+        </li>
+      ))}
+    </ul>
 
-        <p className="content">
-          {tabs.find(tab => id === tab.id).content}
-        </p>
-      </>
-    );
-  };
-}
+    <p className="content">
+      {tabs.find(tab => id === tab.id).content}
+    </p>
+  </>
+);
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(
-    PropTypes.shape({}),
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }),
   ).isRequired,
   id: PropTypes.string.isRequired,
   onTabSelected: PropTypes.func.isRequired,
-}
+};
