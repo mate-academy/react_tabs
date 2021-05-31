3629248c3b2bccba0620632f3e453bb39a0125ee
Vlad Starzhitskiy <study.html.css.java@gmail.com>
2021-05-31 23:38:55 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Tabs } from './Tabs';
 
 import './App.scss';
 
-/* eslint-disable */
 const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  {
+    id: 'tab-1', title: 'Tab 1', content: 'Some text 1',
+  },
+  {
+    id: 'tab-2', title: 'Tab 2', content: 'Some text 2',
+  },
+  {
+    id: 'tab-3', title: 'Tab 3', content: 'Some text 3',
+  },
 ];
-/* eslint-enable */
 
 class App extends React.Component {
   state = {
