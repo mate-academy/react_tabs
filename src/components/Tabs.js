d7b5c1efc46a027b04e6b22f3d8bb1c4c76bbf36
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-07-01 11:16:02 +0300

    Solution

---
+++ src/components/Tabs.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Tabs extends Component {
+  state = {};
+
+  propTypes = {
+    tabs: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    content: PropTypes.string.isRequired,
+    onTabSelected: PropTypes.func.isRequired,
+  }
+
+  render() {
+    const { tabs, content, onTabSelected } = this.props;
+
+    return (
+      <>
+        {tabs.map(tab => (
+          <button
+            type="button"
+            key={tab.id}
+            onClick={() => onTabSelected(tab.id)}
+          >
+            {tab.title}
+          </button>
+        ))}
+
+        <p>{content}</p>
+
+      </>
+    );
+  }
+}
