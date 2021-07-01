5ac0e32500f79ca0a2f5bb7236b12a9a0643c25a
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-07-01 11:19:12 +0300

    Solution v0.0.1

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,16 +4,18 @@ import PropTypes from 'prop-types';
 export class Tabs extends Component {
   state = {};
 
-  propTypes = {
-    tabs: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        id: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-    content: PropTypes.string.isRequired,
-    onTabSelected: PropTypes.func.isRequired,
-  }
+  static = {
+    propTypes: {
+      tabs: PropTypes.arrayOf(
+        PropTypes.shape({
+          title: PropTypes.string.isRequired,
+          id: PropTypes.string.isRequired,
+        }),
+      ).isRequired,
+      content: PropTypes.string.isRequired,
+      onTabSelected: PropTypes.func.isRequired,
+    },
+  };
 
   render() {
     const { tabs, content, onTabSelected } = this.props;
