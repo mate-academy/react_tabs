d6e071316a70bdee1178104d623cf8cf9ac816f4
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-23 10:31:42 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from 'react';
-
+import TitleButton from './component/TitleButton/TitleButton';
+import TitleText from './component/TitleText/TitleText';
 import './App.scss';
 
 /* eslint-disable */
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text  3' },
 ];
 /* eslint-enable */
 
-const App = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    tabs,
+    isActiveTabId: 'tab-1',
+  }
+
+  getIdChoosedTitle = (id) => {
+    this.setState({ isActiveTabId: id });
+  }
+
+  findActiveTab = (value) => {
+    const selectedTab = tabs.find(tab => tab.id === this.state.isActiveTabId);
+
+    return selectedTab[value];
+  }
+
+  render() {
+    const { isActiveTabId } = this.state;
+
+    return (
+      <>
+        <h1
+          className="tabsHeader"
+        >
+          Tab selected
+          {isActiveTabId}
+        </h1>
+        <ul className="tabsList">
+          {this.state.tabs.map(tab => (
+            <li
+              key={tab.id}
+            >
+              <TitleButton
+                id={tab.id}
+                title={tab.title}
+                getIdChoosedTitle={this.getIdChoosedTitle}
+                isActiveTabId={isActiveTabId}
+              />
+            </li>
+          ))}
+        </ul>
+        <TitleText
+          title={this.findActiveTab('title')}
+          content={this.findActiveTab('content')}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
