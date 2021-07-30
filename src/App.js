472ee394b444c68e2604ddc373d9fdb32002563b
Sergei <serheifil@gmail.com>
2021-07-30 18:17:23 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.scss';
-import { Tabs } from './Tabs';
+import { Tabs } from './components/Tabs';
 
 /* eslint-disable */
 const tabs = [
@@ -17,6 +17,10 @@ class App extends React.Component {
   };
 
   onTabSelected = (tab) => {
+    if (tab.id === this.state.selectedTab.id) {
+      return;
+    }
+
     this.setState({ selectedTab: tab });
   }
 
