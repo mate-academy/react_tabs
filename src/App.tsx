a80760179b9c7c959c927a055cdf8935a208ada5
dmytroyevlakhov <yevlakhovdmytro@gmail.com>
2021-08-19 14:35:53 +0300

    typescript was added

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 import './App.scss';
 
-/* eslint-disable */
-const tabs = [
+const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-/* eslint-enable */
 
-const App = () => (
+const App: React.FC = () => (
   <div className="App">
     <h1>Selected tab is&nbsp;Tab 1</h1>
+    <div>{tabs.length}</div>
   </div>
 );
 
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react-scripts" />
+
+interface Tab {
+  id: string;
+  title: string;
+  content: string;
+}
