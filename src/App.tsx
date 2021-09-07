122f6fb0a80470f356638e7682cb032f5a64e6b0
lera-yaskevych <redrum.back@gmail.com>
2021-09-07 21:06:16 +0300

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-
+import { Tabs } from './Tabs';
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, a! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, totam deleniti asperiores distinctio qui nemo iste placeat unde amet quis voluptatem enim dolor laborum illum, similique blanditiis voluptate magnam! Nulla saepe hic eum sed quia corporis consequuntur quam voluptatum natus. Ipsam necessitatibus voluptatum exercitationem velit suscipit. Repellendus deleniti quisquam quaerat!' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit consectetur dolorem doloremque aliquam iste earum aliquid sit, nulla iure nesciunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, totam deleniti asperiores distinctio qui nemo iste placeat unde amet quis voluptatem enim dolor laborum illum, similique blanditiis voluptate magnam! Nulla saepe hic eum sed quia corporis consequuntur quam voluptatum natus. Ipsam necessitatibus voluptatum exercitationem velit suscipit. Repellendus deleniti quisquam quaerat!' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa modi cupiditate fugit voluptate. Earum, omnis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, totam deleniti asperiores distinctio qui nemo iste placeat unde amet quis voluptatem enim dolor laborum illum, similique blanditiis voluptate magnam! Nulla saepe hic eum sed quia corporis consequuntur quam voluptatum natus. Ipsam necessitatibus voluptatum exercitationem velit suscipit. Repellendus deleniti quisquam quaerat!' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  onTabSelected = (chosenTab: Tab) => {
+    this.setState({ selectedTab: chosenTab });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="container d-flex justify-content-center">
+        <div className="App w-75 border border-light p-5 mt-5">
+          <h1>
+            {`Selected tab is ${selectedTab.title}`}
+          </h1>
+          <Tabs tabs={tabs} selectedTabId={selectedTab.id} selectTab={this.onTabSelected} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
