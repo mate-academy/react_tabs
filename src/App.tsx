c501c3d4496febebfe52a2d16a8f91666c924d10
Artem <violence044@gmail.com>
2021-09-14 15:50:22 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 
 import './App.scss';
 
@@ -8,11 +9,38 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+interface State {
+  selectedTab: Tab;
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedTab: tabs[0],
+  };
+
+  handleSelect = (id: string) => {
+    const idx = tabs.findIndex(tab => tab.id === id);
+
+    this.setState({
+      selectedTab: tabs[idx],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected tab is&nbsp;
+          {this.state.selectedTab.id}
+        </h1>
+        <Button
+          tabs={tabs}
+          selectedTab={this.state.selectedTab}
+          handleSelect={this.handleSelect}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './App.scss';
+
+interface Props {
+  tabs: Tab[];
+  selectedTab: Tab;
+  handleSelect: (id: string) => void,
+}
+
+class Button extends React.Component<Props, {}> {
+  state = {};
+
+  render() {
+    const { tabs, selectedTab, handleSelect } = this.props;
+
+    return (
+      <ul className="nav nav-tabs">
+        {tabs.map(tab => {
+          return (
+            <li className="nav-item">
+              <button
+                type="button"
+                className="nav-link"
+                key={tab.id}
+                onClick={() => handleSelect(tab.id)}
+              >
+                {tab.title}
+              </button>
+              {selectedTab.id === tab.id ? tab.content : null }
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default Button;
