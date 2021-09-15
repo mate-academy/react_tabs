c1da99670cc9b5738928f9dea7e355ab51747e7b
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-09-15 13:58:13 +0300

    tabs divided into to parts, always on text div and buttons

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -12,23 +12,20 @@ export const Tabs: React.FC<Props> = (props) => {
   return (
     <div>
       {tabs.map(tab => (
-        <>
-          <button
-            key={tab.id}
-            type="button"
-            value={tab.title}
-            onClick={() => onTabSelected(tab)}
-            className="btn btn-secondary"
-          >
-            {tab.title}
-          </button>
-          <div>
-            {selectedTab.id === tab.id && (
-              <div>{tab.content}</div>
-            )}
-          </div>
-        </>
+        <button
+          key={tab.id}
+          type="button"
+          value={tab.title}
+          onClick={() => onTabSelected(tab)}
+          className="btn btn-secondary"
+        >
+          {tab.title}
+        </button>
       ))}
+
+      <div>
+        {selectedTab.content}
+      </div>
     </div>
   );
 };
