16a8ed9c833daa68b174271e363ab431b77fc001
Roman Yacenko <ryacenko3@gmail.com>
2021-09-15 15:50:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 
 import './App.scss';
+import { Buttons } from './components/Buttons';
+import { Content } from './components/Content';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia hic quod similique iusto error doloremque amet quos, labore nulla, voluptatibus possimus repellat aperiam eius numquam asperiores tempora culpa eveniet dicta nemo pariatur, aspernatur minus deserunt. Sed animi rem illum saepe, ducimus ipsam iusto dignissimos numquam!' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: string;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: 'tab-1',
+  };
+
+  changeTab = (id: string) => {
+    this.setState({ selectedTab: id });
+  };
+
+  render() {
+    const currentTab = tabs.find(tab => tab.id === this.state.selectedTab);
+
+    return (
+      <div className="App">
+        <h1>
+          Selected tab is
+          {' '}
+          {currentTab?.title}
+        </h1>
+        <Buttons tabButtons={tabs} changeTab={this.changeTab} />
+        <Content selectedTab={this.state.selectedTab} tabButtons={tabs} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface Props {
+  tabButtons: Tab[];
+  changeTab: (id: string) => void;
+}
+
+export const Buttons: React.FC<Props> = (props) => {
+  const { tabButtons, changeTab } = props;
+
+  return (
+    <div>
+      {tabButtons.map(tab => {
+        return (
+          <button
+            type="button"
+            key={tab.id}
+            onClick={() => changeTab(tab.id)}
+          >
+            {tab.title}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
