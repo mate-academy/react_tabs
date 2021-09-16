dfa747043d6fd2ddba98b53c8b6a4b2476df1064
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-09-16 10:48:30 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
@@ -8,11 +9,45 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+interface State {
+  selectedTab: Tab;
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedTab: tabs[0],
+  };
+
+  onTabSelected = (tab: Tab) => {
+    this.setState({
+      selectedTab: tab,
+    });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App">
+
+        <div className="container">
+          <h1>
+            {'Selected tab is '}
+          </h1>
+
+          <p className="title">
+            {selectedTab.title}
+          </p>
+        </div>
+
+        <Tabs
+          tabs={[...tabs]}
+          selectedTabId={selectedTab.id}
+          onTabSelected={this.onTabSelected}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
