ee1e0d50145347a4c04b08184edcaf0ab0fd41b6
vpolets <vpolets@gmail.com>
2021-09-22 17:33:04 +0300

    added cypress and mate-scripts to task

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,10 +1,5 @@
 describe('Page', () => {
   it('should be visitable', () => {
     cy.visit('/');
-    cy.get('.home')
-  });
-
-  it('should be visitable', () => {
-    cy.visit('/testsssss');
   });
 });
