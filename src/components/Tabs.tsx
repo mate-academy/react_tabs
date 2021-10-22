34dd88401618f1b4841b501379a1ad2898d62e2e
PresidentComanch <ap.dissembler@gmail.com>
2021-10-22 13:56:01 +0300

    --done

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+type TabsPropsType = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<TabsPropsType> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      <ul className="Tabslist">
+        {tabs.map(tab => (
+          <li key={tab.id} className="Tabslist__item">
+            <button
+              type="button"
+              className={classNames('Tabslist__button', { 'Tabslist__button--active': tab.id === selectedTabId })}
+              onClick={() => {
+                if (tab.id === selectedTabId) {
+                  return {};
+                }
+
+                return onTabSelected(tab);
+              }}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {tabs.filter(tab => tab.id === selectedTabId)
+        .map(tab => (
+          <article
+            key={tab.id}
+          >
+            {tab.content}
+          </article>
+        ))}
+    </>
+  );
+};
