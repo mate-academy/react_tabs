91334241f5a3ad0f3749d18a38fc6c4bc5710178
Roman Mefokov <meforoma@gmail.com>
2021-11-05 19:51:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-prop-types */
 import React from 'react';
 
 import './App.scss';
@@ -8,11 +9,66 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type TabsProps = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onSelect: any,
+};
+
+const Tabs = (props: TabsProps) => {
+  return (
+    <>
+      {tabs.map(tab => (
+        <>
+          <button
+            type="button"
+            key={tab.id}
+            name={tab.id}
+            onClick={props.onSelect}
+          >
+            {tab.title}
+          </button>
+        </>
+      ))}
+    </>
+  );
+};
+
+type AppState = {
+  selectedTabId: string,
+};
+
+class App extends React.Component<{}, AppState> {
+  state = {
+    selectedTabId: '0',
+  };
+
+  render() {
+    const { selectedTabId } = this.state;
+    const tabContent = tabs.find(tab => tab.id === selectedTabId)?.content;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected tab is&nbsp;
+          {selectedTabId}
+        </h1>
+
+        <Tabs
+          tabs={tabs}
+          selectedTabId={selectedTabId}
+          onSelect={(event: any) => {
+            this.setState({ selectedTabId: event.target.name });
+          }}
+        />
+
+        <p>
+          {tabContent}
+        </p>
+
+      </div>
+    );
+  }
+}
 
 export default App;
