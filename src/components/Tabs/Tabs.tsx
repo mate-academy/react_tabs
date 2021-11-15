ff31f843fe878c1965963d3980c12e9eb3a05bfa
Volodymyr Taranin <jaffplay@gmail.com>
2021-11-15 21:16:26 +0200

    code added

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Tab } from '../../types/types';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onTabSelected }) => {
+  const findContent = tabs.find(tab => tab.id === selectedTabId)?.content;
+
+  return (
+    <div className="tabs">
+      <ul className="tabs__list">
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className="tabs__list-item"
+          >
+            <button
+              type="button"
+              className={tab.id === selectedTabId
+                ? 'tabs__list-button tabs__list-button--active'
+                : 'tabs__list-button'}
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <p>
+        {findContent}
+      </p>
+    </div>
+  );
+};
