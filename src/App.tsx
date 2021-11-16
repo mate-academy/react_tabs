58b4e61e50e5f7ba08b8bb79358555efdc56bdb8
Denys Anosov <anosovdenys@gmail.com>
2021-11-16 22:42:51 +0200

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
 import React from 'react';
+import { Tab } from './types/Tab';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Съешь же ещё этих мягких французских булок да выпей чаю.' },
+  { id: 'tab-2', title: 'Tab 2', content: 'The quick brown fox jumps over the lazy dog.' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Лучше рука в небе, чем синица в журавле.' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+interface State {
+  selectedTab: Tab,
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  changeTab = (tab: Tab) => {
+    const { selectedTab } = this.state;
+
+    if (tab.id !== selectedTab.id) {
+      this.setState({
+        selectedTab: tabs.find(item => item.id === tab.id)
+          || { id: '', title: '', content: '' },
+      });
+    }
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App">
+        <h1>
+          Selected tab is&nbsp;
+          {selectedTab.title}
+        </h1>
+        <div>
+          {'Tabs: '}
+          {tabs.length}
+        </div>
+        <Tabs
+          tabs={tabs}
+          selectedTabId={selectedTab.id}
+          onTabSelect={this.changeTab}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import classnames from 'classnames';
+import { Tab } from '../types/Tab';
+import './Tabs.scss';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string,
+  onTabSelect: (tab: Tab) => void,
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelect,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <div>
+      <ul className="List">
+        {tabs.map(tab => (
+          <li
+            className="List__item"
+            key={tab.id}
+          >
+            <button
+              className={classnames({
+                'List__button List__button--selected': selectedTabId === tab.id,
+                List__button: selectedTabId !== tab.id,
+              })}
+              type="button"
+              onClick={() => onTabSelect(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="List__text">
+        {selectedTab && selectedTab.content}
+      </div>
+    </div>
+  );
+};
