980051f7c61caf1ef1ebf3d16dc2cb69a7064802
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-16 13:39:42 +0200

    Some improvements

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Tab } from '../../react-app-env';
-import './tab.scss';
+import './Tabs.scss';
 
 type Props = {
   tabs: Tab[];
@@ -27,7 +27,9 @@ export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onSelect }) => {
               id={tab.id}
               onClick={() => (onSelect(tab))}
             >
-              {tab.title}
+              <span className="tabs__title">
+                {tab.title}
+              </span>
             </button>
           );
         })}
