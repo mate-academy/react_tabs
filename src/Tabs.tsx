5e198cca0a5d385d1e7a6d85adc2d45790a23f71
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-11-16 20:22:33 +0200

    done

---
+++ src/Tabs.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onTabSelected }) => {
+  const selectedTab: Tab | undefined = tabs
+    .find(tab => tab.id === selectedTabId);
+
+  let selectedContent;
+
+  if (selectedTab) {
+    selectedContent = selectedTab.content;
+  } else {
+    selectedContent = tabs[0].content;
+  }
+
+  return (
+    <>
+      <div className="tabs">
+        {tabs.map(tab => (
+          <div className="tabs__container">
+            <button
+              type="button"
+              className={classNames('tabs__tab', { tabs__selected: tab === selectedTab })}
+              key={tab.id}
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div>
+        {selectedContent}
+      </div>
+    </>
+  );
+};
