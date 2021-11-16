73f543e3952e94e9cbca64fb9bf7a6fa50730078
Aleksey Senchenko <sav45731@gmail.com>
2021-11-16 23:35:46 +0300

    tabs solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
@@ -8,11 +9,31 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  chooseSelectedTab = (tab: Tab) => {
+    if (this.state.selectedTab.id !== tab.id) {
+      this.setState({
+        selectedTab: tabs.find(item => item.id === tab.id),
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{`Selected tab is ${this.state.selectedTab.title}`}</h1>
+        <Tabs
+          tabs={tabs}
+          selectedTabId={this.state.selectedTab.id}
+          chooseSelectedTab={this.chooseSelectedTab}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
