4c6d5e9009db97589a2823a60d05a01ecbb30776
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-17 15:53:27 +0200

    add npm classnames

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Tabs.scss';
 
 import { Tab } from '../types';
@@ -20,9 +21,12 @@ export const Tabs: React.FC<Props> = ({
         <li key={tab.id} className="tabs__item">
           <button
             type="button"
-            className={tab.id === selectedTabId.id
-              ? 'tabs__btn tabs__btn--active'
-              : 'tabs__btn '}
+            className={classNames(
+              'tabs__btn',
+              {
+                'tabs__btn--active': tab.id === selectedTabId.id,
+              },
+            )}
             onClick={() => {
               if (selectedTabId.id !== tab.id) {
                 onTabSelected(tab);
