dcc29105fc0c0a0bd93d19527e016f6d23d8c4d0
Lera Permiakova <vv.permiakova@gmail.com>
2021-12-16 16:27:13 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tab 1', content: 'bla '.repeat(265) },
+  { id: 'tab-2', title: 'Tab 2', content: 'ol '.repeat(160) },
+  { id: 'tab-3', title: 'Tab 3', content: 'hoho '.repeat(125) },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  onTabSelected = (tab: Tab) => {
+    this.setState({ selectedTab: tab });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          Selected tab is&nbsp;
+          {selectedTab.title}
+        </h1>
+        <div className="App__content">
+          <Tabs
+            tabs={tabs}
+            selectedTabId={selectedTab.id}
+            onTabSelected={this.onTabSelected}
+          />
+          <p className="App__info">{selectedTab.content}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
