ed09a1326c767bd90f3461c87568a73e084dea86
JOJO\38097 <kartemoko@gmail.com>
2021-12-16 20:30:51 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './types/types';
 
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Text 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Text 2. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Text 3. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    activeTab: tabs[0],
+  };
+
+  ontabSelected = (tab: Tab) => (
+    this.setState({
+      activeTab: tab,
+    })
+  );
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Selected tab is&nbsp;
+          {this.state.activeTab.title}
+        </h1>
+        <Tabs
+          tabs={tabs}
+          selectedTabId={this.state.activeTab.id}
+          onTabSelected={this.ontabSelected}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Tab } from '../../types/types';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onTabSelected }) => {
+  const findContent = tabs.find(tab => tab.id === selectedTabId)?.content;
+
+  return (
+    <div className="tabs">
+      <ul className="tabs__list">
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={classNames(
+              'tabs__list-item',
+              { 'tabs__list-item--active': tab.id === selectedTabId },
+            )}
+          >
+            <button
+              type="button"
+              className={classNames(
+                'tabs__list-button',
+                { 'tabs__list-button--active': tab.id === selectedTabId },
+              )}
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <p>
+        {findContent}
+      </p>
+    </div>
+  );
+};
