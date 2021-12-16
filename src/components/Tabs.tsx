4dcc1de3db7db6e08b8b0638a2555ee2e1da0112
Artem Skachkov <erocezz@gmail.com>
2021-12-16 20:51:11 +0200

    solution

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId)
+    || tabs[0];
+
+  return (
+    <div className="Tab">
+      <ul className="Tab__list">
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              className="Tab__button"
+              onClick={() => (tab !== selectedTab
+                ? onTabSelected(tab)
+                : null
+              )}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p className="Tab__content">
+        {selectedTab.content}
+      </p>
+    </div>
+  );
+};
