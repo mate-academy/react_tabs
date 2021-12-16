25f634915f811630d3003073172b8b11bd66509f
Ivan Meleganych <imi92@ukr.net>
2021-12-16 16:55:39 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import { Tab } from './types/Tab/Tab';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,38 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab,
+};
+
+class App extends React.PureComponent<{}, State> {
+  state: State = {
+    selectedTab: tabs[0],
+  };
+
+  onTabSelected = (tab: Tab) => {
+    this.setState({
+      selectedTab: tab,
+    });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <>
+        <div className="App">
+          <h1>
+            Selected tab is
+            {' '}
+            {selectedTab.title}
+          </h1>
+          <Tabs tabs={tabs} selectedTabId={selectedTab.id} onTabSelected={this.onTabSelected} />
+          <p className="tab-content">{selectedTab.content}</p>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onTabSelected }) => {
+  return (
+    <ul className="tabs-container">
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={classNames('tab-title', { 'tab-title--active': tab.id === selectedTabId })}
+        >
+          <a
+            className="tab-title__link"
+            href="/#"
+            onClick={(event) => {
+              onTabSelected(tab);
+              event.preventDefault();
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
