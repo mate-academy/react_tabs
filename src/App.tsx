d58cf4d5c22cbd92db82e51548f21f2cf44a689f
Aleksandr Prokopovich <aldmpr@gmail.com>
2021-12-18 14:41:29 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
+import { Tabs } from './components/Tabs';
+import { Tab } from './react-app-env';
 
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Home', content: 'Home sweet home' },
+  { id: 'tab-2', title: 'Profile', content: 'Profile info' },
+  { id: 'tab-3', title: 'Contact', content: 'Phone number' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    selectedTab: tabs[0],
+  };
+
+  selectTab = (newSelectedTab: Tab) => {
+    this.setState({
+      selectedTab: newSelectedTab,
+    });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App">
+        <h3 className="selected-title mb-5">
+          {'Selected tab: '}
+          {selectedTab.title}
+        </h3>
+
+        <section className="App__tabs">
+          <Tabs
+            tabs={tabs}
+            selectedTabId={selectedTab.id}
+            onTabSelected={this.selectTab}
+          />
+          {selectedTab.content}
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import { Tab } from '../react-app-env';
+
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: Tab['id'];
+  onTabSelected: (newSelectedTab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <ul className="Tabs">
+      {tabs.map((tab) => (
+        <li
+          key={tab.id}
+          className={classNames('Tabs__tab-title', {
+            'Tabs__tab-title--active': tab.id === selectedTabId,
+          })}
+        >
+          <a
+            href="/#"
+            className="Tabs__tab-link text-decoration-none"
+            onClick={(event) => {
+              onTabSelected(tab);
+              event.preventDefault();
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
