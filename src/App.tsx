7833ab6684d339fc371f13e30ae91abb20cc9f6d
Vladyslav Gonsales <www.magicbox@gmail.com>
2021-12-19 03:29:38 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Кнопка 1', content: 'Ты всё сможешь!' },
+  { id: 'tab-2', title: 'Кнопка 2', content: 'Ты - сладкая булочка' },
+  { id: 'tab-3', title: 'Кнопка 3', content: 'Погладь первого попавшегося тебе котика' },
+  { id: 'tab-4', title: 'Кнопка 4', content: 'Купи шоколадку и съешь всю сам' },
+  { id: 'tab-5', title: 'Кнопка 5', content: 'Мама тебя любит! P.S. Надень шапку' },
+  { id: 'tab-6', title: 'Кнопка 6', content: 'Освободи вечер для Гарри Поттера' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  onTabSelected = (tab: Tab) => {
+    this.setState({ selectedTab: tab });
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App App__container">
+        <h1 className="App__title">
+          Выбрана
+          {' '}
+          {selectedTab.title}
+        </h1>
+        <Tabs
+          tabs={tabs}
+          tabId={selectedTab.id}
+          onTabSelected={this.onTabSelected}
+        />
+        <div className="App__content">
+          {selectedTab.content}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
