6003940f4798e945fc70772bbd9c1d4b909a41cf
nadiaH <nadiahrb79@gmailcom>
2021-12-19 17:27:48 +0200

    comments del

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import tabs from './api/tabs';
 import './App.scss';
 import Tabs from './components/Tabs';
 
-// const tabs: Tab[] = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
 type State = {
   selectedTab: Tab;
 };
