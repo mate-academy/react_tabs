92ebbb639952a07a12cbb163ed141b0b9b31c89e
Samson Sim <samson.sim@ukr.net>
2022-01-26 14:13:08 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ class App extends React.Component<{}, State> {
           selectedTab={selectedTab}
           onChange={this.changeTab}
         />
-        <div className="content">{selectedTab.content}</div>
       </div>
     );
   }
--- src/components/TabsList.tsx
+++ src/components/TabsList.tsx
@@ -8,18 +8,21 @@ type Props = {
 
 export const TabsList: React.FC<Props> = ({ ourTabs, selectedTab, onChange }) => {
   return (
-    <ul className="tab__list">
-      {ourTabs.map(tab => (
-        <li key={tab.id} className="tab__item">
-          <button
-            type="button"
-            onClick={() => onChange(tab)}
-            className={selectedTab.id === tab.id ? 'selected__button button' : 'button'}
-          >
-            {tab.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="tab__list">
+        {ourTabs.map(tab => (
+          <li key={tab.id} className="tab__item">
+            <button
+              type="button"
+              onClick={() => onChange(tab)}
+              className={selectedTab.id === tab.id ? 'selected__button button' : 'button'}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="content">{selectedTab.content}</div>
+    </div>
   );
 };
