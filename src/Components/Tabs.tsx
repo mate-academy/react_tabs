aa54f88cb2f4523b918ee2fccd68e1d248c58130
Serhii Haidukov <sergeyglazgo@gmail.com>
2022-01-27 12:37:33 +0200

    removed return

--- src/Components/Tabs.tsx
+++ src/Components/Tabs.tsx
@@ -8,34 +8,30 @@ type Props = {
   onTabSelected: (tab: Tab) => void;
 };
 
-export const Tabs: React.FC<Props> = (props) => {
-  const {
-    tabs,
-    selectedTabId,
-    selectedTab,
-    onTabSelected,
-  } = props;
-
-  return (
-    <>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li key={tab.id} className={classNames({ 'is-active': selectedTabId === tab.id })}>
-              <a
-                href="#preventReload"
-                onClick={() => {
-                  onTabSelected(tab);
-                }}
-              >
-                <span className="icon is-small"><i className="fas fa-image" aria-hidden="true" /></span>
-                <span>{tab.title}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <p>{selectedTab.content}</p>
-    </>
-  );
-};
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  selectedTab,
+  onTabSelected,
+}) => (
+  <>
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li key={tab.id} className={classNames({ 'is-active': selectedTabId === tab.id })}>
+            <a
+              href="#preventReload"
+              onClick={() => {
+                onTabSelected(tab);
+              }}
+            >
+              <span className="icon is-small"><i className="fas fa-image" aria-hidden="true" /></span>
+              <span>{tab.title}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <p>{selectedTab.content}</p>
+  </>
+);
