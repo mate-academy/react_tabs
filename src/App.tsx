a38615da62c2ed635fee7329675d59fe72167b94
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2022-01-28 16:03:40 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Tabs } from './components/Tabs';
 import './App.scss';
 
 const tabs: Tab[] = [
@@ -8,11 +8,33 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  tab: Tab,
+};
+
+class App extends React.PureComponent<{}, State> {
+  state : State = {
+    tab: tabs[0],
+  };
+
+  changeTab = (id: string) => {
+    this.setState(() => ({
+      tab: tabs.find(el => el.id === id) || tabs[0],
+    }));
+  };
+
+  render() {
+    const { tab } = this.state;
+
+    return (
+      <div className="App">
+        <Tabs tabs={tabs} changeContent={this.changeTab} />
+        <h1>{ `Selected tab is ${tab.title}` }</h1>
+        <div>{tab.content}</div>
+
+      </div>
+    );
+  }
+}
 
 export default App;
