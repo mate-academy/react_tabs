284388da5b3a89898af2f1eade9478c29f55dc32
Alex Ivanov <bcmp4p1@gmail.com>
2022-01-28 21:00:14 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.scss';
+import { Tabs } from './Components/Tabs';
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +9,46 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type Props = {};
+type State = {
+  tabId: string,
+  title: string | null,
+};
+
+class App extends React.PureComponent<Props, State> {
+  state: State = {
+    tabId: 'tab-1',
+    title: 'Tab 1',
+  };
+
+  handlerButtonClick = (id: string) => {
+    let tabTitle;
+
+    if (tabs.find(tab => tab.id === id)) {
+      tabTitle = (tabs.find(tab => tab.id === id));
+    }
+
+    if (tabTitle !== undefined) {
+      this.setState({
+        tabId: id,
+        title: tabTitle.title || null,
+      });
+    }
+  };
+
+  render(): React.ReactNode {
+    const { tabId, title } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="app__title">
+          Selected tab is&nbsp;
+          {title}
+        </h1>
+        <Tabs allTabs={tabs} tabId={tabId} handleClick={this.handlerButtonClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
