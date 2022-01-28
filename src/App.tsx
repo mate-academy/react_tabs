3872574dcfdae29dbdff97d53c83f3793437571a
levix2l <alexleviz13@gmail.com>
2022-01-28 13:06:39 +0200

    react tabs solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,39 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type State = {
+  selectedTab: Tab;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    selectedTab: tabs[0],
+  };
+
+  selectTab = (tab: Tab) => (
+    this.setState({ selectedTab: tab })
+  );
+
+  render() {
+    return (
+      <div className="p-5">
+        <h1>
+          Selected tab is
+          {' '}
+          <span
+            className="tag is-info is-light"
+          >
+            {this.state.selectedTab.title}
+          </span>
+        </h1>
+        <Tabs
+          tabs={tabs}
+          selectTab={this.selectTab}
+          selectedTabId={this.state.selectedTab.id}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
