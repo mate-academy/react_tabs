2f00a321b7c2743176942eb722508809fe442789
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-01-30 17:23:37 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Tabs } from './Tabs';
 
 import './App.scss';
+import './Tabs.scss';
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,41 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type Props = {};
 
-export default App;
+type State = {
+  selectedTab: string,
+};
+
+const findTabTitle = (id: string) => tabs.find(tab => tab.id === id) || tabs[0];
+
+export class App extends React.Component<Props, State> {
+  state: State = {
+    selectedTab: 'tab-1',
+  };
+
+  handleSelector = (tab: string) => {
+    this.setState((
+      { selectedTab: tab }
+    ));
+  };
+
+  render() {
+    const { selectedTab } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          Selected tab is
+          {' '}
+          {findTabTitle(selectedTab).title}
+        </h1>
+        <Tabs
+          tabs={tabs}
+          selectedTabId={selectedTab}
+          handleSelector={this.handleSelector}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/Tabs.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  handleSelector: (tab: string) => void;
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, handleSelector }) => {
+  const findTabContent = () => tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div className="Tabs__tab">
+      {tabs.map(tab => (
+        <>
+          <button
+            key={tab.id}
+            type="button"
+            className={classNames('Tabs__button',
+              { 'Tabs__button--active': tab.id === selectedTabId })}
+            onClick={() => handleSelector(tab.id)}
+          >
+            {tab.title}
+          </button>
+          {' '}
+        </>
+      ))}
+      <p className="Tabs__text">
+        {findTabContent().content}
+      </p>
+    </div>
+  );
+};
