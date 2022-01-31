49a6d74be74c62af9dbf417c6dfd52a0e6f2123c
Iryna Zolko <koko_irina@ukr.net>
2022-01-31 11:55:03 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
+// import { render } from 'react-dom';
+// import { Tabs } from './components/Tabs/Tabs';
 
 import './App.scss';
 
@@ -8,11 +11,61 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+type Props = {};
+type State = {
+  copyTabs: Tab[];
+  selectedTab: string;
+  contentOfSelectedTab: string;
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    copyTabs: [...tabs],
+    selectedTab: 'Tab 1',
+    contentOfSelectedTab: 'Some text 1',
+  };
+
+  render() {
+    const { copyTabs, selectedTab, contentOfSelectedTab } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="selected-tab">{`Selected tab is ${selectedTab}`}</h1>
+        <ul className="nav">
+          {copyTabs.map(tab => (
+            <div>
+              <button
+                type="button"
+                key={tab.id}
+                className="nav__item"
+                onClick={() => {
+                  this.setState({
+                    selectedTab: `${tab.title}`,
+                    contentOfSelectedTab: `${tab.content}`,
+                  });
+                }}
+              >
+                {tab.title}
+              </button>
+              <span
+                className={
+                  classNames(
+                    'content-hidden',
+                    { 'content-visible': contentOfSelectedTab === tab.content },
+                  )
+                }
+              >
+                {tab.content}
+              </span>
+            </div>
+          ))}
+        </ul>
+        {/* <Tabs copyTabs={[...tabs]} /> */}
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// &nbsp;
