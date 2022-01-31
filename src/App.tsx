8db4b3f63f839c5ae6eb95dca4c54a5a39a6c179
Iryna Zolko <koko_irina@ukr.net>
2022-01-31 16:31:28 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import classNames from 'classnames';
 // import { render } from 'react-dom';
-// import { Tabs } from './components/Tabs/Tabs';
+import { Tabs } from './components/Tabs/Tabs';
 
 import './App.scss';
 
@@ -13,59 +12,37 @@ const tabs: Tab[] = [
 
 type Props = {};
 type State = {
-  copyTabs: Tab[];
   selectedTab: string;
   contentOfSelectedTab: string;
 };
 
 class App extends React.Component<Props, State> {
   state: State = {
-    copyTabs: [...tabs],
     selectedTab: 'Tab 1',
     contentOfSelectedTab: 'Some text 1',
   };
 
+  handleTabChange = (tab: Tab) => {
+    this.setState({
+      selectedTab: `${tab.title}`,
+      contentOfSelectedTab: `${tab.content}`,
+    });
+  };
+
   render() {
-    const { copyTabs, selectedTab, contentOfSelectedTab } = this.state;
+    const { selectedTab, contentOfSelectedTab } = this.state;
 
     return (
       <div className="App">
         <h1 className="selected-tab">{`Selected tab is ${selectedTab}`}</h1>
-        <ul className="nav">
-          {copyTabs.map(tab => (
-            <div>
-              <button
-                type="button"
-                key={tab.id}
-                className="nav__item"
-                onClick={() => {
-                  this.setState({
-                    selectedTab: `${tab.title}`,
-                    contentOfSelectedTab: `${tab.content}`,
-                  });
-                }}
-              >
-                {tab.title}
-              </button>
-              <span
-                className={
-                  classNames(
-                    'content-hidden',
-                    { 'content-visible': contentOfSelectedTab === tab.content },
-                  )
-                }
-              >
-                {tab.content}
-              </span>
-            </div>
-          ))}
-        </ul>
-        {/* <Tabs copyTabs={[...tabs]} /> */}
+        <Tabs
+          copyTabs={[...tabs]}
+          handleTabChange={this.handleTabChange}
+          content={contentOfSelectedTab}
+        />
       </div>
     );
   }
 }
 
 export default App;
-
-// &nbsp;
