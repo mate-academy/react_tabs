7bde2239fbda62b2b56954f56329b2f8d0ea2608
AndryKK <korbutiaktso@gmail.com>
2022-02-23 20:58:04 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
 import './App.scss';
+import { Tab } from './components/Tab';
 
-const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
-
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Tab />
+    </div>
+  );
+};
 
 export default App;
