90bac79ed5ff73872a4867b0b903f995ae9d3c1a
AndryKK <korbutiaktso@gmail.com>
2022-03-07 11:03:14 +0200

    Solution

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import { tabs } from '../tabs';
 
 export const Tab: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [selectedTab, setSelectedTab] = useState(tabs.find(x => x.id === 'tab-1'));
 
   return (
     <div>
       <h1>
         Selected tab is
-        {` ${selectedTab.title}`}
+        {selectedTab && (` ${selectedTab.title}`)}
       </h1>
       <div className="column">
         <div className="text">
-          {selectedTab.content}
+          {selectedTab && (selectedTab.content)}
         </div>
         <ul>
           {tabs.map(tab => (
@@ -22,8 +22,8 @@ export const Tab: React.FC = () => {
             >
               <button
                 type="button"
-                className="button"
-                onClick={() => (setSelectedTab(tabs[+tab.id.slice(-1) - 1]))}
+                className={selectedTab && (selectedTab.id === tab.id ? 'button active' : 'button')}
+                onClick={() => (setSelectedTab(tabs.find(x => x.id === tab.id)))}
               >
                 {tab.title}
               </button>
