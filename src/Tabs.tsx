758fd56c9a2ee0b83462c2553843f7e59b871e2b
maxim2310 <m23a09x96@gmail.com>
2022-03-15 21:57:51 +0200

    TabsIsChanged

---
+++ src/Tabs.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Tab } from './react-app-env';
+
+type Props = {
+  tabs: Tab[]
+  selected: Tab
+  onChanger: (target: Tab) => void
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selected, onChanger }) => {
+  return (
+    <>
+      <div>
+        {tabs.map(tab => (
+          <button
+            type="button"
+            onClick={() => {
+              if (tab.id !== selected.id) {
+                onChanger(tab);
+              }
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      <p>
+        {selected.content}
+      </p>
+    </>
+  );
+};
