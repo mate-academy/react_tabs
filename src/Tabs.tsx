81a4a7aed788665e8b2a2df498e8541a624abcf0
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-04-03 18:10:04 +0300

    Selecting tabs

---
+++ src/Tabs.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onSelect: (tab: Tab) => void,
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onSelect }) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+  const selectTab = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onSelect(tab);
+    }
+  };
+
+  return (
+    <>
+      <div className="tabs">
+        <ul>
+          {tabs.map((tab) => {
+            return (
+              <li
+                key={tab.id}
+              >
+                <button
+                  className={classNames('button', { 'is-info': selectedTabId === tab.id })}
+                  type="button"
+                  onClick={() => selectTab(tab)}
+                >
+                  {tab.title}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="content">
+        {selectedTab?.content}
+      </div>
+    </>
+  );
+};
