63d1182c123d513a275701b2075bfe580c0cca60
Anton Chernokulskyi <ton7.ok@icloud.com>
2022-04-04 17:32:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-
+import React, { useCallback, useState } from 'react';
 import './App.scss';
+import { Tabs } from './components/Tabs';
+import { Tab } from './types';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tabs 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tabs 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tabs 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+export const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState<Tab>(tabs[0]);
+  const selectTab = useCallback((tab: Tab) => {
+    setSelectedTab(tab || tabs[0]);
+  }, []);
 
-export default App;
+  return (
+    <div className="App">
+      <h1>
+        Selected tab is&nbsp;
+        <span className="selected-tab">{`${selectedTab.title}`}</span>
+      </h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        selectTab={selectTab}
+      />
+      <span>{selectedTab.content}</span>
+    </div>
+  );
+};
