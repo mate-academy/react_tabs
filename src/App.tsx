707dfee19162aa40c3f14619ca03b29e799eb766
Max Shlikhta <maxok.vibe@gmail.com>
2022-04-05 23:20:43 +0300

    Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './components/Tabs';
+import { tabs } from './source/tabs';
 
 import './App.scss';
 
-const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+export const App: React.FC = React.memo(() => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+  return (
+    <div className="app">
+      <h1 className="app__title">
+        {`Selected tab is ${activeTab.title}`}
+      </h1>
 
-export default App;
+      <Tabs
+        tabs={tabs}
+        activeTabId={activeTab.id}
+        onActiveTab={setActiveTab}
+      />
+    </div>
+  );
+});
