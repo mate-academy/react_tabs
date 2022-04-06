7411d55dd0430afe60a6d0d54e79b912da83a06b
Roman Khemych <roman19940912@gmail.com>
2022-04-06 09:34:44 +0300

    React Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.scss';
+import { Tabs } from './tabs';
+
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +15,25 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectTab, setSelectTab] = useState(tabs[0]);
+  const onTabSelected = useCallback((tab: Tab) => {
+    setSelectTab(tab || tabs[0]);
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Selected tab is&nbsp;
+        {selectTab.title}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        onTabSelected={onTabSelected}
+        selectedTabId={selectTab.id}
+      />
+    </>
+  );
+};
 
 export default App;
