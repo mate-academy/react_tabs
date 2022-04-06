edf3fd13cb7d8719e2ebc9e06a00a6656335fb58
Vlad Brynzya <vladbrynzya@gmail.com>
2022-04-06 20:37:35 +0300

    solution1

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Props = {
+  tabs: Tab[],
+  onSelect: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, onSelect }) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li key={tab.id}>
+          <button
+            type="button"
+            onClick={() => onSelect(tab)}
+          >
+            {tab.title}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
