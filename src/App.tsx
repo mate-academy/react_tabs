cfaa2b3b2cc99ddaf532f8797e02ad3444ea16b3
Juliya <jursylian@gmail.com>
2022-04-06 18:38:46 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-
+import { useState } from 'react';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Statistic', content: 'Ежегодно люди на Земле покупают шоколад примерно на сумму в семь миллиардов долларов США.' },
+  { id: 'tab-2', title: 'Legend', content: 'Когда-то давно, существовал прекрасный сад, окутанный временем. В этом саду жили боги, а хранителями сада были первые люди, мужчина и женщина. Они обрабатывали землю и жили в гормонии со всеми стихиями — землей, водой и воздухом.' },
+  { id: 'tab-3', title: 'Fact', content: 'Впервые твёрдый шоколад был получен в 19-м веке. До этого момента он был только жидким.' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  return (
+    <div className="App">
+      <h1 className="title has-text-centered mb-5 mt-3">
+        Selected tab is
+        {' '}
+        {selectedTab.title}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        onTabSelected={setSelectedTab}
+      />
+    </div>
+  );
+};
 
 export default App;
