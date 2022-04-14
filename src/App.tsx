7d5d00aa16913e3b620952220ca980571e4f7ac3
Inna <inna_temis@ukr.net>
2022-04-14 13:11:40 +0300

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './Tabs';
 
 import './App.scss';
 
@@ -8,11 +9,22 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const onSelected = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className="App">
+      <Tabs
+        tabs={tabs}
+        selectedTab={selectedTab}
+        onSelected={onSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
