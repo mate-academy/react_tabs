232983caf33c3c73e7be34f3695822bdc2493c92
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2022-04-27 20:58:50 +0300

    Implement Tabs Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
-
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import './App.scss';
+import { Tabs } from './Tabs';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Home', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
+  { id: 'tab-2', title: 'Profile', content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.' },
+  { id: 'tab-3', title: 'Contact', content: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const selectTab = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        Selected tab:
+        {' '}
+        {selectedTab.title}
+      </h1>
+
+      <Tabs
+        allTabs={tabs}
+        tabSelected={selectedTab}
+        onTabSelected={selectTab}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Tabs.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames';
+import React from 'react';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  allTabs: Tab[];
+  tabSelected: Tab;
+  onTabSelected: CallableFunction;
+};
+
+export const Tabs: React.FC<Props> = ({
+  allTabs,
+  tabSelected,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      <div className="tabs">
+        {allTabs.map(tab => (
+          <button
+            className={classNames(
+              'tabs__item',
+              { tabs__item__active: tabSelected === tab },
+            )}
+            key={tab.id}
+            type="button"
+            onClick={() => {
+              onTabSelected(tab);
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      <div className="tab__content">
+        <p>{tabSelected.content}</p>
+      </div>
+    </>
+
+  );
+};
