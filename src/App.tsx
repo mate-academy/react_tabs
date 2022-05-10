dbdc4ae9229675c8e2c2c55925afc623851a0555
Eugene Khablenko <eugene.khablenko@gmail.com>
2022-05-10 15:39:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
 const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  {
+    id: 'home',
+    title: 'Home',
+    content: 'Welcome to the Mate Academy home page',
+  },
+  {
+    id: 'about',
+    title: 'About',
+    content: 'We are Mate Academy',
+  },
+  {
+    id: 'company',
+    title: 'Profile',
+    content: 'We teach React, JS, Python and many more',
+  },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState('home');
+  const [selectedTabTitle, setSelectedTabTitle] = useState('Home');
+
+  const handleTabSelect = (tab: Tab) => {
+    setSelectedTabId(tab.id);
+    setSelectedTabTitle(tab.title);
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        {'Selected tab is: '}
+        {selectedTabTitle}
+      </h1>
+
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        onTabSelect={handleTabSelect}
+      />
+
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelect: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs, selectedTabId, onTabSelect,
+}) => {
+  return (
+    <>
+      <ul className="tab__list">
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              className={classNames(
+                'tab__item',
+                { 'tab__item--active': tab.id === selectedTabId },
+              )}
+              onClick={() => onTabSelect(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="tab__content">
+        {tabs.map(tab => (tab.id === selectedTabId && tab.content))}
+      </div>
+    </>
+  );
+};
