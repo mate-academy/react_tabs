6c77e41cf3f6c81ba23eda8ff46625348cda78f4
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-11 17:26:54 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs } from './components/Tabs';
 
 import './App.scss';
 
@@ -8,11 +9,27 @@ const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTab] = useState(tabs[0].id);
+
+  return (
+    <div className="App">
+      <h1 className="App__title">
+        Selected tab is&nbsp;
+        {tabs.find(tab => tab.id === selectedTabId)?.title}
+      </h1>
+
+      <div className="App__counter">
+        {`Total tabs ${tabs.length}`}
+      </div>
+
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        setSelectedTab={setSelectedTab}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  setSelectedTab: (id: string) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  setSelectedTab,
+}) => {
+  return (
+    <div className="tabs-list">
+      {tabs.map(tab => (
+        <div
+          key={tab.id}
+          className="tab"
+        >
+          <button
+            type="button"
+            onClick={() => setSelectedTab(tab.id)}
+            className={`tab__button ${selectedTabId === tab.id ? 'activated' : 'deactivated'}`}
+          >
+            {tab.title}
+          </button>
+
+          {selectedTabId === tab.id && (
+            <p className="tab__content">
+              {tab.content}
+            </p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
