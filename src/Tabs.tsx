6b3332fd2d82f6c457428e81f824c8c697f94999
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-05-18 23:44:36 +0200

    react_tabs

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,33 @@
-// implement a component here
+import React from 'react';
+
+type Callback = (currentTab: Tab) => void;
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: Callback,
+};
+
+export const Tab: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId)
+    || tabs[0];
+
+  return (
+    <>
+      {tabs.map(tab => (
+        <button
+          type="button"
+          key={tab.id}
+          onClick={() => onTabSelected(tab)}
+        >
+          {tab.title}
+        </button>
+      ))}
+      <p data-cy="tab-content">{selectedTab.content}</p>
+    </>
+  );
+};
