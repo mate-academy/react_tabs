93a1fb5cc73067ee3fecb93668e8d1b583bea4ab
Ihor Mural <igor201278789887000@gmail.com>
2022-05-19 09:54:00 +0300

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,36 @@
-// implement a component here
+import React from 'react';
+import './App.scss';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  setSelectedTab: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  setSelectedTab,
+}) => {
+  return (
+    <ul className="list">
+      {
+        tabs.map(tab => (
+          <li key={tab.id} className="list__item">
+            <button
+              type="button"
+              onClick={() => setSelectedTab(tab)}
+              className="list__button"
+            >
+              {tab.title}
+            </button>
+
+            {selectedTabId === tab.id && (
+              <p className="list__content">{tab.content}</p>
+            )}
+          </li>
+        ))
+      }
+    </ul>
+  );
+};
