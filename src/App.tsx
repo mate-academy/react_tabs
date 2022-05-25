8fb96e25ad3dac219abe200972411ccdfb03c01c
unknown <avicostov2@gmail.com>
2022-05-25 22:34:41 +0200

    solut

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tabs from './components/Tabs';
 
 import './App.scss';
 
@@ -8,11 +9,33 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, changeTab] = useState(tabs[0]);
+
+  const onTabSelected = (tabsItem: Tab) => {
+    const findTab = tabs.find(tab => tab.id === tabsItem.id) || tabs[0];
+
+    changeTab(findTab);
+  };
+
+  return (
+    <div className="App">
+      <h1
+        style={{ color: 'green' }}
+      >
+        Selected tab is
+        <b style={{ color: 'blue' }}>
+          &nbsp;
+          {selectedTab.title}
+        </b>
+      </h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        onTabSelected={onTabSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
