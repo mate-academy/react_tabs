fd32263d9145a19cfbb04d71bd012b11b5a1b38e
Oleksandr <pogrebnyak19999@gmail.com>
2022-05-29 22:01:56 +0200

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,54 @@
-// implement a component here
+import React from 'react';
+import ClassNames from 'classnames';
+
+type Props = {
+  tabs: Tab[],
+  selectedTab: Tab,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab,
+  onTabSelected,
+}) => {
+  const changeTabId = (tab: Tab) => {
+    if (selectedTab.id !== tab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div className="tabs">
+      <div className="tabs__nav">
+        {tabs.map(tab => (
+          <div className={ClassNames(
+            'tabs__navItem',
+            { tabs__navItem_active: tab.id === selectedTab.id },
+          )}
+          >
+            <button
+              type="button"
+              onClick={() => changeTabId(tab)}
+            >
+              {tab.title}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="tabs__content">
+        {tabs.map(tab => (
+          <div
+            className={ClassNames(
+              'tabs__contentItem',
+              { tabs__contentItem_active: tab.id === selectedTab.id },
+            )}
+            data-cy="tab-content"
+          >
+            {tab.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
