3294346ef8a4b4e364012673a7e5e2ca1454fa60
Den <81646849+DenCoderok@users.noreply.github.com>
2022-05-31 18:55:12 +0300

    commit

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,48 @@
-// implement a component here
+import React from 'react';
+import './App.scss';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs:React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      <div className="tabs__wrapper">
+        {tabs.map(tab => {
+          const { id, title } = tab;
+
+          return (
+            <button
+              type="button"
+              className={
+                id === selectedTabId
+                  ? 'isSelectedTab'
+                  : 'tab'
+              }
+              key={id}
+              onClick={() => {
+                onTabSelected(tab);
+              }}
+            >
+              {title}
+            </button>
+          );
+        })}
+      </div>
+
+      <div
+        className="content"
+        data-cy="tab-content"
+      >
+        {tabs.find(elem => elem.id === selectedTabId)?.content}
+      </div>
+    </>
+  );
+};
