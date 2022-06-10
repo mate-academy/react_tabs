50e309e939a054cdad1d76d7d774372d14add0da
Valerii Derkach <valerij.derkach@gmail.com>
2022-06-10 19:48:44 +0300

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+// eslint-disable-next-line import/no-cycle
+import { Tabs } from './Tabs';
 
 import './App.scss';
 
@@ -8,11 +10,37 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+export const findTabByID = (tabId: string): Tab | null => {
+  return tabs.find(tab => tab.id === tabId) || null;
+};
+
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+
+  const onTabSelected = (id: string) => {
+    setSelectedTabId(id);
+  };
+
+  const selectedTab = findTabByID(selectedTabId) || tabs[0];
+
+  return (
+    <main
+      className="
+       App box
+       is-flex
+       is-flex-direction-column"
+    >
+      <h1 className="title is-2">
+        {`Selected tab is ${selectedTab.title}`}
+      </h1>
+
+      <Tabs
+        tabs={tabs}
+        selector={onTabSelected}
+        selectedTabId={selectedTabId}
+      />
+    </main>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,61 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+// eslint-disable-next-line import/no-cycle
+import { findTabByID } from './App';
+
+interface Props {
+  tabs: Tab[],
+  selector: (id: string) => void,
+  selectedTabId: string,
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selector,
+  selectedTabId,
+}) => {
+  const selectedTab = findTabByID(selectedTabId) || tabs[0];
+
+  return (
+    <section className="box">
+      <div
+        className="
+          buttons
+          is-flex
+          is-flex-direction-row"
+      >
+        {tabs.map(tab => (
+          <button
+            key={tab.id}
+            className={classNames('button', {
+              'is-outlined': tab.id !== selectedTabId,
+              'is-link': tab.id === selectedTabId,
+            })}
+            type="button"
+            onClick={() => {
+              if (selectedTabId !== tab.id) {
+                selector(tab.id);
+              }
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
+        <progress
+          className="
+            progress
+            is-link
+            is-small"
+          value="1"
+          max="1"
+        />
+      </div>
+      <p
+        className="subtitle is-3"
+        data-cy="tab-content"
+      >
+        {selectedTab.content}
+      </p>
+    </section>
+  );
+};
