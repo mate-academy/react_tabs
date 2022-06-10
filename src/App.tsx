09a82754bc3b74f226d87c2bc41cbcbdb90ef9cd
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-11 00:04:13 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Tab } from './react-app-env';
+import { Tabs } from './Tabs';
 import './App.scss';
 
 export const tabs: Tab[] = [
@@ -8,11 +9,32 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState('tab-1');
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const idSelect = (tab: Tab): void => {
+    setSelectedTabId(tab.id);
+  };
+
+  const findTabById = (id: string): void => {
+    const currentTab = tabs.find(tab => tab.id === id) || tabs[0];
+
+    setSelectedTab(currentTab);
+  };
+
+  return (
+    <div className="App">
+      <h1>{`Selected tab is ${selectedTab.title}`}</h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        selectedTab={selectedTab}
+        onTabSelected={idSelect}
+        findTabById={findTabById}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,40 @@
-// implement a component here
+import React, { useEffect } from 'react';
+import { Tab } from './react-app-env';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string;
+  selectedTab: Tab;
+  onTabSelected: (tab: Tab) => void;
+  findTabById: (id: string) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  selectedTab,
+  onTabSelected,
+  findTabById,
+}) => {
+  useEffect(() => {
+    findTabById(selectedTabId);
+  }, [selectedTabId]);
+
+  return (
+    <>
+      <div className="tabs">
+        {tabs.map(tab => (
+          <button
+            type="button"
+            onClick={() => onTabSelected(tab)}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      <div className="tab-content" data-cy="tab-content">
+        {selectedTab.content}
+      </div>
+    </>
+  );
+};
