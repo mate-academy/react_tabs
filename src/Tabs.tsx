4d64d2337aa7537b5995770b0dd32c50c4647b1c
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-06-11 15:58:01 +0300

    solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,45 @@
-// implement a component here
+import React from 'react';
+import { Tab, OnTabSelected, PrintContent } from './react-app-env';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: OnTabSelected
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const printContent: PrintContent = (tabId) => {
+    return tabs.find(tab => tab.id === tabId)?.content;
+  };
+
+  return (
+    <>
+      <div className="buttons has-addons is-centered">
+        {tabs.map(tab => (
+          <button
+            className="button is-success is-rounded"
+            key={tab.id}
+            type="button"
+            onClick={() => {
+              onTabSelected(tab);
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+
+      <div
+        className="content"
+        data-cy="tab-content"
+      >
+        {printContent(selectedTabId)}
+      </div>
+
+    </>
+  );
+};
