9059cbf80736a23babaaab638f222eb88ddcc2d4
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-06-12 19:21:49 +0300

    tabs on react1.1

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -15,37 +15,35 @@ export const Tabs: React.FC<Props> = ({
   selectedTabId,
   content,
   onTabSelected,
-}) => {
-  return (
-    <div className="tab__box m-6">
-      <div className="tab__buttons-box is-flex-direction-column">
-        {tabs.map((tab: Tab) => {
-          return (
-            <div
-              key={tab.id}
-              data-cy="tab-content"
-              className={classNames(
-                'tab__buttons',
-                {
-                  active: tab.content === content,
-                },
-              )}
+}) => (
+  <div className="tab__box m-6">
+    <div className="tab__buttons-box is-flex-direction-column">
+      {tabs.map((tab: Tab) => {
+        return (
+          <div
+            key={tab.id}
+            data-cy="tab-content"
+            className={classNames(
+              'tab__buttons',
+              {
+                active: tab.content === content,
+              },
+            )}
+          >
+            <button
+              type="button"
+              className="has-text-info button is-white"
+              onClick={() => {
+                selectedTabId(tab.id);
+                onTabSelected(tab.id);
+              }}
             >
-              <button
-                type="button"
-                className="has-text-info button is-white"
-                onClick={() => {
-                  selectedTabId(tab.id);
-                  onTabSelected(tab.id);
-                }}
-              >
-                {tab.title}
-              </button>
-            </div>
-          );
-        })}
-      </div>
-      <div className="tab__subtitle subtitle">{content}</div>
+              {tab.title}
+            </button>
+          </div>
+        );
+      })}
     </div>
-  );
-};
+    <div className="tab__subtitle subtitle">{content}</div>
+  </div>
+);
