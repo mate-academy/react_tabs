32f688b3246527592792337610ec6bcb2cf7c509
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-06-13 11:59:58 +0300

    made tabs select

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './App.scss';
+import { Tab } from './react-app-env';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,16 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const tabsCopy = [...tabs];
+
+  return (
+    <div className="App">
+      <Tabs
+        tabs={tabsCopy}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,48 @@
-// implement a component here
+import React, { useState } from 'react';
+import { Tab } from './react-app-env';
+
+interface Props {
+  tabs: Tab[]
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+}) => {
+  const [selected, setSelected] = useState(tabs[0]);
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        Selected tab is&nbsp;
+        {selected.title}
+      </h1>
+
+      {tabs.map(tab => (
+        <button
+          type="button"
+          value={tab.id}
+          className={
+            tab.id === selected.id
+              ? 'container__button--focus container__button'
+              : 'container__button'
+          }
+          key={tab.id}
+          onClick={() => {
+            setSelected(tab);
+          }}
+        >
+          {tab.title}
+        </button>
+      ))}
+      <input
+        type="text"
+        className="container__border"
+        disabled
+      />
+
+      <p className="content">
+        {selected.content}
+      </p>
+    </div>
+  );
+};
