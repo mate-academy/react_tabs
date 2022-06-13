27f01fff983302d4bc12acd473727359d18d2789
Vicky Miziukanova <enrikadej@gmail.com>
2022-06-13 13:46:31 +0300

    task without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Tabs } from './Tabs';
 import './App.scss';
 
+import { Tab } from './react-app-env';
+
 export const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Home', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Profile', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Contact', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setselectedTab] = useState(tabs[0]);
+
+  const onTabSelected = (tab: Tab) => {
+    setselectedTab(tab);
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        Selected tab is&nbsp;
+        {selectedTab.title}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        onTabSelected={onTabSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
