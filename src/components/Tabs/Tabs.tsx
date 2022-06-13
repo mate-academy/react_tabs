a22baeb0d05518e20121f83e6e98bb652d8da946
Daria Liubymova <dellachaise@gmail.com>
2022-06-13 15:23:32 +0300

    Refactored code acording to comment

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -8,33 +8,34 @@ type Props = {
   onTabSelected: (tabId: string) => void,
 };
 
-export const Tabs: React.FC<Props>
-  = ({ allTabs, selectedTabId, onTabSelected }) => {
-    const clickHandler = (tabId: string) => {
-      if (tabId !== selectedTabId) {
-        onTabSelected(tabId);
-      }
-    };
-
-    return (
-      <ul className="tabs-list">
-        {allTabs.map(tab => (
-          <React.Fragment key={tab.id}>
-            <li className={classNames(
-              'tabs-list__item',
-              { 'tabs-list__item--selected': tab.id === selectedTabId },
-            )}
-            >
-              <a
-                href={`#${tab.title}`}
-                onClick={() => clickHandler(tab.id)}
-                className="tabs-list__link"
-              >
-                {tab.title}
-              </a>
-            </li>
-          </React.Fragment>
-        ))}
-      </ul>
-    );
+export const Tabs: React.FC<Props> = ({
+  allTabs, selectedTabId, onTabSelected,
+}) => {
+  const clickHandler = (tabId: string) => {
+    if (tabId !== selectedTabId) {
+      onTabSelected(tabId);
+    }
   };
+
+  return (
+    <ul className="tabs-list">
+      {allTabs.map(tab => (
+        <React.Fragment key={tab.id}>
+          <li className={classNames(
+            'tabs-list__item',
+            { 'tabs-list__item--selected': tab.id === selectedTabId },
+          )}
+          >
+            <a
+              href={`#${tab.title}`}
+              onClick={() => clickHandler(tab.id)}
+              className="tabs-list__link"
+            >
+              {tab.title}
+            </a>
+          </li>
+        </React.Fragment>
+      ))}
+    </ul>
+  );
+};
