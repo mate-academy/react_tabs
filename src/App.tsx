abb8dc4c183d5fa26661e7823b14cb27b5402d37
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-14 11:11:13 +0300

    Selected tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.scss';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +8,32 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const onTabSelect = (tabId: string) => {
+    const currentSelectedTab = tabs.find(tab => tab.id === tabId);
+
+    setSelectedTabId(tabId);
+    setSelectedTab(currentSelectedTab || tabs[0]);
+  };
+
+  return (
+    <div data-cy="tab-content">
+      <h1>
+        Selected tab:
+        {selectedTabId}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        onTabSelect={onTabSelect}
+      />
+      <div>
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,22 @@
 // implement a component here
+import React from 'react';
+
+interface Props {
+  tabs: Tab[],
+  onTabSelect: (tabId: string) => void,
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, onTabSelect }) => {
+  return (
+    <>
+      {tabs.map((tab: Tab) => (
+        <button
+          type="button"
+          onClick={() => onTabSelect(tab.id)}
+        >
+          {tab.title}
+        </button>
+      ))}
+    </>
+  );
+};
