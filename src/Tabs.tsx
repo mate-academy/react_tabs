009d022071f15afe6b8259a9c77c00328b2cdf20
Ihor Yeremenko <igoree86@gmail.com>
2022-06-15 09:38:16 +0300

    Without_css

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,36 @@
-// implement a component here
+import React from 'react';
+
+interface Props {
+  selectedTabId: string,
+  onTabSelected: (id: string) => void,
+  tabs: Tab[],
+  selectedTab: Tab,
+}
+
+export const Tabs: React.FC<Props> = ({
+  selectedTabId,
+  onTabSelected,
+  tabs,
+  selectedTab,
+}) => {
+  return (
+    <div>
+      <div>
+        {tabs.map(tab => (
+          <button
+            key={tab.id}
+            type="button"
+            onClick={() => {
+              if (selectedTabId !== tab.id) {
+                onTabSelected(tab.id);
+              }
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      {selectedTab.content}
+    </div>
+  );
+};
