a14e1ef7e594024ffed824b6722e6577c6dcafdf
Danylo-Dziuban <dz.danylo@gmail.com>
2022-06-16 14:09:27 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState } from 'react';
 
 import './App.scss';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +11,28 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const onTabSelected = (id: string) => (setSelectedTabId(id));
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div className="App">
+      <h1>Selected tab is&nbsp;{selectedTab.title}</h1>
+      <div>{tabs.length}</div>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        onSelect={onTabSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
