197a2020bc04403ffb7df68ca3894f8a68adb713
Swazi Kunene <kuneneswazi18@gmail.com>
2022-06-21 13:35:35 +0200

    React Tabs Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Tab } from './react-app-env';
 import './App.scss';
+import { Tabs } from './Tabs';
+import 'bulma/css/bulma.min.css';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,28 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [currentTabs] = useState(tabs);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const selectHandler = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className="App box has-text-centered">
+      <h1 className="title is-1">
+        Selected tab is&nbsp;
+        {tabs.find(tab => tab.id === selectedTab.id)?.title}
+      </h1>
+
+      <Tabs
+        tabs={currentTabs}
+        onSelect={selectHandler}
+        selectedTabId={selectedTab.id}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,41 @@
-// implement a component here
+import { Tab } from './react-app-env';
+import './Tabs.scss';
+import 'bulma/css/bulma.min.css';
+
+interface Props {
+  tabs: Tab[];
+  onSelect: (tab: Tab) => void;
+  selectedTabId: string;
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, onSelect, selectedTabId }) => {
+  const printContent = (tabId: string) => {
+    return tabs.find(tab => tab.id === tabId)?.content;
+  };
+
+  return (
+    <>
+      <ul className="is-flex is-justify-content-center my-6">
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              className="button is-info is-large"
+              onClick={() => onSelect(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        className="content mt-6 py-6 is-size-3
+        has-text-white has-background-dark"
+        data-cy="tab-content"
+      >
+        {printContent(selectedTabId)}
+      </div>
+    </>
+  );
+};
