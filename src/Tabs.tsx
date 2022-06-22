780f5a2fa18610b8e7532cd1c3da847dc949794b
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-06-22 15:02:00 +0200

    solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,35 @@
 // implement a component here
+import { FC } from 'react';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+}
+export const Tabs: FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => (
+  <div>
+    {tabs.map(tab => (
+      <button
+        type="button"
+        key={tab.id}
+        onClick={() => {
+          if (selectedTabId !== tab.id) {
+            onTabSelected(tab);
+          }
+        }}
+      >
+        {tab.title}
+      </button>
+    ))}
+    <div
+      data-cy="tab-content"
+    >
+      <p>{tabs.find(tab => selectedTabId === tab.id)?.content}</p>
+    </div>
+
+  </div>
+);
