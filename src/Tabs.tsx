c40608fa93bb8900b73b5e2791ebfa0053e1f0da
Lebogang Mogane <mogane420@gmail.com>
2022-06-22 07:38:03 +0200

    solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,37 @@
-// implement a component here
+import React from 'react';
+import { Tab } from './react-app-env';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <div>
+      {tabs.map(tab => (
+        <React.Fragment key={tab.id}>
+          <button
+            type="button"
+            onClick={() => {
+              if (selectedTabId === tab.id) {
+                return;
+              }
+
+              onTabSelected(tab);
+            }}
+          >
+            {tab.title}
+          </button>
+          {tab.id === selectedTabId
+          && <p data-cy="tab-content">{tab.content}</p>}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
