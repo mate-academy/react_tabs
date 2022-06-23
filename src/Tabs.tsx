6361f9048a319ac14eee90f55f3bdb543ed6d8ca
Karina Prozner <prozner.karina@gmail.com>
2022-06-23 23:21:00 +0200

    tried hard to understand tests

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,32 @@
-// implement a component here
+interface Props {
+  tabs: Tab[];
+  onSelect: (tab: Tab) => void;
+  selectedTabId: string;
+}
+
+export const Tabs: React.FC<Props> = ({ tabs, onSelect, selectedTabId }) => {
+  const printContent = (tabId: string) => {
+    return tabs.find(tab => tab.id === tabId)?.content;
+  };
+
+  return (
+    <>
+      <ul>
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              onClick={() => onSelect(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div data-cy="tab-content">
+        {printContent(selectedTabId)}
+      </div>
+    </>
+  );
+};
