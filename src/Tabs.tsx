d23d74ae2f6e28ebc94442b498d885036547d69d
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-25 15:34:16 +0300

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,41 @@
-// implement a component here
+import React from 'react';
+import './Tabs.scss';
+import classnames from 'classnames';
+
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tabs: Tab[],
+  selected: Tab,
+  onTabSelected: (selectedTab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selected, onTabSelected }) => {
+  return (
+    <>
+      <div className="buttons is-centered container">
+        {tabs.map(tab => (
+          <button
+            key={tab.id}
+            type="button"
+            className={classnames('button is-rounded', {
+              'button__is-active': selected === tab,
+            })}
+            onClick={tab === selected ? undefined : () => onTabSelected(tab)}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      <div className="container container-content">
+        <p className="content" data-cy="tab-content">
+          {selected.content}
+        </p>
+      </div>
+    </>
+  );
+};
