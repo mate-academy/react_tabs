631e77e9156be22ee97a986180072846579c3cd8
Mfano <mabuzamfano@gmail.com>
2022-06-29 13:28:33 +0100

    Implemented suggested changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tabs } from './Tabs';
 import 'bulma/css/bulma.min.css';
 import './App.scss';
@@ -9,10 +9,28 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App level-item has-text-centered">
-    <Tabs tabs={tabs} />
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const selectTab = (tab: Tab) => {
+    return setSelectedTab(tab);
+  };
+
+  return (
+    <div className="App level-item has-text-centered">
+      <h1 className="title is-1">
+        Selected tab is&nbsp;
+        {selectedTab.title}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        onTabSelected={selectTab}
+      />
+      <section className="App__Content">
+        <p>{selectedTab.content}</p>
+      </section>
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,43 +1,29 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 interface TabsProps {
-  tabs: Tab[],
+  tabs: Tab[];
+  onTabSelected: (tab: Tab) => void;
 }
 
-export const Tabs: FC<TabsProps> = ({ tabs }) => {
-  const [selectedTab, setSelectedTab] = useState('tab-1');
-  const [selectedContent, setSelectedContent] = useState('Some text 1');
-
-  return (
-    <>
-      <h1 className="title is-1">
-        Selected tab is&nbsp;
-        {selectedTab}
-      </h1>
-      <div className="tabs is-centered is-toggle is-large App__Tab">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              className="is-active"
-              key={tab.id}
-            >
-              <a
-                href="/"
-                onClick={(event) => {
-                  event.preventDefault();
-                  setSelectedTab(tab.id);
-                  setSelectedContent(tab.content);
-                }}
-              >
-                <span>{tab.title}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <section className="App__Content">
-        <p data-cy="tab-content">{selectedContent}</p>
-      </section>
-    </>
-  );
-};
+export const Tabs: FC<TabsProps> = ({ onTabSelected, tabs }) => (
+  <div className="tabs is-centered is-toggle is-large App__Tab">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className="is-active"
+          key={tab.id}
+        >
+          <a
+            href="/"
+            onClick={(event) => {
+              event.preventDefault();
+              onTabSelected(tab);
+            }}
+          >
+            <span>{tab.title}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
