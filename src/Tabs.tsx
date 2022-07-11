0b0f55416b6f4eefdefdebbfe987d5fa2a3cf1e0
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-07-11 17:28:49 +0300

    Tabs added

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,42 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from './react-app-env';
+
+type Props = {
+  tabs: Tab[],
+  selectedTab: Tab,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = React.memo(({
+  tabs,
+  selectedTab,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      <ul className="nav nav-tabs">
+        {tabs.map(tab => (
+          <li key={tab.id} className="nav-item">
+            <button
+              type="button"
+              onClick={() => onTabSelected(tab)}
+              className={classNames(
+                'nav-link',
+                {
+                  active: selectedTab.id === tab.id,
+                },
+              )}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <h3 data-cy="tab-content">
+        {selectedTab.content}
+      </h3>
+    </>
+  );
+});
