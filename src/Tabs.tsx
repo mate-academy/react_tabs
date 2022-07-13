507ac93426cc98202c17947db7f6ebbd935359c8
KostyaKonev <kosgoh.214@gmail.com>
2022-07-13 22:20:47 +0300

    solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,27 @@
-// implement a component here
+interface Props {
+  tabs: Tab[],
+  onSelect: (tab: Tab) => void,
+}
+
+const Tabs: React.FC<Props> = ({ tabs, onSelect }) => {
+  return (
+    <ul className="nav nav-tabs">
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className="nav-item"
+        >
+          <button
+            type="button"
+            onClick={() => onSelect(tab)}
+            className="nav-link"
+          >
+            {tab.title}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Tabs;
