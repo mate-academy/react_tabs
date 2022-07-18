b41602e68ae06789c87fcee865487a3f262902c6
Roman Marunkevich <vinnibea@gmail.com>
2022-07-18 21:58:15 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
+import { Tabs } from './Tabs';
+import { Tab } from './Tab';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,45 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const mainContent = classNames({
+    'App__main-content': true,
+  });
+
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const tabChanger = (tab: Tab) => {
+    if (tab.id === selectedTab.id) {
+      return;
+    }
+
+    const newTab = tabs.find(element => element.id === tab.id) || tabs[0];
+
+    setSelectedTab(newTab);
+  };
+
+  return (
+    <div className="App">
+      <div className="App__main">
+        <h1>
+          {`Selected tab is ${selectedTab.title}`}
+        </h1>
+
+        <Tabs
+          tabs={tabs}
+          tabChanger={tabChanger}
+          currentTab={selectedTab.id}
+        />
+
+        <p
+          data-cy="tab-content"
+          className={mainContent}
+        >
+          {selectedTab.content}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,34 @@
 // implement a component here
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from './Tab';
+
+type TabsType = {
+  tabs: Tab[],
+  tabChanger(tab: Tab): void,
+  currentTab: string,
+};
+
+export const Tabs: React.FC<TabsType> = ({ tabs, tabChanger, currentTab }) => {
+  return (
+    <div>
+      {tabs.map((tab) => {
+        const active = classNames({
+          'App__button--active': currentTab === tab.id,
+          App__button: true,
+        });
+
+        return (
+          <button
+            key={tab.id}
+            type="button"
+            className={active}
+            onClick={() => tabChanger(tab)}
+          >
+            {tab.title}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
