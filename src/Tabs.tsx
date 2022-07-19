3813a5b99079dc1db336baad7961efa2ebf6b676
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-07-19 13:10:03 +0300

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,52 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from './Tab';
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: React.Dispatch<React.SetStateAction<Tab>>;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  let selectedTab = null;
+
+  return (
+    <div className="tabs">
+      {tabs.map(tab => {
+        const isTabSelected = selectedTabId === tab.id;
+
+        if (isTabSelected) {
+          selectedTab = tab;
+        }
+
+        return (
+          <button
+            key={tab.id}
+            type="button"
+            className={classNames(
+              'tabs__button',
+              { 'tabs__button--active': isTabSelected },
+            )}
+            onClick={() => {
+              if (!isTabSelected) {
+                onTabSelected(tab);
+              }
+            }}
+          >
+            {tab.title}
+          </button>
+        );
+      })}
+      <hr className="tabs__divider" />
+      <p data-cy="tab-content">
+        {(selectedTab ?? tabs[0]).content}
+      </p>
+    </div>
+  );
+};
