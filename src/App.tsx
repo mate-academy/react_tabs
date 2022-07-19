4296bdeb8b65074fd460067600b2b56fbf1a8f36
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-07-19 02:21:59 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
+import { Tab } from './Tab';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +10,24 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState<Tab>(tabs[0]);
+
+  return (
+    <div className="App">
+      <h1 className="title">{`Selected tab is ${selectedTab.title}`}</h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        onTabSelected={(tab: Tab) => {
+          return tab ? setSelectedTab(tab) : setSelectedTab(tabs[0]);
+        }}
+      />
+      <p data-cy="tab-content">
+        {selectedTab.content}
+      </p>
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,45 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from './Tab';
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <div className="tabs is-centered">
+      <ul>
+        {tabs.map((tab: Tab) => (
+          <li key={tab.id}>
+            <button
+              type="button"
+              className={classNames(
+                'button',
+                {
+                  active: selectedTabId === tab.id,
+                },
+              )}
+              onClick={() => {
+                if (tab.id === selectedTabId) {
+                  return;
+                }
+
+                onTabSelected(tab);
+              }}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
