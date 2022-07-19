1487998314b401af7a7d199bc19033125ba25a1e
Ostap Salo <jlumon211@gmail.com>
2022-07-19 14:45:06 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Home', content: '#' },
+  { id: 'tab-2', title: 'Profile', content: 'LinkeDin' },
+  { id: 'tab-3', title: 'Contact', content: '123-453-7543' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const handleSelectedClick = (tab: Tab) => {
+    if (tab.id !== selectedTab.id) {
+      setSelectedTab(tab);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1
+        className="App__title"
+      >
+        {`Selected tab is ${selectedTab.title};`}
+      </h1>
+      <Tabs
+        selectedTab={selectedTab}
+        tabs={tabs}
+        handleSelectedClick={handleSelectedClick}
+      />
+    </div>
+  );
+};
 
 export default App;
