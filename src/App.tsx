7169491441e737096f21225900a2cfa0a6298984
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-07-19 14:29:31 +0300

    tabs were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
+import Tabs from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +9,23 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const changeTab = (tab: Tab) => (
+    setSelectedTab(tab));
+
+  return (
+    <div className="App">
+      <h1 className="text-center">{`Selected tab is ${selectedTab.title}`}</h1>
+
+      <Tabs
+        tabs={tabs}
+        selectedTab={selectedTab}
+        changeTab={changeTab}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,38 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+import 'bootstrap/scss/bootstrap.scss';
+
+type Props = {
+  tabs: Tab[],
+  selectedTab: Tab,
+  changeTab: (tab: Tab) => void,
+};
+
+const Tabs: React.FC<Props> = ({ tabs, selectedTab, changeTab }) => (
+  <nav>
+    <ul className="nav nav-tabs justify-content-center">
+      {tabs.map(tab => (
+        <li key={tab.id} className="nav-item">
+          <a
+            href={tab.id}
+            className={classNames('nav-link', {
+              active: tab.id === selectedTab.id,
+            })}
+            onClick={(event) => {
+              event.preventDefault();
+              changeTab(tab);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+
+    <p className="text-center">
+      {selectedTab.content}
+    </p>
+  </nav>
+);
+
+export default Tabs;
