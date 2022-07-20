cdc1288d12cef6d708ec3f885266ffbad090afc5
Stas Yasinchyk <grodh06@gmail.com>
2022-07-20 16:37:37 +0300

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
+import { Tabs } from './Tabs';
 
 export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,11 +9,29 @@ export const tabs: Tab[] = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Selected tab is&nbsp;Tab 1</h1>
-    <div>{tabs.length}</div>
-  </div>
-);
+const App: React.FC = () => {
+  const innerTabs = [...tabs];
+  const [selectedTab, setSelectedTab] = useState('tab-1');
+
+  const changeId = (tabId: string) => {
+    if (tabId !== selectedTab) {
+      setSelectedTab(tabId);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        Selected tab is&nbsp;
+        {innerTabs.find(tab => tab.id === selectedTab)?.title}
+      </h1>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab}
+        changeId={changeId}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,40 @@
-// implement a component here
+import React from 'react';
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[]
+  selectedTabId: string
+  changeId: (tab: string) => void
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, changeId }) => {
+  return (
+    <div>
+      <ul className="list">
+        {tabs.map((tab) => (
+          <li
+            className="list__item"
+            key={tab.id}
+          >
+            <button
+              className={selectedTabId === tab.id
+                ? ('list__button')
+                : ('list__button list__button--active')}
+              type="button"
+              onClick={() => changeId(tab.id)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <p
+        className="content"
+        data-cy="tab-content"
+      >
+        {tabs.find(tab => tab.id === selectedTabId)?.content || ''}
+      </p>
+    </div>
+  );
+};
