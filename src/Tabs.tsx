9ed23b5f88034c30811db64e7c96b7141e7e4ccc
Sasha Havryliuk <princesasha9178@icloud.com>
2022-07-21 15:27:21 +0300

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,25 @@
-// implement a component here
+import { FC } from 'react';
+
+interface Props {
+  tabs: Tab[],
+  selectedTab: Tab,
+  onTabSelected: (tab: Tab) => void,
+}
+
+export const Tabs: FC<Props> = ({ tabs, onTabSelected, selectedTab }) => {
+  return (
+    <>
+      {tabs.map(tab => (
+        <button
+          className={`btn ${selectedTab.id === tab.id ? 'active' : ''}`}
+          key={tab.id}
+          type="button"
+          onClick={() => onTabSelected(tab)}
+        >
+          {tab.title}
+        </button>
+      ))}
+      <p className="content" data-cy="tab-content">{selectedTab.content}</p>
+    </>
+  );
+};
