1173c2df73c447cd4b37c386aee334ba9c3d60f3
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-21 17:31:17 +0300

    Solution 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { Tab, Tabs } from './components/Tabs/Tabs';
 
-export const tabs = [
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const App: React.FC = () => {
+  const [selectTab, setSelectTab] = useState(tabs[0]);
+
+  const handleSelectTab = (tab: Tab) => {
+    if (tab.id !== selectTab.id) {
+      setSelectTab(tab);
+    }
+  };
+
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is&nbsp;Tab 1
+        {`Selected tab is ${selectTab.title}`}
       </h1>
 
-      <div>
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active">
-              <a href="#tab-1">Tab 1</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-2">Tab 2</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-3">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="tab-content">
-          Some text 1
-        </div>
-      </div>
+      <Tabs
+        selectTab={selectTab}
+        tabs={tabs}
+        handleSelectTab={handleSelectTab}
+      />
     </div>
   );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { FC } from 'react';
+
+export interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+type Props = {
+  selectTab: Tab,
+  tabs: Tab[],
+  handleSelectTab: (tab: Tab) => void,
+};
+
+export const Tabs: FC<Props> = ({
+  selectTab,
+  tabs,
+  handleSelectTab,
+}) => {
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={classNames({
+                'is-active': tab.id === selectTab.id,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => handleSelectTab(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div
+        className="block"
+        data-cy="tab-content"
+      >
+        {selectTab.content}
+      </div>
+    </div>
+  );
+};
