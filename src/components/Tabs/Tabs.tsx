02e5f10d86504e520ac5287c5f898af33cac38c0
Ihor Shynkar <aedricson@gmail.com>
2022-07-21 15:56:36 +0300

    tabs were added

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tabId: Tab) => void
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={selectedTabId === tab.id ? 'is-active' : 'non-active'}
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => {
+                  onTabSelected(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="tab-content"
+      >
+        {tabs.filter(tab => tab.id === selectedTabId).map(tab => tab.content)}
+      </div>
+    </div>
+  );
+};
