3d3b1425aebeec23f8d245899d8320e98b1b6bae
Kate Khon <katerynakhon@gmail.com>
2022-07-21 15:49:25 +0300

    initial commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[],
+  selectedTab: Tab,
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab,
+  onTabSelected,
+}) => (
+  <div>
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <a
+              href={`#${tab.id}`}
+              className={classNames({ 'is-active': tab.id === selectedTab.id })}
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="tab-content">
+      {selectedTab.content}
+    </div>
+  </div>
+);
