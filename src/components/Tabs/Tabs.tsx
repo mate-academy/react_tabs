7e11804638f136701e0fbba3aafca6279a52376f
Ievgen Volkov <neoodessa86@gmail.com>
2022-07-23 18:50:16 +0300

    Initial commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,57 @@
-export const Tabs = () => {};
+import React, { useCallback } from 'react';
+import { styled } from '@mui/system';
+import classNames from 'classnames';
+import { Tab } from '../../types';
+
+interface Props {
+  tabs: Tab[];
+  selectedTab: Tab;
+  onClick: (id: string) => void;
+}
+
+const Wrapper = styled('div')`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const Button = styled('div')`
+  height: 45px;
+  width: 100px;
+  text-align: center;
+  padding: 13px;
+  border-radius: 5px 5px 0 0;
+  transition: transform 0.8s, background-color 0.8s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #b393d3;
+    transform: translateY(-3px);
+  }
+`;
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTab, onClick }) => {
+  const onClickHandler = useCallback((id) => {
+    onClick(id);
+  }, [onClick]);
+
+  return (
+    <div>
+      <Wrapper>
+        {tabs.map(t => (
+          <Button
+            className={classNames('', {
+              'is-active': t.id === selectedTab.id,
+            })}
+            key={t.id}
+            onClick={() => onClickHandler(t.id)}
+          >
+            {t.title}
+          </Button>
+        ))}
+      </Wrapper>
+      <div data-cy="tab-content">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
