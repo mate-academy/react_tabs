f45f3ce31ffc93d20b35f52adb686025955feb91
Ihor Kuchin <vedunigor@gmail.com>
2022-07-24 23:57:47 +0300

    Solution

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,1 +1,38 @@
-// implement a component here
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[]
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void
+
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      <ul className="nav nav-tabs">
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className="nav-item"
+          >
+            <button
+              className={classNames('nav-link is-secondary', {
+                'show active is-active': tab.id === selectedTabId,
+              })}
+              type="button"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
