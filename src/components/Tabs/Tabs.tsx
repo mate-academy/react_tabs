57a1f9a3500b156576595b3735e5fb7141a9e1f6
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-25 08:15:31 +0300

    finished with Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[]
+  selectedTab: Tab
+  tabSelectHandler: (clickedTab: Tab) => void
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab,
+  tabSelectHandler: onTabSelected,
+}) => {
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={classNames({
+                'is-active': tab.id === selectedTab.id,
+              })}
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => {
+                  onTabSelected(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="tab-content">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
