19cfcae16d06c9b2ebbab07e7f0f47b4e0c0fc34
Denys Politov <dpolitov2001@gmail.com>
2022-07-25 05:33:51 +0300

    first commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  tabs: Tab[];
+  selectedTab: Tab;
+  setSelectedTabs: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab,
+  setSelectedTabs,
+}) => {
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+            <li
+              className={(tab === selectedTab && 'is-active') || ''}
+              onClick={() => setSelectedTabs(tab)}
+              key={tab.id}
+            >
+              <a href={`#${tab.id}`}>{tab.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
