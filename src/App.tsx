d5c72a432650c1fa6f134e472da985618a65e8ce
PavloMarkov <r9markovpn@gmail.com>
2022-08-16 18:05:19 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './components/types/Tab';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,32 +11,29 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  const onTabSelected = (tab: Tab): void => {
+    const newTab = tabs.find(link => link.id === tab.id) || tabs[0];
+    const ind = tabs.indexOf(newTab);
+
+    setSelectedTab(tabs[ind]);
+  };
+
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is&nbsp;Tab 1
+        Selected tab is&nbsp;
+        {selectedTab.title}
       </h1>
 
       <div>
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active">
-              <a href="#tab-1">Tab 1</a>
-            </li>
+        <Tabs
+          tabs={tabs}
+          selectedTabId={selectedTab.id}
+          onTabSelected={onTabSelected}
+        />
 
-            <li className="">
-              <a href="#tab-2">Tab 2</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-3">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="tab-content">
-          Some text 1
-        </div>
       </div>
     </div>
   );
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,60 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: Callback;
+};
+
+type Callback = (tab: Tab) => void;
+
+export const Tabs: React.FC<Props> = (props: Props) => {
+  const {
+    tabs,
+    selectedTabId,
+    onTabSelected,
+  } = props;
+
+  const tabContent = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <>
+      <div
+        className="tabs is-boxed"
+      >
+        <ul>
+          {tabs.map((tab) => (
+            <li
+              key={tab.id}
+              className={classNames(
+                {
+                  'is-active': tab.id === selectedTabId
+              || (!tabContent && tab === tabs[0]),
+                },
+              )}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => {
+                  if (selectedTabId !== tab.id) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          )) }
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="tab-content"
+      >
+        {tabContent?.content}
+      </div>
+    </>
+  );
+};
