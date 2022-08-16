f7d05512620379f7582beec399171d580068c727
Kate <krasnova2912@gmail.com>
2022-08-16 21:45:39 +0300

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './components/Tabs/Tab';
 
-export const tabs = [
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -17,7 +18,7 @@ export const App: React.FC = () => {
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is&nbsp;${activeTab.title}`}
+        {`Selected tab is ${activeTab.title}`}
       </h1>
 
       <Tabs
