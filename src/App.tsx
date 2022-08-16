07b31e48a8a71a23eecc0ff1fd64117293073af9
Vitaliy <vitmixgt@icloud.com>
2022-08-17 00:25:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
+import { Tabs } from './components/Tabs/Tabs';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 export const tabs = [
@@ -9,35 +10,20 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is&nbsp;Tab 1
+        Selected tab is&nbsp;
+        {selectedTab.title}
       </h1>
 
-      <div>
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active">
-              <a href="#tab-1">Tab 1</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-2">Tab 2</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-3">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="tab-content">
-          Some text 1
-        </div>
-      </div>
+      <Tabs
+        tabs={tabs}
+        selectedTab={selectedTab.id}
+        setTabSelected={setSelectedTab}
+      />
     </div>
   );
 };
-
-export default App;
