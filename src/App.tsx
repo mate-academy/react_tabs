aca66a2b7fa798de7f6ad662e3aa0ca440a0c605
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-08-16 12:59:48 +0300

    decomposed tabs into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Tabs } from './components/Tabs/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,33 +10,29 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+
+  const onTabSelected = (tabId: string) => {
+    setSelectedTab(() => {
+      // eslint-disable-next-line no-console
+      console.log('updated');
+
+      return tabId;
+    });
+  };
+
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is&nbsp;Tab 1
+        Selected tab is&nbsp;
+        {tabs.find(tab => tab.id === selectedTab)?.title}
       </h1>
 
-      <div>
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active">
-              <a href="#tab-1">Tab 1</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-2">Tab 2</a>
-            </li>
-
-            <li className="">
-              <a href="#tab-3">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="tab-content">
-          Some text 1
-        </div>
-      </div>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTab}
+        onTabSelect={onTabSelected}
+      />
     </div>
   );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,52 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../interfaces/Tab';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelect: (tabId: string) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelect,
+}) => {
+  const shouldUpdate = (tabId: string) => {
+    let selectedId = tabId;
+
+    if (!tabs.map(tab => tab.id).includes(selectedId)) {
+      selectedId = tabs[0].id;
+    }
+
+    if (selectedTabId !== selectedId) {
+      onTabSelect(selectedId);
+    }
+  };
+
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={classNames({ 'is-active': tab.id === selectedTabId })}
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => shouldUpdate(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
