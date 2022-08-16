729353faaf7df9a5da5aa1a1840ded682557e64a
Oleg Vetrov <oleg94038@gmail.com>
2022-08-16 15:01:51 +0300

    Added Tabs implementation

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,48 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  setSelectedTab: CallableFunction,
+};
+
+export const Tabs: React.FC<Props> = (props) => {
+  const { tabs, selectedTabId, setSelectedTab } = props;
+  const tabContent = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={cn('', { 'is-active': selectedTabId === tab.id })}
+            >
+              <a
+                href={tab.id}
+                onClick={(event) => {
+                  event.preventDefault();
+                  setSelectedTab(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {tabContent?.content}
+      </div>
+    </div>
+  );
+};
