448578ce7e38b51c23ea75b89c33a4267e88ce3e
Yevhen Nikolaienko <illnino380@gmail.com>
2022-08-17 13:03:27 +0300

    implemented tabs list

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,57 @@
-export const Tabs = () => {};
+import { FC } from 'react';
+import cn from 'classnames';
+import { Tab } from '../../Types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (newTab: Tab) => void,
+};
+
+export const Tabs: FC<Props> = (props) => {
+  const {
+    tabs,
+    selectedTabId,
+    onTabSelected,
+  } = props;
+
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={cn(
+                  { 'is-active': tab.id === selectedTabId },
+                )}
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  onClick={
+                    () => {
+                      if (tab.id !== selectedTabId) {
+                        onTabSelected(tab);
+                      }
+                    }
+                  }
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
