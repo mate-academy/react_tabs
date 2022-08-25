ff5d8f23df7beb9e7afa86dc23d8b1cc8ffa78e5
Vitalii-Heiko <papus@ukr.net.com>
2022-08-25 11:46:38 +0300

    first commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={classNames(
+            {
+              'is-active': tab.id === selectedTabId,
+            },
+          )}
+        >
+          <a
+            href={`#${tab.id}`}
+            onClick={() => {
+              onTabSelected(tab);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+
+      ))}
+    </ul>
+
+  );
+};
