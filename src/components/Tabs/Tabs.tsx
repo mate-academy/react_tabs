a15c724fab6f76408e7c7f40b26e187b6116695e
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-09-12 17:27:38 +0300

    develop

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+import React from 'react';
+// import classNames from 'classnames';
+
+interface Tab {
+  id: string;
+  title: string;
+  content: string;
+}
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <>
+      {tabs.map(tab => (
+        <li
+          className={tab.id === selectedTabId ? 'is-active' : ''}
+          key={tab.id}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            onClick={() => onTabSelected(tab)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </>
+  );
+};
