6df209160c8e3ba0c6caf7d2e932d17237afe0a9
Maks Sobko <tiserettt@gmail.com>
2022-09-13 18:58:20 +0300

    changes according to review

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Tab } from '../../types/tab';
 
 type Props = {
@@ -13,30 +14,30 @@ export const Tabs: React.FC<Props> = ({
 }) => {
   const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 
+  const handleClick = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+
+    return null;
+  };
+
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => (
             <li
-              className={
-                selectedTab.id === tab.id
-                  ? 'is-active'
-                  : ''
-              }
+              className={classNames({
+                'is-active': selectedTab?.id === tab.id,
+              })}
               data-cy="Tab"
               key={tab.id}
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => {
-                  if (tab.id === selectedTabId) {
-                    return;
-                  }
-
-                  onTabSelected(tab);
-                }}
+                onClick={() => handleClick(tab)}
               >
                 {tab.title}
               </a>
