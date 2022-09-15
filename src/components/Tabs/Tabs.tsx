4aa3d363cc3f0f213b7dda8a7c18b1395fbb1b24
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-09-15 16:04:37 +0300

    REact Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import React from 'react';
+
+import { TabsArrow } from '../../types/TabsArrow';
+
+type Props = {
+  tabs: TabsArrow[],
+  tabId: string,
+  onTab: TabsArrow,
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, tabId, onTab }) => {
+  return (
+    <div data-cy="tab-content">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={tab.id === tabId ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onTab(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === tabId)?.content}
+      </div>
+    </div>
+  );
+};
