c772d16efa309bc31c215cd1f0593e0575cebe76
Yurii Kozlov <yurka22870@gmail.com>
2022-09-17 15:32:32 +0300

    some fixes were added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Tabs } from './Tabs';
+import { Tabs } from './components/Tabs';
 import { Tab } from './Tab';
 
 export const tabs = [
