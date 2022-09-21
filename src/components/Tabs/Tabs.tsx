6a788863804d61edbba495af0d941cb63f5b89ed
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-09-21 22:45:48 +0300

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,57 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+interface Props {
+  id: string,
+  title: string,
+  content: string,
+}
+
+interface TabsType {
+  tabs: Props[],
+  selectedTabId: Props,
+  handelClick: (value: Props) => void,
+}
+
+export const Tabs: React.FC<TabsType> = React.memo(
+  ({
+    tabs,
+    selectedTabId,
+    handelClick,
+  }) => {
+    return (
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {
+              tabs.map((tab) => {
+                return (
+                  <li
+                    key={tab.id}
+                    className={classNames(
+                      { 'is-active': tab.id === selectedTabId.id },
+                    )}
+                    data-cy="Tab"
+                  >
+                    <a
+                      href={`#${tab.id}`}
+                      data-cy="TabLink"
+                      onClick={() => handelClick(tab)}
+                    >
+                      {tab.title}
+                    </a>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {selectedTabId.content}
+        </div>
+      </div>
+
+    );
+  },
+);
