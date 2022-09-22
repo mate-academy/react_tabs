aacd3e14d327c6740985be07f68d839033406817
NataliyaAn <otchet333@gmail.com>
2022-09-22 15:32:32 +0300

    react-tabs soluton

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,40 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../type';
+
+type Props = {
+  tabs:Tab[]
+  isActiveId: string;
+  setActiveId:(tab: Tab) => void;
+
+};
+
+export const Tabs:React.FC <Props> = ({
+  tabs,
+  isActiveId,
+  setActiveId,
+}) => {
+  return (
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={classNames(
+              { 'is-active': tab.id === isActiveId },
+            )}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => setActiveId(tab)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
