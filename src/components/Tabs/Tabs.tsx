26a2b5f6b8e7ccedd85b0900ce3fff82af264bce
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-09-27 16:57:31 +0300

    Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,54 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { TabType } from '../../types/TabType';
+
+interface Props {
+  tabs: TabType[],
+  onTabSelected: (tab: TabType) => void,
+  selectedTabId: string,
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  onTabSelected,
+  selectedTabId,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const switchTabs = (tab: TabType) => {
+    if (selectedTabId !== tab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {
+            tabs.map(tab => {
+              return (
+                <li
+                  className={classNames('',
+                    { 'is-active': selectedTabId === tab.id })}
+                  data-cy="Tab"
+                >
+                  <a
+                    href="#tab-1"
+                    data-cy="TabLink"
+                    onClick={() => switchTabs(tab)}
+                  >
+                    Tab 1
+                  </a>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
