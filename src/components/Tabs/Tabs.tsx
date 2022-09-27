8d670f1df9808aa893edcc8136bc4709dcb5e208
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-09-27 13:33:09 +0300

    Fix issues after review

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,14 +1,11 @@
 import classNames from 'classnames';
+import { getTabByID } from '../../helpers/getTabByID';
 import { Tab } from '../../types/tab';
 
 type Props = {
   tabs: Tab[];
   selectedTabId: string;
   onTabSelected: (tab: Tab) => void;
-};
-
-const getTabByID = (tabs: Tab[], tabId: string) => {
-  return tabs.find(tab => tab.id === tabId) || tabs[0];
 };
 
 export const Tabs: React.FC<Props> = (props) => {
@@ -20,7 +17,7 @@ export const Tabs: React.FC<Props> = (props) => {
 
   const { id: validSelectedTabId, content } = getTabByID(tabs, selectedTabId);
 
-  const handleClick = (id: string) => {
+  const handleTabSelect = (id: string) => {
     if (id !== validSelectedTabId) {
       onTabSelected((getTabByID(tabs, id)));
     }
@@ -31,16 +28,18 @@ export const Tabs: React.FC<Props> = (props) => {
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(({ id, title }) => (
-            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
             <li
               key={id}
               className={classNames(
                 { 'is-active': id === validSelectedTabId },
               )}
               data-cy="Tab"
-              onClick={() => handleClick(id)}
             >
-              <a href={`#${id}`} data-cy="TabLink">
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabSelect(id)}
+              >
                 {title}
               </a>
             </li>
