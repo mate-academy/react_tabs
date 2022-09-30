2daca493caf11dbdf93f6a7f6c7a6775aca526ba
11Olya112 <OlyaBushovska767@gmail.com>
2022-10-01 00:32:14 +0300

    commit message

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,28 @@
-export const Tabs = () => {};
+import React from 'react';
+import Tab from './types';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  tabSelected: React.Dispatch<React.SetStateAction<Tab>>;
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectedTabId, tabSelected }) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        key={tab.id}
+        className={tab.id === selectedTabId ? 'is-active' : 'tab'}
+        onClick={() => {
+          if (tab.id !== selectedTabId) {
+            tabSelected(tab);
+          }
+        }}
+      >
+        <a href="#">
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
