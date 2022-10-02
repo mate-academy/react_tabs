52ae68e99521c4c838fa3e0c79a6b25a322336eb
Oleksii <oleksii@it.com>
2022-10-02 16:03:30 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,36 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Tabs } from './components/Tabs';
 
-interface TabType {
+interface TabsProps {
   id: string;
   title: string;
   content: string;
 }
 
-export const tabs: TabType[] = [
+export const tabs: TabsProps[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const App: React.FC = () => {
-  const [selectedTabId, setId] = useState(tabs[0].id);
-  const tabSelected = (tab: TabType) => setId(tab.id);
-  const isSelectedTab = tabs.find(tab => tab.id === selectedTabId)
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+
+  const selectTab = (tab: TabsProps) => setSelectedTabId(tab.id);
+
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId)
     || tabs[0];
 
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${isSelectedTab.title}`}
+        {`Selected tab is ${selectedTab.title}`}
       </h1>
 
       <Tabs
         tabs={tabs}
         selectedTabId={selectedTabId}
-        tabSelected={tabSelected}
+        tabSelected={selectTab}
       />
     </div>
   );
