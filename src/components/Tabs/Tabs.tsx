13344fd74e89732718fe99ef340635908da9f456
HusakOleh <husakoleh91@gmail.com>
2022-10-04 11:34:55 +0300

    firs solution(need fix TS)

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+import React, { useState, MouseEvent } from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectTab: (event: MouseEvent, callback: () => {}, tabsArr: Tab[]) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectTab,
+}) => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+
+          {tabs.map(tab => (
+            <li
+              className={classNames({
+                'is-active': selectedTab.id === tab.id,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                id={tab.id}
+                onClick={(event) => {
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                  // @ts-ignore
+                  return selectTab(event, setSelectedTab, tabs);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
