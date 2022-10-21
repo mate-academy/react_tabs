5e4649335110c1b1ab6513595360ecaa61fef7a2
Andriy Andrieiev <proph7000@gmail.com>
2022-10-21 14:19:05 +0300

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,61 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../Type/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onClick: React.MouseEventHandler<HTMLAnchorElement>,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onClick,
+}) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            let isSelected = false;
+
+            if (tab.id === selectedTabId) {
+              isSelected = true;
+            }
+
+            return (
+              <li
+                className={classNames(
+                  {
+                    'is-active': isSelected,
+                  },
+                )}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={(event) => {
+                    if (!isSelected) {
+                      onClick(event);
+                    }
+                  }}
+                >
+                  { tab.title }
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {(tabs.find(
+          tab => selectedTabId === tab.id,
+        )?.content)}
+      </div>
+    </div>
+  );
+};
