c5a0617378eeb2115e5255590c1476844d1547ba
Maksym845 <mzuravel845@gmail.com>
2022-10-21 10:09:25 +0300

    solution was added

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export interface Tab {
+  id: string;
+  title: string;
+  content: string;
+}
+
+interface ArrOfTabs {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (arg: Tab) => void;
+}
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }: ArrOfTabs) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab: Tab) => (
+            <li
+              className={classNames({ 'is-active': selectedTabId === tab.id })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => ((selectedTabId !== tab.id)
+                  && onTabSelected(tab))}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.filter((tab: Tab) => tab.id === selectedTabId)
+          .map((tab: Tab) => tab.content)}
+      </div>
+    </div>
+  );
+};
