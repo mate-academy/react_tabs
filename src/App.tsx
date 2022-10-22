7152b6db137cfdc6c2f25f7d1ca8e4d1ad05777c
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-22 09:06:48 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Tabs } from './components/Tabs';
 import { Tab } from './react-app-env';
 
 export const tabs = [
-  { id: 'asasas', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
