5039d2d3ab68c784007dc3c1a6d3c930917ef5f9
Serhii Romaniuk <deepspark@gmail.com>
2022-10-23 18:16:41 +0300

    create Tabs component, added render and styles logic

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,52 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { FC } from 'react';
+import { Tab } from '../../react-app-env';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                data-cy="Tab"
+                className={classNames({
+                  'is-active': id === selectedTab.id,
+                })}
+                onFocus={() => {
+                  return selectedTabId !== id
+                    ? onTabSelected(tab)
+                    : 0;
+                }}
+                key={id}
+              >
+                <a href={`#${id}`} data-cy="TabLink">
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+export interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
