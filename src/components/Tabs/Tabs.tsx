2ccefa7347d4d405cb7d76b235542330a7d10dcf
Myroslav Diiak <diiakm23819@gmail.com>
2022-10-24 14:48:22 +0300

    useless fragment removed

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -17,43 +17,41 @@ export const Tabs: React.FC<Props> = ({
   const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 
   return (
-    <>
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => {
-              const { id, title } = tab;
-              const isCurrentSelected = id === selectedTab.id;
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+            const isCurrentSelected = id === selectedTab.id;
 
-              return (
-                <li
-                  key={id}
-                  data-cy="Tab"
-                  className={classNames({
-                    'is-active': isCurrentSelected,
-                  })}
+            return (
+              <li
+                key={id}
+                data-cy="Tab"
+                className={classNames({
+                  'is-active': isCurrentSelected,
+                })}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (!isCurrentSelected) {
+                      onTabSelected(tab);
+                    }
+                  }}
                 >
-                  <a
-                    href={`#${id}`}
-                    data-cy="TabLink"
-                    onClick={() => {
-                      if (!isCurrentSelected) {
-                        onTabSelected(tab);
-                      }
-                    }}
-                  >
-                    {title}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <p className="block" data-cy="TabContent">
-          {selectedTab.content}
-        </p>
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </>
+
+      <p className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </p>
+    </div>
   );
 };
