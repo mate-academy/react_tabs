822c6276fb8701ce3b5dae0daab80ac7163da1a4
Anton Iskryk <iskryk.anton@gmail.com>
2022-10-25 17:29:06 +0300

    Implemented changing content when clicking on different tab

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-
-interface Tab {
-  id: string,
-  title: string,
-  content: string,
-}
+import { Tab } from '../../react-app-env';
 
 type Props = {
   tabs: Tab[];
@@ -18,37 +13,37 @@ export const Tabs: React.FC<Props> = ({
   selectedTabId,
   onTabSelected,
 }) => {
-  return (
-    <>
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => (
-              <li
-                className={cn({
-                  'is-active': selectedTabId === tab.id,
-                })}
-                data-cy="Tab"
-                key={tab.id}
-              >
-                <a
-                  href={`#${tab.id}`}
-                  data-cy="TabLink"
-                  onClick={() => (
-                    selectedTabId !== tab.id && onTabSelected(tab)
-                  )}
-                >
-                  {tab.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+  const activeTab = tabs.find(tab => selectedTabId === tab.id) || tabs[0];
 
-        <div className="block" data-cy="TabContent">
-          Some text 1
-        </div>
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': selectedTabId === tab.id,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => (
+                  selectedTabId !== tab.id && onTabSelected(tab)
+                )}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
   );
 };
