f421c0d647eed9a1d1064f030bba2282d8df7d2d
korniienkokarina <k.tashlytska@gmail.com>
2022-10-25 23:33:09 +0300

    tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[];
+  tabSelectedId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = (
+  { tabs, tabSelectedId, onTabSelected },
+) => {
+  const tabChosen = tabs.find(
+    tab => tab.id === tabSelectedId,
+  ) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={classNames({
+                'is-active': tab.id === tabChosen.id,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab.id !== tabChosen.id) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabChosen.content}
+      </div>
+    </div>
+  );
+};
