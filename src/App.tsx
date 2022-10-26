38314892f3814863adf4b2a4b811917017d26ccb
igor-1987 <igor.gayvoronsky@gmail.com>
2022-10-26 13:06:38 +0300

    added type to used useState item

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { Tabs } from './components/Tabs';
+import { Tab } from './react-app-env';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 
-export const tabs = [
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const App: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [selectedTab, setSelectedTab] = useState<Tab>(tabs[0]);
 
   return (
     <div className="section">
