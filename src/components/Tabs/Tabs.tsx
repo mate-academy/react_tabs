13af171bca8257c4f2fbdb8741125558fa90bd7b
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-10-28 17:25:16 +0300

    removed destructuring for tab, selectedTabContent variable, changed selectedTab variable

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -14,47 +14,38 @@ export const Tabs: React.FC<Props> = ({
   selectedTabId,
   onTabSelected,
 }) => {
-  const selectedTab = tabs.some(tab => tab.id === selectedTabId)
-    ? selectedTabId
-    : tabs[0].id;
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
   const onSelectTab: OnSelectTab = (id, tab) => {
     if (selectedTabId !== id) {
       onTabSelected(tab);
     }
   };
 
-  const selectedTabContent = tabs
-    .find(tab => selectedTabId === tab.id)?.content;
-
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => {
-            const { id, title } = tab;
-
-            return (
-              <li
-                className={classNames({
-                  'is-active': selectedTab === id,
-                })}
-                data-cy="Tab"
+          {tabs.map(tab => (
+            <li
+              className={classNames({
+                'is-active': selectedTab.id === tab.id,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onSelectTab(tab.id, tab)}
               >
-                <a
-                  href={`#${tab.id}`}
-                  data-cy="TabLink"
-                  onClick={() => onSelectTab(id, tab)}
-                >
-                  {title}
-                </a>
-              </li>
-            );
-          })}
+                {tab.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTabContent}
+        {selectedTab.content}
       </div>
     </div>
   );
