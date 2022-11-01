516ad5f0198aad1164f6c69fb588473c4e5e093b
Kamil Witek <kamilprogramuje@gmail.com>
2022-11-01 15:01:49 +0100

    tabnr changed to tabData, added space between colon and value type, boolean value moved to shouldTabBeMarked, added function handleClickTab

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,8 +5,8 @@ import cn from 'classnames';
 import { Tab } from '../../types';
 
 type Props = {
-  onTabSelected: (tabNr: Tab) => void,
-  tabs: Tab[] | [],
+  onTabSelected: (tabData: Tab) => void,
+  tabs: Tab[],
   selectedTabId: string,
 };
 
@@ -19,9 +19,15 @@ export const Tabs: FC<Props> = ({
     tab.id === selectedTabId
   ))?.content;
 
-  const isIdCorrect:boolean = tabs.some((tab:Tab) => (
+  const isIdCorrect: boolean = tabs.some((tab: Tab) => (
     tab.id === selectedTabId
   ));
+
+  const handleClickTab = (id: string, tab: Tab) => {
+    if (id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
 
   return (
     <div className="TabsComponent" data-cy="tab-content">
@@ -31,14 +37,15 @@ export const Tabs: FC<Props> = ({
             tabs.map((tab, index) => {
               const { id, title } = tab;
 
+              const shouldTabBeMarked = (selectedTabId === id
+                // select first tab if selectedTabId is incorrect
+                || (!isIdCorrect && index === 0));
+
               return (
                 <li
                   className={cn(
                     {
-                      'is-active':
-                      selectedTabId === id
-                      // select first tab if selectedTabId is incorrect
-                      || (!isIdCorrect && index === 0),
+                      'is-active': shouldTabBeMarked,
                     },
                   )}
                   data-cy="Tab"
@@ -47,11 +54,7 @@ export const Tabs: FC<Props> = ({
                   <a
                     href={`#${id}`}
                     data-cy="TabLink"
-                    onClick={() => {
-                      if (id !== selectedTabId) {
-                        onTabSelected(tab);
-                      }
-                    }}
+                    onClick={() => handleClickTab(id, tab)}
                   >
                     {title}
                   </a>
