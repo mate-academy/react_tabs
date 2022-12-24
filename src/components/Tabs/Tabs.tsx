f4e556192b61c6772fcb5bf66146805996a78ca7
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2022-12-24 12:19:29 +0200

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,60 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (object: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const currentTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const isTabSelected = (tab: Tab) => currentTab.id === tab.id;
+
+  const handleTabSelect = (tab: Tab) => {
+    if (isTabSelected(tab)) {
+      return;
+    }
+
+    onTabSelected(tab);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={classNames({
+                'is-active': tab.id === currentTab.id,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleTabSelect(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+};
