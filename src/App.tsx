287229fe3f445388d69de84bc83ae04043be3916
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-12-28 21:23:24 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+
+import { Tabs } from './components/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,32 +11,38 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App: React.FC = () => {
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+export const App = () => {
+  const [selected, setSelected] = useState(tabs[0]);
+
+  const onTabSelected = (tab: Tab) => {
+    if (selected !== tab) {
+      setSelected(tab);
+    }
+  };
+
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is Tab 1
+        {`Selected tab is ${selected.title}`}
       </h1>
 
       <div data-cy="TabsComponent">
         <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-            </li>
-          </ul>
+          <Tabs
+            selectedTab={selected}
+            tabs={tabs}
+            onTabSelected={onTabSelected}
+          />
         </div>
 
-        <div className="block" data-cy="TabContent">
-          Some text 1
+        <div className="block" data-cy="tab-content">
+          {selected.content}
         </div>
       </div>
     </div>
