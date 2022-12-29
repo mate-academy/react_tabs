720ec61e3ba1d4f1d88b8f567877c07c2a4701dd
oleksandr-saulko <aleksandrsaulko@gmail.com>
2022-12-29 22:58:49 +0200

    Solution

---
+++ src/Tabs.tsx
@@ -0,0 +1,55 @@
+import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+};
+
+const Tabs: React.FC<Props> = ({ tabs, selectedTabId, onTabSelected }) => {
+  const [tabContent, setTabContent] = useState('');
+
+  useEffect(() => {
+    const currentTabContent = tabs.filter(tab => tab.id === selectedTabId)[0]
+      .content;
+
+    setTabContent(currentTabContent);
+  }, [selectedTabId]);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              className={classNames({ 'is-active': id === selectedTabId })}
+              data-cy="Tab"
+              key={id}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={onTabSelected}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabContent}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
