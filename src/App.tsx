79a95ef185c1ae9afc5912a9e8b48160aad9d329
Anton Martyniuk <antonmartyniuk@gmail.com>
2023-01-05 21:47:32 +0200

    Solution react-tabs

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,50 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 
+import { Tabs } from './components/Tabs';
+import { Tab } from './types/Tab';
+
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App: React.FC = () => {
-  return (
-    <div className="section">
-      <h1 className="title">
-        Selected tab is Tab 1
-      </h1>
+export class App extends React.Component {
+  state = {
+    selectedTab: tabs[0],
+    selectedTabId: 'tab-1',
+  };
 
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-            </li>
+  changeTab = (tab: Tab) => {
+    if (this.state.selectedTab !== tab) {
+      this.setState({ selectedTab: tab, selectedTabId: tab.id });
+    }
+  };
 
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-            </li>
+  render() {
+    const { selectedTab, selectedTabId } = this.state;
 
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-            </li>
-          </ul>
-        </div>
+    return (
+      <div className="section">
+        <h1 className="title">
+          {`Selected tab is ${selectedTab.title}`}
+        </h1>
 
-        <div className="block" data-cy="TabContent">
-          Some text 1
+        <div data-cy="TabsComponent">
+          <div className="tabs is-boxed">
+            <Tabs
+              tabs={tabs}
+              selectId={selectedTabId}
+              onTabSelected={this.changeTab}
+            />
+          </div>
+
+          <div className="block" data-cy="TabContent">
+            {selectedTab.content}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectId: string,
+  onTabSelected: any,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectId,
+  onTabSelected,
+}) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        const { id, title } = tab;
+
+        const handleChange = () => {
+          onTabSelected(tab);
+        };
+
+        return (
+          <li
+            className={selectId === id ? 'is-active' : ''}
+            data-cy="Tab"
+            key={id}
+          >
+            <a
+              href={`#${id}`}
+              data-cy="TabLink"
+              onClick={handleChange}
+            >
+              {title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
