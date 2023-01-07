f0380e9053b28beb4fd257ff4fe6ab01925eb646
mr-valentos <nerpavlad@gmail.com>
2023-01-07 18:07:51 +0200

    Tabs solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,53 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  selectTab: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  selectedTabId,
+  tabs,
+  selectTab,
+}) => {
+  const { content } = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const active = tab.id === selectedTabId;
+
+            return (
+              <li
+                key={tab.id}
+                className={cn({ 'is-active': active })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (!active) {
+                      selectTab(tab);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
