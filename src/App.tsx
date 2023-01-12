bc556fd84dc121a7b1e1f9f0f05669be66b52c67
kreisberg <anastasia.kreisberg@yahoo.com>
2023-01-12 22:13:17 +0200

    create handleOnClick function, find selectedTabs by ID and markup to return after

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export const App: React.FC = () => {
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${setSelectedTab.title}`}
+        {`Selected tab is ${selectedTab.title}`}
       </h1>
 
       <Tabs
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -3,7 +3,7 @@ import { Tab } from './types/Tab';
 type Props = {
   tabs: Tab[],
   selectedTabId: string,
-  selectedOnTab: (tab: Tab) | null,
+  selectedOnTab: (tab: Tab) => void,
 };
 
 export const Tabs: React.FC<Props> = ({
@@ -11,5 +11,26 @@ export const Tabs: React.FC<Props> = ({
   selectedTabId,
   selectedOnTab,
 }) => {
-  //
+  const selectedTab = tabs.find(({ id }) => id === selectedTabId || tabs[0]);
+
+  function handleOnClick(tab: Tab): void | '' {
+    return (tab.id !== selectedTabId
+      ? selectedOnTab(tab)
+      : ''
+    );
+  }
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          // maped tabs
+        </ul>
+      </div>
+
+      <div className="block" data-ce="TabContent">
+        {selectedTab?.content}
+      </div>
+    </div>
+  );
 };
