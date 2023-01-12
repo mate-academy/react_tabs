31adcd172b38720b70ce572198241828d2354549
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-01-12 16:52:06 +0200

    first commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import { Tab } from '../../type/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  setSelectedTab: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props>
+  = ({ tabs, selectedTabId, setSelectedTab }) => {
+    const properTab = tabs.find(element => (
+      element.id === selectedTabId
+    )) || tabs[0];
+
+    const newSelectedTab = (newTab: Tab) => {
+      if (newTab.id !== selectedTabId) {
+        setSelectedTab(newTab);
+      }
+    };
+
+    return (
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={classNames(
+                  { 'is-active': properTab?.id === tab.id },
+                )}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => newSelectedTab(tab)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {properTab?.content}
+        </div>
+      </div>
+    );
+  };
