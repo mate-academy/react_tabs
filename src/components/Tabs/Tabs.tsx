963fc745ae2726488f55bbc2fc431f16ba4e64c5
Andrii Petrinko <apetrinko@gmail.com>
2023-01-19 08:49:57 +0200

    memo for tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames';
+import { memo } from 'react';
 import { Tab } from '../../types/Tab';
 
 type Props = {
@@ -7,45 +8,47 @@ type Props = {
   onTabSelected: (tab: Tab) => void,
 };
 
-export const Tabs: React.FC<Props> = (props) => {
-  const {
-    tabs,
-    selectedTabId,
-    onTabSelected,
-  } = props;
-  const selectedTab = tabs.find(({ id }) => id === selectedTabId) || tabs[0];
-  const { id, content } = selectedTab;
+export const Tabs: React.FC<Props> = memo(
+  (props) => {
+    const {
+      tabs,
+      selectedTabId,
+      onTabSelected,
+    } = props;
+    const selectedTab = tabs.find(({ id }) => id === selectedTabId) || tabs[0];
+    const { id, content } = selectedTab;
 
-  const handleTab = (tab: Tab) => {
-    if (tab.id !== selectedTabId) {
-      onTabSelected(tab);
-    }
-  };
+    const handleTab = (tab: Tab) => {
+      if (tab.id !== selectedTabId) {
+        onTabSelected(tab);
+      }
+    };
 
-  return (
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              className={cn({ 'is-active': id === tab.id })}
-              data-cy="Tab"
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => handleTab(tab)}
+    return (
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={cn({ 'is-active': id === tab.id })}
+                data-cy="Tab"
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
-        </ul>
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleTab(tab)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {content}
+        </div>
       </div>
-      <div className="block" data-cy="TabContent">
-        {content}
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
