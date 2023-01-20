66c9e877b3fc9d5e1fb69e1c8ac9286e5048ca96
Elias Montauti <egsm147@gmail.com>
2023-01-20 12:38:35 +0000

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+export type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tables: Tab[],
+  selectedTabId: string,
+  onSelectedTabId: ((selectedTabId: string) => void),
+};
+
+export const Tabs: React.FC<Props> = React.memo(
+  ({
+    tables,
+    selectedTabId,
+    onSelectedTabId,
+  }) => {
+    return (
+      <>
+        {tables.map((tab: Tab) => (
+          <li
+            key={tab.id}
+            className={classNames({
+              'is-active': tab.id === selectedTabId,
+            })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => onSelectedTabId(tab.id)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </>
+    );
+  },
+);
