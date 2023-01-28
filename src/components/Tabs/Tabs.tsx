9ed8bd051f84727a8800a3640320d93619833d3f
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-01-28 23:31:57 +0200

    shrek

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import { FC } from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div className="tabs is-toggle is-fullwidth">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={classNames({
+              'is-active': tab.id === selectedTab.id,
+            })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                if (tab.id !== selectedTab.id) {
+                  onTabSelected(tab);
+                }
+              }}
+            >
+              {`Reason #${tab.id}`}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/truestOfFacts.ts
@@ -0,0 +1,38 @@
+/* eslint-disable max-len, operator-linebreak */
+export const tabs = [
+  {
+    id: '1',
+    title: 'Shrek 2 Perfectly Expands the World and the Characters',
+    content: 'When Shrek came out, it quickly found its way into our hearts because of its entertaining storyline, friendly characters, and hilarious scenes. It’s safe to say that writers and directors had a tough job on their hands when creating this sequel. But kudos to them because Shrek 2 is truly a work of art that truly lives up to the original.\n' +
+      '\n' +
+      'It perfectly expands the storyline and builds up the characters in an even more profound way. If you thought you couldn’t love Shrek, Fiona, and Donkey, even more, Shrek 2 proves you wrong. You immediately fall in love with them even more as you watch them embark on a new adventure together to a land far, far away.',
+  },
+  {
+    id: '2',
+    title: 'Shrek 2 Is as Funny as the First Movie',
+    content: 'One of the biggest reasons why this is the best sequel ever made is that it’s as funny (if not even funnier) as the first movie. Whether that’s because of Shrek’s inability to adapt to the royal family or Puss-in-Boot’s hairball scene, you’ll definitely laugh like crazy watching this sequel.\n' +
+      '\n' +
+      'But Shrek 2 also plays around with popular cultural references that you might not even catch at the first viewing. It includes inside jokes from Cinderella, Beauty and the Beast, Pretty Woman, The Little Mermaid, Pinnochio, Peter Pan, and many more',
+  },
+  {
+    id: '3',
+    title: 'It Has a Phenomenal Soundtrack',
+    content: 'One thing’s for sure; Shrek 2 might not be a better movie than the first one, but it certainly has much better music. It does an amazing job with the motion picture soundtrack, with songs during memorable scenes such as Holding Out for a Hero (that the Fairy God Mother performs toward the end of the movie), Livin La Vida Loca (that Puss and Donkey sing together), Accidentally In Love (used as a honeymoon montage for Shrek and Fiona), and Funkytown (played while the crew arrives to Far Far Away), etc.\n' +
+      '\n' +
+      'These songs are still massive hits to this day, with some of them becoming even more legendary that the original versions. Let’s face it when you hear the song Funkytown, do you think of Lipps Inc or Shrek?',
+  },
+  {
+    id: '4',
+    title: 'New Characters Are a Breath of Fresh Air',
+    content: 'Shrek, Fiona, Donkey. These memorable characters will certainly go down in (animated) history as some of the most human-like, relatable, funny characters. But what makes Shrek 2 the greatest sequel of all time is that it introduces a whole bunch of new characters that are as warm and relatable as the original crew.\n' +
+      '\n' +
+      'Puss-in-Boots (voiced by the one and only Antonio Banderas) is definitely a fan favorite. He’s a dangerous and bossy outlaw disguised as a cute fury kitten who goes on to become one of the protagonists of the movie and a big helper to the Shrek crew. Aside from Puss-in-Boots, Shrek 2 also welcomes Prince Charming, Fairy God Mother, Fiona’s royal parents, and many more.',
+  },
+  {
+    id: '5',
+    title: 'The Animation Is Astonishing',
+    content: 'Shrek 2 is an ideal sequel. Not only does it further develop the main characters, but it also introduces new ones that are equally important to the story. It also gives a thematic depth and answers all of our questions from the first movie. But one of the most applaudable things about Shrek 2 is the vast improvement in animation.\n' +
+      '\n' +
+      'The animators truly did a phenomenal job with the sequel, making it incredibly relatable, funny, and warm. It draws you into the Shrek world so seamlessly that you never want to leave it. Shrek 2 is truly a piece of art and a classic in the animation world. It’s a must-see!',
+  },
+];
