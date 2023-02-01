2809f2b80c26a18dc9a757e8d7c4589caa556650
Maks <comanderius@gmail.com>
2023-02-01 18:09:32 +0100

    React Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+type Props = {
+  tab: Tab,
+  id: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({ tab, id, onTabSelected }) => {
+  return (
+    <li
+      className={classNames(
+        { 'is-active': tab.id === id },
+      )}
+      data-cy="Tab"
+      key={tab.id}
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          if (tab.id !== id) {
+            onTabSelected(tab);
+          }
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
