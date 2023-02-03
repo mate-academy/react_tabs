324347cb942e400172dd16f3e6ec48d9b83c81e3
Драгота Инна <dragotaIV@gmail.com>
2023-02-03 18:21:46 +0200

    first

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+interface Tab {
+  id: string
+  title: string
+  content: string
+}
+
+type Props = {
+  tab: Tab;
+  id: string;
+  onTabSelect: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({ tab, id, onTabSelect }) => {
+  return (
+    <li
+      className={classNames({ 'is-active': tab.id === id })}
+      data-cy="Tab"
+      key={tab.id}
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          if (tab.id !== id) {
+            onTabSelect(tab);
+          }
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
