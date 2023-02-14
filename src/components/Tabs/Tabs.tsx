33358d8a77a10567bc07768fb808a062f1b8e70d
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-14 10:51:15 +0200

    Finish task

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,49 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const tab = tabs.find((t) => t.id === selectedTabId);
+  const currentTab = tab || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((t) => (
+            <li
+              key={t.id}
+              className={cn({ 'is-active': t.id === currentTab.id })}
+              data-cy="Tab"
+            >
+              <a
+                onClick={() => {
+                  if (t.id !== selectedTabId) {
+                    onTabSelected(t);
+                  }
+                }}
+                href={`#${t.id}`}
+                data-cy="TabLink"
+              >
+                {t.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+};
