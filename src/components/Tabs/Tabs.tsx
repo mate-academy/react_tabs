c10a633228985d899aa5f721e3b7b68648c75649
Aleks <offsset@gmail.com>
2023-02-16 20:48:50 +0200

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import React from 'react';
+import ClassNames from 'classnames';
+
+type Props = {
+  tabs: {
+    id: string;
+    title: string;
+    content: string;
+  }[]
+  selectedTabId: string;
+  getSelectedTabId: (id: string) => void;
+};
+
+export const Tabs: React.FC<Props> = (
+  { tabs, selectedTabId, getSelectedTabId },
+) => {
+  return (
+    <ul>
+      {
+        tabs.map(tab => {
+          const { id, title } = tab;
+          const booleanClass = id === selectedTabId;
+
+          return (
+            <li
+              className={ClassNames({ 'is-active': booleanClass })}
+              data-cy="Tab"
+            >
+              <a
+                href={`'#tab-' + ${id}`}
+                data-cy="TabLink"
+                onClick={() => getSelectedTabId(id)}
+              >
+                {title}
+              </a>
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+};
