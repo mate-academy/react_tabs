b272caa2a6cd6938882a80a54c6989bd3f05d4a3
illia_kots <illyahennadievich@gmail.com>
2023-02-19 18:12:47 +0200

    React_Tabs was done

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        className={classNames(
+          selectedTabId === tab.id && 'is-active',
+        )}
+        data-cy="Tab"
+        key={tab.id}
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={() => onTabSelected(tab)}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
