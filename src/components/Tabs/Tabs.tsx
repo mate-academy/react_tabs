06298804ab8e8ea833f911cb89d4154a221a1ec1
Taras Baran <baranprivat22@gmail.com>
2023-03-01 14:56:12 +0200

    finished

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+import { Tab } from '../../type/Tab';
+
+type Callback = (id: number) => void;
+
+type Props = {
+  tabs: Tab[];
+  selectedTab : number;
+  changeTab: Callback;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab: selectedTabId,
+  changeTab: handleClick,
+}) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }, index) => (
+            <li
+              className={classNames(
+                {
+                  'is-active': index === selectedTabId,
+                },
+              )}
+              data-cy="Tab"
+              key={id}
+            >
+              <a
+                href={`#${id}`}
+                data-cy="TabLink"
+                onClick={() => handleClick(index)}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs[selectedTabId].content}
+      </div>
+    </div>
+  );
+};
