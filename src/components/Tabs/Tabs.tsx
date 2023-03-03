5348dd04044f4143ffaa1b62e6e0f87f96eaf5b8
Anton Chetveryk <chetverykantonma@gmail.com>
2023-03-03 23:44:34 +0200

    done

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tabId: string) => void
+}
+
+export const Tabs = (props: Props) => {
+  const { tabs, selectedTabId, onTabSelected } = props;
+  const content = tabs.find(tab => tab.id === selectedTabId)?.content;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                // eslint-disable-next-line max-len
+                className={classNames({ 'is-active': tab.id === selectedTabId })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => onTabSelected(tab.id)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
