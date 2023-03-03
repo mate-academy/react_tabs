7941b5a706448da196ad6eaad02631e9467033b3
Skuratovskyi Vlad <vladskur0369@gmail.com>
2023-03-03 22:22:54 +0200

    Solution v0.1

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+type TabsType = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+export const Tabs = ({
+  tab,
+  selectId,
+  onTabSelected,
+}: {
+  tab: TabsType;
+  selectId: string;
+  onTabSelected: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const { id, title } = tab;
+
+  return (
+    <li
+      data-cy="Tab"
+      className={classNames(
+        { 'is-active': id === selectId },
+      )}
+    >
+      <a
+        href={`#${id}`}
+        data-cy="TabLink"
+        id={id}
+        onClick={event => onTabSelected(event.currentTarget.id)}
+      >
+        {title}
+      </a>
+    </li>
+  );
+};
