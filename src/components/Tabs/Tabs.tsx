05a4d52bad280dcb0e075377c81aed02148287b3
Nadiia Kotliar <eknakaron@gmail.com>
2023-03-13 17:15:31 +0200

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import { Tab } from '../../Type';
+
+type Props = {
+  tabs: Tab[],
+  hendlerTabs: (tab: Tab) => void,
+  selectedTabId: string,
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, hendlerTabs, selectedTabId }) => (
+  <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={classNames({ 'is-active': tab.id === selectedTabId })}
+          data-cy="Tab"
+          key={tab.id}
+        >
+          <a
+            href="#{tab.id}"
+            data-cy="TabLink"
+            onClick={() => hendlerTabs(tab)}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
