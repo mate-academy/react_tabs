b69195d848f35913643d0ecfce627742f7e6835d
Tetiana Koval <tetiana.koval.bit@gmail.com>
2023-03-14 15:21:35 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,34 +10,42 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App: React.FC = () => {
-  return (
-    <div className="section">
-      <h1 className="title">
-        Selected tab is Tab 1
-      </h1>
-
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Some text 1
-        </div>
-      </div>
-    </div>
-  );
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
 };
+
+type State = {
+  selectedTabId: string,
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    selectedTabId: tabs[0].id,
+  };
+
+  handleSwitch = (tab:Tab) => {
+    this.setState({
+      selectedTabId: tab.id,
+    });
+  };
+
+  render() {
+    const { selectedTabId } = this.state;
+
+    return (
+      <div className="section">
+        <h1 className="title">
+          {`Selected tab is ${tabs[tabs.findIndex(tab => tab.id === selectedTabId)].title}`}
+        </h1>
+
+        <Tabs
+          tabs={tabs}
+          selectedTabId={this.state.selectedTabId}
+          onTabSelected={this.handleSwitch}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,60 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void
+};
+
+export const Tabs:React.FC<Props> = (props) => {
+  const { tabs, selectedTabId, onTabSelected } = props;
+
+  const tabInTabs = tabs.find(tab => tab.id === selectedTabId);
+
+  const rigtTabId = tabInTabs ? tabInTabs.id : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => (
+            <li
+              className={
+                classNames({
+                  'is-active': tab.id === rigtTabId,
+                })
+              }
+              data-cy="Tab"
+              key={tab.title}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (tab.id === selectedTabId) {
+                    return;
+                  }
+
+                  onTabSelected(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find((tab:Tab) => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
