6c463167d657373a20997cfddad6a463487e7a92
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-19 15:21:02 +0200

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import React from "react";
+import { Tab, TabProps } from "./Tab";
+
+export interface ITabDefinition
+  extends Omit<TabProps, "onSelect" | "selected"> {
+  content: React.ReactNode;
+}
+
+interface Props {
+  tabs: ITabDefinition[];
+  selectedTabId: string;
+  onTabSelected: (tab: ITabDefinition) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = React.useMemo<ITabDefinition>(
+    () => tabs.find((tab) => tab.id === selectedTabId) ?? tabs[0],
+    [tabs, selectedTabId]
+  );
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => (
+            <Tab
+              id={tab.id}
+              title={tab.title}
+              onSelect={() => onTabSelected(tab)}
+              selected={tab === selectedTab}
+            />
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
