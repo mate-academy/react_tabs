142294ddc94fe5f209ae4b16c0444bf492bfc2e7
ctk12 <chetankatwa12@gmail.com>
2023-03-27 11:09:59 +0530

    Added React Tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+interface TabType {
+  id: string,
+  title: string,
+  content: string
+}
+
+type Props = {
+  clickTabId: string,
+  tabs: TabType[],
+  onClick: (tabId: string, tabTitle: string) => void,
+};
+
+export const Tabs: React.FC<Props> = ({ clickTabId, tabs, onClick }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={classNames({ 'is-active': clickTabId === tab.id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                onClick={() => onClick(tab.id, tab.title)}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.filter(tab => tab.id === clickTabId)[0].content}
+      </div>
+    </div>
+  );
+};
