90effccbf9f9278f61d2b04868187cb082c33ad3
Sergey Mironenko <serg1prime@gmail.com>
2023-03-29 00:44:16 +0300

    Tabs first commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedId: string,
+  onTabSelected(tab: Tab): void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedId,
+  onTabSelected,
+}) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        className={selectedId === tab.id ? 'is-active' : ''}
+        data-cy="Tab"
+        key={tab.id}
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={() => {
+            if (selectedId !== tab.id) {
+              onTabSelected(tab);
+            }
+          }}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
