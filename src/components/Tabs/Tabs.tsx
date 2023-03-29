cb72e0d2fc776c7a26c8002ab023302a27391293
Denys Dubas <den.dubas.44@gmail.com>
2023-03-29 19:13:48 +0300

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,62 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const currentTabs = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const handleClickOnTab = (isTabCurrent: boolean, tab: Tab) => {
+    if (!isTabCurrent) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+            const isCurrentTab = id === currentTabs.id;
+
+            return (
+              <li
+                className={classNames(
+                  { 'is-active': isCurrentTab },
+                )}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleClickOnTab(isCurrentTab, tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTabs.content}
+      </div>
+    </div>
+  );
+};
