50d7462ca5a2e508ac525278d17a5a09cb8ae724
Anton-Tretiak <tretiak.ap@gmail.com>
2023-03-29 15:50:25 +0300

    all tests passed

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,41 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../Types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected(tab: Tab): void,
+};
+
+export const Tabs: React.FC<Props> = (props) => {
+  const { tabs, onTabSelected, selectedTabId } = props;
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({ 'is-active': selectedTab.id === tab.id })}
+              key={tab.id}
+            >
+              <a
+                data-cy="TabLink"
+                href={`#${tab.id}`}
+                onClick={() => onTabSelected(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
