913d62560bbb3451e755405e2db1da5af24da267
vadym4che <vadym4che@gmail.com>
2023-03-30 04:48:19 +0300

    solution centered on {tab} itself instead of tab.id

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectTab: React.Dispatch<React.SetStateAction<Tab>>;
+  selected: Tab;
+};
+
+export const Tabs: React.FC<Props> = ({ tabs, selectTab, selected }) => {
+  return (
+    <ul>
+      {tabs.map((tab) => (
+        <li
+          className={classNames(
+            { 'is-active': tab.id === selected.id },
+          )}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => selectTab(tab)}
+          >
+            { tab.title }
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
