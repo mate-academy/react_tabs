ec0a9c30026d670204e9720a9948db411c9010f4
diamat1997 <diamat1997@gmail.com>
2023-04-06 12:39:14 +0300

    commit message

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,62 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../types';
+
+type Props = {
+  tabs: Tab[],
+  onTabSelected: (tab: Tab) => void,
+  selectedTabId: string,
+};
+
+export const Tabs: React.FC<Props> = (props) => {
+  const {
+    tabs,
+    onTabSelected,
+    selectedTabId,
+  } = props;
+
+  const currentTab = tabs
+    .find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const updateSelectedTab = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                key={id}
+                className={classNames(
+                  {
+                    'is-active': id === currentTab.id,
+                  },
+                )}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => updateSelectedTab(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+};
