ea182ca6d1e61cb512f315bc063cb2d9f3cfdbe7
Ashton <nonlocality@gmail.com>
2023-04-08 19:00:45 +0300

    Can't resolve these test errors

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../../types/tab';
+
+interface TabsProps {
+  tabs: Tab[];
+  selectedTab: Tab;
+  handleTabClick: (tab: Tab) => void;
+}
+
+export const Tabs: React.FC<TabsProps> = (
+  { tabs, selectedTab, handleTabClick },
+) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => (
+            <li
+              key={tab.id}
+              className={selectedTab.id === tab.id ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a href={`#${tab.id}`} onClick={() => handleTabClick(tab)} data-cy="TabLink">
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
