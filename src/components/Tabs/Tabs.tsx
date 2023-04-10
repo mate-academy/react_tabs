0fac237ab1d6bfead74abdd9af5f0daee58e9e0a
Eugene Pristupa <eugenepristupa@MacBook-Pro-Eugene.local>
2023-04-10 19:28:56 +0300

    react-tabs1

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+import { SyntheticEvent } from 'react';
+import { Tab, TabsProps } from '../../types/tab';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }: TabsProps) => {
+  const onTabClickHandle = (event: SyntheticEvent, tab: Tab) => {
+    event.preventDefault();
+
+    if (!tab?.id) {
+      onTabSelected(tabs[0]);
+
+      return;
+    }
+
+    if (tab.id === selectedTabId.id) {
+      return;
+    }
+
+    onTabSelected(tab);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab: Tab) => (
+            <li
+              className={tab.id === selectedTabId.id ? 'is-active' : ''}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={(event) => onTabClickHandle(event, tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="block"
+        data-cy="TabContent"
+      >
+        {selectedTabId.content}
+      </div>
+    </div>
+  );
+};
