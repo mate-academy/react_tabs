243def755ac4ec6039f51aa9b35c2b5b680566df
Serhii Nikolin <sergeynikolin@gmail.com>
2023-04-17 19:18:22 +0300

    Ready

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,47 @@
-export const Tabs = () => {};
+import React from 'react';
+
+interface TabsType {
+  id: string;
+  title: string;
+  content: string;
+}
+
+type Props = {
+  tabs: TabsType[],
+  selectedTabsId: string,
+  onTabSelected: React.MouseEventHandler<HTMLAnchorElement>,
+  selectedTab: TabsType | undefined,
+};
+
+export const Tabs: React.FC<Props> = React.memo(({
+  tabs,
+  selectedTabsId,
+  onTabSelected,
+  selectedTab,
+}) => (
+  <>
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(({ id, title }) => (
+          <li
+            key={id}
+            className={id === selectedTabsId ? 'is-active' : ''}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${id}`}
+              data-cy="TabLink"
+              onClick={onTabSelected}
+            >
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab?.content}
+    </div>
+  </>
+));
