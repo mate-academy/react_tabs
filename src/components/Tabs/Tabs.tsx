1248f21aa1cc25ea4b20f6a4daae827dcbcfc91a
Daria <hamdsad8@gmail.com>
2023-04-19 09:28:55 +0300

    created a tab func

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,61 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(({ id }: Tab): boolean => id === selectedTabId);
+
+  const isNoTabSelected = tabs.every(
+    ({ id }: Tab): boolean => id !== selectedTabId,
+  );
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab, index) => {
+            const isTabSelected = tab.id === selectedTabId;
+            const isFirstTab = index === 0;
+            const isActiveTab = isTabSelected
+            || (isNoTabSelected && isFirstTab);
+
+            return (
+              <li
+                className={classNames({
+                  'is-active': isActiveTab,
+                })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (!isActiveTab) {
+                      onTabSelected(tab);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content}
+      </div>
+    </div>
+  );
+};
