a23865afd3a0cef7fa3d6a00552ec184f264414e
andbilous <andbilous@gmail.com>
2023-04-22 11:39:59 +0200

    draft

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,43 @@
-export const Tabs = () => {};
+import { Tab } from '../../types';
+
+interface TabsProps {
+  tabs: Tab[],
+  onTabSelected: (id: string) => void,
+  selectedTabId: string
+}
+
+export const Tabs: React.FC<TabsProps>
+  = ({ tabs, onTabSelected, selectedTabId }) => {
+    const getContentById = (id: string) => tabs.find(
+      (tab: Tab) => tab.id === id,
+    )?.content;
+
+    return (
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map((tab: Tab) => (
+              <li
+                className={selectedTabId === tab.id ? 'is-active' : ''}
+                data-cy="Tab"
+              >
+                <a
+                  onClick={() => {
+                    onTabSelected(tab.id);
+                  }}
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {getContentById(selectedTabId)}
+        </div>
+      </div>
+    );
+  };
