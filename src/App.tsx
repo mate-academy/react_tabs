ce3c00fc51c88e6197cae758e168d65db05586bb
andriibilous4 <92803546+andriibilous4@users.noreply.github.com>
2023-04-24 09:59:22 +0200

    Update src/App.tsx
    Co-authored-by: Agniya Ivanishyna <32853034+agniya-i@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,7 @@ export const App: React.FC = () => {
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is
-        {` ${getTabTitleByID(selectedTabId)}`}
+       {`Selected tab is ${selectedTab.title}`}
       </h1>
       <Tabs
         tabs={tabs}
