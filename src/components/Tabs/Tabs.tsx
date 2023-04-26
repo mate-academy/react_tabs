534be18045030db09fe1285255cba3aa60280723
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-04-26 17:46:48 +0300

    react tabs is solved

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import { FC } from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../Tab';
+
+interface Props {
+  tabs: Tab[];
+  setSelectedTab: (tab: Tab) => void,
+  selectedTab: Tab,
+}
+
+export const Tabs: FC<Props> = ({ tabs, setSelectedTab, selectedTab }) => {
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab: Tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={classNames({
+                  'is-active': selectedTab.id === id,
+                })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleClick(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
