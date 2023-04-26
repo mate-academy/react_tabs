39723386d6abdad20460bd008a9fa2c0fdb31fd2
Oleh Kolodii <bigknife4@gmail.com>
2023-04-26 23:41:05 +0300

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,57 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(({ id }) => id === selectedTabId);
+  const isSelectedTabExists = tabs
+    .find(({ id }) => id === selectedTabId) !== undefined;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+            const isCurrentTabSelected = id === selectedTabId;
+            const isCurrentTabFirst = id === tabs[0].id;
+            const activeCondition = (!isSelectedTabExists && isCurrentTabFirst)
+            || (isSelectedTabExists && isCurrentTabSelected);
+
+            return (
+              <li
+                className={classNames({
+                  'is-active': activeCondition,
+                })}
+                data-cy="Tab"
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => isCurrentTabSelected || onTabSelected(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content}
+      </div>
+    </div>
+  );
+};
