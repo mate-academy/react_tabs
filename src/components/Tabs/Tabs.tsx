a08939c8464a18bcf4f22ca0284fb8883079690d
Tolik Petras <toliketras4@gmail.com>
2023-04-26 14:51:25 +0300

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,17 @@
-export const Tabs = () => {};
+import { Tab } from '../../react-app-env';
+
+interface Props {
+  tab: Tab,
+  onTabSelected: (ev: React.MouseEvent<HTMLAnchorElement>) => void,
+}
+
+export const Tabs: React.FC<Props> = ({ tab, onTabSelected }) => (
+  <a
+    href={`#${tab.id}`}
+    data-cy="TabLink"
+    id={tab.id}
+    onClick={onTabSelected}
+  >
+    {tab.title}
+  </a>
+);
