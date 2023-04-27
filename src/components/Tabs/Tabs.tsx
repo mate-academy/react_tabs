191ae73ff71a89b0bcac6f8e16991472959bf0b4
Andrii <remote.makandriival@gmail.com>
2023-04-27 16:02:44 +0300

    fixes

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,50 +1,31 @@
-import classNames from 'classnames';
 import { Tab } from '../../types/Tab';
+import { TabInfo } from '../Tab/TabInfo';
 
 interface Props {
   tabs: Tab[],
   onTabClick: (id: string) => void,
   selectedTab: Tab,
 }
-
 export const Tabs: React.FC<Props> = ({
   tabs,
   onTabClick,
   selectedTab,
-}) => {
-  const handleTabClick = (id: string) => {
-    if (selectedTab.id !== id) {
-      onTabClick(id);
-    }
-  };
-
-  return (
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              className={classNames({ 'is-active': selectedTab.id === tab.id })}
-              data-cy="Tab"
-              key={tab.id}
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => {
-                  handleTabClick(tab.id);
-                }}
-              >
-                {tab.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        {selectedTab.content}
-      </div>
+}) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <TabInfo
+            tab={tab}
+            onTabClick={onTabClick}
+            selectedTab={selectedTab}
+          />
+        ))}
+      </ul>
     </div>
-  );
-};
+
+    <div className="block" data-cy="TabContent">
+      {selectedTab.content}
+    </div>
+  </div>
+);
