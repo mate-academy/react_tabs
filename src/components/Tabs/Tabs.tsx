abadb27176e6a0a323dc092217f6a90fcf94b08b
Goto-Light <vadim.tkachenko.1997@gmail.com>
2023-04-27 17:53:45 +0300

    fixed mistakes about test

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -12,27 +12,45 @@ export const Tabs: React.FC<Props> = ({
   selectedTabId,
   onTabSelected,
 }) => {
-  return (
-    <ul>
-      {tabs.map((tab) => {
-        const { id, title } = tab;
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
 
-        return (
-          <li
-            className={cn({ 'is-active': (selectedTabId === id) })}
-            data-cy="Tab"
-            key={tab.id}
-          >
-            <a
-              href={`#${id}`}
-              data-cy="TabLink"
-              onClick={() => onTabSelected(tab)}
-            >
-              {title}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
+  const handleClick = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+            const isSelectedTab = selectedTab.id === id;
+
+            return (
+              <li
+                data-cy="Tab"
+                className={cn({ 'is-active': isSelectedTab })}
+                key={id}
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleClick(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
   );
 };
