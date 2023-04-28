c6653cd3dc0043be5562742fc625171897df49fb
Anatolii Kosolapov <trogwar4ik4@gmail.com>
2023-04-28 18:03:09 +0300

    create tabs app

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../Types/Tab';
+
+interface Props {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (id: string) => void;
+}
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        const { id, title } = tab;
+
+        return (
+          <li
+            className={classNames({ 'is-active': id === selectedTabId })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                onTabSelected(id);
+              }}
+            >
+              {title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
