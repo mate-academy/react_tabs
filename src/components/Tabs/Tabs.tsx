2ba91e3010ddbb5fc60c84941a71ccb30f412b7a
Hamuud <artemlask2014@gmail.com>
2023-04-30 19:56:20 +0300

    Create tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,60 @@
-export const Tabs = () => {};
+import React, { useState } from 'react';
+import cn from 'classnames';
+import { Tab } from '../../types/Tab';
+import { TabInfo } from './TabInfo';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const [selectedTab, setSelectedTab] = useState(
+    tabs.find(tab => tab.id === selectedTabId) || tabs[0],
+  );
+
+  const { id, content } = selectedTab;
+
+  const handleTabClick = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      setSelectedTab(tab);
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab: Tab) => {
+            const isSelected = tab.id === id;
+
+            return (
+              <li
+                key={tab.id}
+                className={cn({
+                  'is-active': isSelected,
+                })}
+                data-cy="Tab"
+              >
+                <TabInfo
+                  tab={tab}
+                  onTabSelected={handleTabClick}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {content}
+      </div>
+    </div>
+  );
+};
