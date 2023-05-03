f8b63e9be16cce5e9ed7829c6a68e9320fb256a6
Evgen Maximov <e19861025@gmail.com>
2023-05-03 12:03:36 +0300

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { FC } from 'react';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  tabs: Tab[],
+  handleChangeTab: (event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    id: string) => void,
+  selected: string
+};
+
+export const Tabs: FC<Props> = ({ tabs, handleChangeTab, selected }: Props) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          value={tab.id}
+          data-cy="Tab"
+          onClick={(event) => handleChangeTab(event, tab.id)}
+          aria-hidden="true"
+          className={classNames('',
+            { 'is-active': tab.id === selected })}
+        >
+          <a href={tab.id} data-cy="TabLink">
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
