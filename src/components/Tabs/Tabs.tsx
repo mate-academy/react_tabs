50b89014d131d5ed04033e0675fe18965511012d
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-05-11 22:48:13 +0200

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { Tab } from '../../tab';
+
+interface PropsTabs {
+  tabs: Tab[];
+  currentTab: Tab;
+  selectedTab: (id: string) => void
+}
+
+export const Tabs = ({ tabs, selectedTab, currentTab }: PropsTabs) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({ 'is-active': currentTab.id === tab.id })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => selectedTab(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab.content}
+      </div>
+    </div>
+  );
+};
