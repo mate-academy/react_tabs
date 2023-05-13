123129c6c36757d5f5ba1b84726b6b79e377cdc4
Ilia <koleshnik160799@gmail.com>
2023-05-13 20:33:33 +0300

    Done

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,35 @@
-export const Tabs = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+interface Tab {
+  id:string;
+  title:string;
+  content:string;
+}
+
+type TabsProps = {
+  tab:Tab;
+  btn:string;
+  setIsBtn:React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const Tabs: React.FC<TabsProps> = ({ tab, btn, setIsBtn }) => {
+  return (
+    <li
+      data-cy="Tab"
+      className={
+        classNames(
+          { 'is-active': btn === tab.title },
+        )
+      }
+    >
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => setIsBtn(tab.title)}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
