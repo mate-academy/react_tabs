10954c507c80ee72cb8851d83e746607f12b74d0
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-05-16 13:01:14 +0200

    solution v1

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import { Tab } from '../../react-app-env';
+
+interface TabsProps {
+  onTabSelected:(arg0:Tab)=>void,
+  selectedTabId: string,
+  tabs: Tab[],
+}
+
+export const Tabs: React.FC<TabsProps>
+  = ({ onTabSelected, tabs, selectedTabId }) => {
+    const checkedSelectedTabId
+      = tabs.find(tab => tab.id === selectedTabId)
+        ? selectedTabId
+        : tabs[0].id;
+
+    return (
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tabItem => (
+              <li
+                key={tabItem.id}
+                className={`${checkedSelectedTabId === tabItem.id
+                  ? 'is-active'
+                  : ''}`}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tabItem.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (tabItem.id !== checkedSelectedTabId) {
+                      onTabSelected(tabItem);
+                    }
+                  }}
+                >
+                  {tabItem.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {tabs
+            .filter(tabItem => tabItem.id === checkedSelectedTabId)[0].content}
+        </div>
+      </div>
+    );
+  };
