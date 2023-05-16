e97b817647e76ab9f5ec31bfdcebefd05aeecebe
Kocherha Roman <f.your.d.2017@gmail.com>
2023-05-16 19:12:49 +0300

    first commit

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import { Tab } from '../types/Tab';
+
+export const Tabs: React.FunctionComponent<{
+  tabs: Tab[], selectedTab: Tab, onClickTab: (tab : Tab) => void
+}> = ({
+  tabs,
+  selectedTab,
+  onClickTab,
+}) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === selectedTab.id ? 'is-active' : ''}
+              data-cy="Tab"
+
+            >
+              <a
+                href={tab.id}
+                data-cy="TabLink"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onClickTab(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+
+  );
+};
