7a28dd84fba1d9c6abc3c95a6899010129e22598
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-22 12:27:03 +0300

    commit message

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,66 @@
-export const Tabs = () => {};
+import React, { useState } from 'react';
+
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tabs: Tab[]
+};
+
+export const Tabs: React.FC<Props> = ({ tabs }) => {
+  const [selectedTab, setToggle] = useState(tabs[0]);
+
+  const selectedTabId: Tab = tabs.find(
+    tab => selectedTab.id === tab.id,
+  ) || tabs[0];
+
+  const chooseTab = (tab: Tab) => {
+    if (selectedTab.id !== tab.id) {
+      setToggle(tab);
+    }
+  };
+
+  return (
+
+    <>
+      <h1 className="title">
+        Selected tab is
+        {' '}
+        {selectedTab.title}
+      </h1>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map((tab) => (
+              <li
+                key={tab.id}
+                className={selectedTab.id === tab.id ? 'is-active' : ''}
+                data-cy={selectedTab.id === tab.id ? 'tab-content' : 'Tab'}
+              >
+                <a
+                  href={tab.id}
+                  data-cy="TabLink"
+                  onClick={(e) => {
+                    chooseTab(tab);
+                    e.preventDefault();
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+
+            ))}
+          </ul>
+
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {selectedTabId.content}
+        </div>
+      </div>
+    </>
+  );
+};
