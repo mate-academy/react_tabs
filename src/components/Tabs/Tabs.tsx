04f4bbd4eeb23f29ba2af6f02a572858fc02a376
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-05-22 09:16:47 +0300

    Solution-1

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import { Dispatch, SetStateAction } from 'react';
+import cn from 'classnames';
+
+type Tab = {
+  id: string,
+  title: string,
+  content: string,
+};
+
+type Props = {
+  tabs: Tab[],
+  handleContent: Dispatch<SetStateAction<string>>,
+  handleTile: Dispatch<SetStateAction<string>>,
+  selectedTab: string,
+};
+
+export const Tabs = ({
+  tabs,
+  handleContent,
+  handleTile,
+  selectedTab,
+}: Props) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={
+            cn(
+              { 'is-active': selectedTab.trim() === tab.title },
+            )
+          }
+          data-cy="Tab"
+          key={tab.id}
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={() => {
+              handleContent(tab.content);
+              handleTile(` ${tab.title}`);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+
+    </ul>
+  );
+};
