1213419047e573fbd480ed41ff23995c803a8f46
Natali <swetswet1234@gmail.com>
2023-05-22 21:55:04 +0300

    develop

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,46 @@
-export const Tabs = () => {};
+import React from 'react';
+import { Tab } from '../../Tupes/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = (props) => {
+  const {
+    tabs,
+    selectedTabId,
+    onTabSelected,
+  } = props;
+
+  const setSelect = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <>
+      {tabs.map(tab => {
+        return (
+          <li
+            key={tab.id}
+            className={selectedTabId === tab.id
+              ? 'is-active'
+              : ''}
+            data-cy="Tab"
+          >
+            <a
+              onClick={() => setSelect(tab)}
+              href={`#tab-${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </>
+  );
+};
