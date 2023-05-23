28cd230459bfaff42159b5201cf97a2f80c37867
Евгений Петровский <petrovskij@MacBook-Pro-Evgenij.local>
2023-05-23 23:21:08 +0300

    Changes requested

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,13 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './types/Tab';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-
-type Tab = {
-  id: string,
-  title: string,
-  content: string,
-};
 
 export const App: React.FC = () => {
   const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
@@ -23,21 +18,20 @@ export const App: React.FC = () => {
 
   const { title } = currentTab;
 
-  const onTabSelected = (tab: Tab) => {
+  const handleTabSelected = (tab: Tab) => {
     setSelectedTabId(tab.id);
   };
 
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is
-        {` ${title}`}
+        {`Selected tab is ${title}`}
       </h1>
 
       <Tabs
         tabs={tabs}
         selectedTabId={selectedTabId}
-        onTabSelected={onTabSelected}
+        onTabSelected={handleTabSelected}
       />
     </div>
   );
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-
-interface Tab {
-  id: string,
-  title: string,
-  content: string,
-}
+import { Tab } from '../../types/Tab';
 
 type Props = {
   tabs: Tab[],
