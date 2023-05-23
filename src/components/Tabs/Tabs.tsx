9b15765d0b8495aba595a675936fb11967f8bdad
catgode <105614630+catgode@users.noreply.github.com>
2023-05-24 00:42:48 +0200

    create working Tabs component - renders a list of tabs and content of selected tab from a received prop also implement class for the active tab

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+interface TabsProps {
+  tabs: Tab[],
+  selectedTabId: string,
+}
+
+export const Tabs = ({ tabs, selectedTabId }: TabsProps) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                data-cy="Tab"
+                {...selectedTabId === tab.id
+                  ? { className: 'is-active' }
+                  : undefined}
+              >
+                <a
+                  href={`"#${tab.id}"`}
+                  data-cy="TabLink"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {(tabs.find(tab => tab.id === selectedTabId)?.content)}
+      </div>
+    </div>
+  );
+};
