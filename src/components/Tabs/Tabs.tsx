5850ed691c7f567b5bff18ee8b84e46e7385c97f
Kacper Łyczba <suaby1725@gmail.com>
2023-05-24 10:38:22 +0200

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,61 @@
-export const Tabs = () => {};
+import React from 'react';
+
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
+
+interface TabsProps {
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+  tabs: Tab[],
+}
+
+export const Tabs: React.FC<TabsProps> = ({
+  tabs, selectedTabId, onTabSelected,
+}) => {
+  function findTab(tabId: string): Tab {
+    return tabs.find((tab) => tab.id === tabId) || tabs[0];
+  }
+
+  function handleTabClick(clickedTab: string) {
+    if (selectedTabId !== clickedTab) {
+      onTabSelected(findTab(clickedTab));
+    }
+  }
+
+  const selectedTab = findTab(selectedTabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                key={id}
+                className={id === selectedTab.id ? 'is-active' : ''}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleTabClick(tab.id)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
