820b47f2dafa8b78526bf80313ff4829bca634d7
Zykhor Andrii <zuxoran@gmail.com>
2023-06-02 17:21:08 +0300

    task done

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,42 @@
-export const Tabs = () => {};
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[]
+  selectedTabId: string;
+  setSelectedTabId: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs, setSelectedTabId, selectedTabId,
+}) => {
+  const onTabSelected = (tab: Tab) => {
+    if (selectedTabId !== tab.id) {
+      setSelectedTabId(tab);
+    }
+  };
+
+  return (
+    <>
+      {tabs.map((tab) => {
+        return (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={selectedTabId === tab.id
+              ? 'is-active'
+              : ''}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </>
+
+  );
+};
