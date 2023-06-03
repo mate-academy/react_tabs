e964ba73267fb0a7ef950c5f1f08c6dfc16bcc01
EhorSaprykin <astralik1993@gmail.com>
2023-06-03 10:56:20 +0300

    v-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -10,31 +10,43 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState('tab-1');
+  const selectedTab = tabs.find((tab) => tab.id === selectedTabId) || tabs[0];
+
   return (
     <div className="section">
-      <h1 className="title">
-        Selected tab is Tab 1
-      </h1>
+      <h1 className="title">{`Selected tab is ${selectedTab.title}`}</h1>
 
       <div data-cy="TabsComponent">
         <div className="tabs is-boxed">
           <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">Tab 1</a>
-            </li>
+            {tabs.map((tab) => {
+              const isActive = tab.id === selectedTabId;
 
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">Tab 2</a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">Tab 3</a>
-            </li>
+              return (
+                <li
+                  className={isActive ? 'is-active' : ''}
+                  data-cy="Tab"
+                  key={tab.id}
+                >
+                  <a
+                    href={`#${tab.id}`}
+                    data-cy="TabLink"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      setSelectedTabId(tab.id);
+                    }}
+                  >
+                    {tab.title}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
 
         <div className="block" data-cy="TabContent">
-          Some text 1
+          {selectedTab.content}
         </div>
       </div>
     </div>
