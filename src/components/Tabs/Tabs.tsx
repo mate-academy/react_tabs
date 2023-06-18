54d661979358586047b3892f4241cf400e715e11
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-06-19 02:22:34 +0300

    tabs were created

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,53 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type TabsProps = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (selectedTab: Tab) => void,
+};
+
+export const Tabs: React.FC<TabsProps> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  if (!tabs.find(tab => tab.id === selectedTabId)) {
+    // eslint-disable-next-line no-param-reassign
+    selectedTabId = tabs[0].id;
+  }
+
+  return (
+    <>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                className={classNames(
+                  { 'is-active': tab.id === selectedTabId },
+                )}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    return selectedTabId !== tab.id ? onTabSelected(tab) : null;
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {tabs.find(tab => tab.id === selectedTabId)?.content}
+        </div>
+      </div>
+    </>
+  );
+};
