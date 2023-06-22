ec10e28368d22636fc770742933f69f2a3333379
Anton Cherepinets <manickmage@ukr.net>
2023-06-23 01:47:20 +0300

    solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { Tab } from '../../types';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: Tab;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId.id);
+
+  return (
+    <>
+      <ul>
+        {tabs.map((tab) => (
+          <li
+            className={cn({ 'is-active': selectedTabId.id === tab.id })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              href={tab.id}
+              data-cy="TabLink"
+              onClick={(event) => {
+                event.preventDefault();
+                onTabSelected(tab);
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content}
+      </div>
+    </>
+  );
+};
