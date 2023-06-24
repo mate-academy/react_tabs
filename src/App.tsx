2e32a834dc0546e4761d72bbf01ca725b5408c30
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-06-24 08:57:28 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -17,13 +17,8 @@ const tabs: Tab[] = [
 ];
 
 export const App: React.FC = () => {
-  const [selectedTabId, setSelectedTabId] = useState('');
-
-  useEffect(() => {
-    if (!selectedTabId || !tabs.some(tab => tab.id === selectedTabId)) {
-      setSelectedTabId(tabs[0].id);
-    }
-  }, [tabs, selectedTabId]);
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const selectedTabTitle = tabs.find(tab => tab.id === selectedTabId)?.title;
 
   const handleTabSelected = (tab: Tab) => {
     if (selectedTabId !== tab.id) {
@@ -34,9 +29,7 @@ export const App: React.FC = () => {
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is
-        {' '}
-        {tabs.find(tab => tab.id === selectedTabId)?.title}
+        {`Selected tab is ${selectedTabTitle}`}
       </h1>
 
       <Tabs
