734a21f1e8b17cb4a9b09f76a531706fdd4179b8
Alla Gutsul <allagutsul1990@gmail.com>
2023-06-27 00:36:09 +0300

    add solution for the react tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        key={tab.id}
+        className={cn(
+          {
+            'is-active': selectedTabId === tab.id,
+          },
+        )}
+        data-cy="Tab"
+      >
+        <a
+          onClick={() => onTabSelected(tab.id)}
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
