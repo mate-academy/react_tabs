ffd801ae665d5eceb7e935cef8997ee8d433f67b
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-06-29 18:22:16 +0300

    all tests good

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        key={tab.id}
+        className={classNames(
+          {
+            'is-active': selectedTabId === tab.id,
+          },
+        )}
+        data-cy="Tab"
+      >
+        <a
+          onClick={() => onTabSelected(tab.id)}
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
