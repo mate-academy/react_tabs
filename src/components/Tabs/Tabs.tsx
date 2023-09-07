1a7a23fc1de6834dad45fa1bdd3928c366507122
Dolnys <thedolnys@gmail.com>
2023-09-07 12:00:05 +0200

    initial, add state and split App

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+/* eslint-disable */
+import { TabsProps } from "../../types";
+
+export const Tabs: React.FC<TabsProps> = ({ selectedTabId }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          <li className="is-active" data-cy="Tab">
+            <a href="#tab-1" data-cy="TabLink">
+              Tab 1
+            </a>
+          </li>
+
+          <li data-cy="Tab">
+            <a href="#tab-2" data-cy="TabLink">
+              Tab 2
+            </a>
+          </li>
+
+          <li data-cy="Tab">
+            <a href="#tab-3" data-cy="TabLink">
+              Tab 3
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        Some text 1
+      </div>
+    </div>
+  );
+};
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type TabsProps = {
+  selectedTabId: string;
+};
