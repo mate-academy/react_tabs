5c31779340795aba4cbd654a72c477d15334dd20
Hubert Klamann <hubert.klamann1@gmail.com>
2023-09-11 19:14:48 +0200

    task solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,50 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+import { Tab } from '../../types';
+
+type TabsProp = {
+  tabs: Tab[],
+  selectedTabId: string,
+  onTabSelected: (active: Tab) => void,
+};
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }: TabsProp) => {
+  const handleTabClick = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                data-cy="Tab"
+                className={cn({
+                  'is-active': tab.id === selectedTabId
+                  || (tab.id === tabs[0].id
+                  && tabs.every(item => item.id !== selectedTabId)),
+                })}
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleTabClick(tab)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
