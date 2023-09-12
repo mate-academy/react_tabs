db4b526e4d9d4194b545f410d3800378c9f404f8
Szymon Pachucki <szymonpachucki010@gmail.com>
2023-09-12 12:11:31 +0200

    add task solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,44 @@
-export const Tabs = () => {};
+import { Tab } from '../../Types';
+
+type TabsProps = {
+  tabs: Tab[],
+  handleTabSelect: (tab: Tab) => void,
+  selectedTabId: string,
+};
+
+export const Tabs = ({ tabs, handleTabSelect, selectedTabId }:
+TabsProps) => {
+  const {
+    id: activeId,
+    content: activeContent,
+  } = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  return (
+    <>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={tab.id === activeId ? 'is-active' : ''}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="tabLink"
+                  onClick={() => tab.id !== activeId && handleTabSelect(tab)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {activeContent}
+        </div>
+      </div>
+    </>
+  );
+};
