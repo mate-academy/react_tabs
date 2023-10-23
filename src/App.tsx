beaaf30552bb03c2e70328d7f67aea87c2efd878
Alessandra Nitti <alessandrannitti@gmail.com>
2023-10-23 14:24:47 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Tab } from './types/Tab';
 import { Tabs } from './components/Tabs';
 
 export const tabs = [
@@ -12,24 +11,18 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
-
-  const onTabSelected = (tab: Tab) => {
-    setSelectedTabId(tab.id);
-  };
-
-  const isActive = tabs.find(tab => selectedTabId === tab.id);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
   return (
     <div className="section">
       <h1 className="title">
-        {`Selected tab is ${isActive?.title}`}
+        {`Selected tab is ${selectedTab?.title}`}
       </h1>
 
       <Tabs
         tabs={tabs}
-        selectedTabId={selectedTabId}
-        onTabSelected={onTabSelected}
+        selectedTabId={selectedTab.id}
+        onTabSelected={setSelectedTab}
       />
 
     </div>
