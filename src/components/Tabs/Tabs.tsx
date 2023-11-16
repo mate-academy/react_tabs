e00e4938a7b94032b1c71b4b6b3377dc5d18fa40
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-11-16 23:59:22 +0100

    Add task solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,57 @@
-export const Tabs = () => {};
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type TabsProps = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<TabsProps> = (
+  {
+    tabs,
+    selectedTabId,
+    onTabSelected,
+  },
+) => {
+  const isValidTabId = tabs.some(tab => tab.id === selectedTabId);
+  const activeTabId = isValidTabId ? selectedTabId : tabs[0].id;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(item => (
+            <li
+              className={activeTabId === item.id
+                ? 'is-active'
+                : ''}
+              data-cy="Tab"
+              key={item.id}
+            >
+              <a
+                href={`#${item.id}`}
+                data-cy="TabLink"
+                onClick={() => {
+                  if (item.id !== selectedTabId) {
+                    onTabSelected(item);
+                  }
+                }}
+              >
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
