3cc7240ec33c4c62c2e9711378b0a8a37b2a6203
Marcin Ligmann <marcinligmann2@gmail.com>
2023-12-12 13:14:36 +0100

    added tabs component and handleClick in tabs component

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,37 @@
-export const Tabs = () => {};
+import { Tab } from '../../types/tab';
+
+type Props = {
+  tabs: Tab[]
+  selectedTabId: string,
+  onTabSelected: (tab: Tab) => void,
+};
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }: Props) => {
+  console.log(selectedTabId, onTabSelected);
+
+  const handleClick = (tab: Tab) => {
+    onTabSelected(tab);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li className="is-active" data-cy="Tab" key={tab.id}>
+                <a href={`#${tab.id}`} data-cy="TabLink" onClick={() => handleClick(tab)}>
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        Some text 1
+      </div>
+    </div>
+  );
+};
