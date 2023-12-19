cbb2bffc3a6e4a5157a917624e90ba5075b9df30
Kamil <anu88kl@gmail.com>
2023-12-19 14:08:56 +0100

    Solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,64 @@
-export const Tabs = () => {};
+import React from 'react';
+
+const tabsData = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type TabsProps = {
+  tabs: Tab[];
+  selectedTabId: string;
+  setSelectedTabId: (id: string) => void;
+  onTabSelected?: (newTabId: string) => void;
+};
+
+export const Tabs: React.FC<TabsProps>
+= ({ tabs, selectedTabId, setSelectedTabId }) => {
+  const handleTabClick = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      setSelectedTabId(tab.id);
+      if (onTabSelected) {
+        onTabSelected(tab);
+      }
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabsData.map(tab => (
+            <li
+              key={tab.id}
+              className={selectedTabId === tab.id
+                ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleTabClick(tab);
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
