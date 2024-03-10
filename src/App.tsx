3b117500f694efcd713f315036ce10708a3b2fb0
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-03-10 09:07:11 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
+import { Tab } from './components/Tabs/Types/Tab';
 
-export const tabs = [
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const App: React.FC = () => {
+  const [tab, setTab] = useState<Tab>(tabs[0]);
+  const findTab = (ta: Tab) => {
+    const foundTab = tabs.find(t => t.id === ta.id);
+
+    if (foundTab) {
+      setTab(foundTab);
+    }
+  };
+
   return (
-    <div className="section">
-      <h1 className="title">Selected tab is Tab 1</h1>
+    <>
+      <div className="section">
+        <h1 className="title">Selected tab is {tab.title}</h1>
 
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">
-                Tab 1
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">
-                Tab 2
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">
-                Tab 3
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Some text 1
-        </div>
+        <Tabs tabs={tabs} findTab={findTab} selectedTab={tab} />
       </div>
-    </div>
+    </>
   );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,45 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { Tab } from './Types/Tab';
+
+type Propslist = {
+  tabs: Tab[];
+  findTab: (tab: Tab) => void;
+  selectedTab: Tab;
+};
+
+export const Tabs: React.FC<Propslist> = ({ tabs, findTab, selectedTab }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={cn({ 'is-active': selectedTab.id === tab.id })}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (tab.id !== selectedTab.id) {
+                      findTab(tab);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.content}
+      </div>
+    </div>
+  );
+};
