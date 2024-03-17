2dcbbee2ce3304a7d5a9ff6be212e77957da0d02
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-03-17 20:00:54 +0100

    add task solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,49 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }: Props) => {
+  const validTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const handleClick = (tab: Tab) => {
+    if (tab.id !== validTab.id) {
+      onTabSelected(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                className={classNames({ 'is-active': id === validTab.id })}
+                key={id}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleClick(tab)}
+                >
+                  {title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {validTab.content}
+      </div>
+    </div>
+  );
+};
