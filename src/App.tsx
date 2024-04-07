fd4946761107627f6299f97a916a2f838fb0691b
Olaf <olaf.chuszno@gmail.com>
2024-04-07 10:27:25 +0200

    Implement tab switching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
-export const tabs = [
+export type Tab = { id: string; title: string; content: string };
+
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
+const tabIds: Tab['id'][] = tabs.map((tab: Tab) => tab.id);
+
 export const App: React.FC = () => {
+  const [selectedTabId, setSelectedTabId] = useState(tabIds[0]);
+
+  useEffect(() => {
+    if (!tabIds.includes(selectedTabId)) {
+      setSelectedTabId(tabIds[0].toString());
+    }
+  }, [selectedTabId]);
+
+  const onTabSelected = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      setSelectedTabId(tab.id);
+    }
+  };
+
   return (
     <div className="section">
-      <h1 className="title">Selected tab is Tab 1</h1>
+      <h1 className="title">
+        Selected tab is{' '}
+        {tabs.find((tab: Tab) => tab.id === selectedTabId)?.title}
+      </h1>
 
       <div data-cy="TabsComponent">
         <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">
-                Tab 1
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">
-                Tab 2
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">
-                Tab 3
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Some text 1
+          <Tabs
+            tabs={tabs}
+            selectedTabId={selectedTabId}
+            onTabSelected={onTabSelected}
+          />
         </div>
       </div>
     </div>
