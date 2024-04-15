214dde429eb84cfc3d214195ea783b55bac12df7
spencerspe <spencerspe@gmail.com>
2024-04-15 12:32:28 +0100

    used find for content

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 type Tab = {
   id: string;
   title: string;
@@ -8,53 +6,30 @@ type Tab = {
 
 type Props = {
   tabs: Tab[];
+  onChange: (title: string, content: string) => void;
+  activeTab: string;
 };
 
-export const Tabs: React.FC<Props> = ({ tabs }) => {
+export const Tabs: React.FC<Props> = ({ tabs, onChange, activeTab }) => {
 
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0].title);
-  const [content, setContent] = useState(tabs[0].content);
-
-  const onTabSelected = (title: string, theContent: string) => {
-    if (title !== selectedTabId) {
-      setSelectedTabId(title);
-      setContent(theContent);
-    }
-
-    if (!title) {
-      setSelectedTabId(tabs[0].title);
-    }
-  };
 
   return (
-    <div className="section">
-    <h1 className="title">{`Selected tab is ${selectedTabId}`}</h1>
-
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
       <ul>
         {tabs.map((tab) => (
           <li
-            className={tab.title === selectedTabId ? "is-active" : ""}
+            className={tab.title === activeTab ? "is-active" : ""}
             data-cy="Tab"
             key={`#${tab.id}`}
           >
             <a
               data-cy="TabLink"
               href={`#${tab.id}`}
-              onClick={() => onTabSelected(tab.title, tab.content)}
+              onClick={() => onChange(tab.title, tab.content)}
             >
               {tab.title}
             </a>
           </li>
         ))}
-    </ul>
-        </div>
-      </div>
-        <div className="block" data-cy="TabContent" style={{marginTop: "20px"}}>
-            {content}
-        </div>
-      </div>
-
+      </ul>
   );
 };
