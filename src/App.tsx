ae952c7ebfa6621bbea82c64e07b66a5584c831e
spencerspe <spencerspe@gmail.com>
2024-04-15 12:07:23 +0100

    move all code to tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.scss";
 import { Tabs } from "./components/Tabs";
-import { tabs } from "./components/tabsData";
+
+export const tabs = [
+  { id: "tab-1", title: "Tab 1", content: "Some text 1" },
+  { id: "tab-2", title: "Tab 2", content: "Some text 2" },
+  { id: "tab-3", title: "Tab 3", content: "Some text 3" },
+];
 
 export const App: React.FC = () => {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0].title);
-  const [content, setContent] = useState(tabs[0].content);
-
-  const onTabSelected = (title: string, theContent: string) => {
-    if (title !== selectedTabId) {
-      setSelectedTabId(title);
-      setContent(theContent);
-    }
-
-    if (!title) {
-      setSelectedTabId(tabs[0].title);
-    }
-  };
-
   return (
-    <div className="section">
-      <h1 className="title">{`Selected tab is ${selectedTabId}`}</h1>
-
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <Tabs
-            tabs={tabs}
-            onChange={onTabSelected}
-            activeTab={selectedTabId}
-          />
-        </div>
-        <div className="block" data-cy="TabContent">
-          {content}
-        </div>
-      </div>
-    </div>
+          <Tabs tabs={tabs}/>
   );
 };
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 type Tab = {
   id: string;
   title: string;
@@ -6,31 +8,53 @@ type Tab = {
 
 type Props = {
   tabs: Tab[];
-  onChange: (title: string, content: string) => void;
-  activeTab: string;
 };
 
-export const Tabs: React.FC<Props> = ({ tabs, onChange, activeTab }) => {
+export const Tabs: React.FC<Props> = ({ tabs }) => {
 
-  const changeTab = onChange;
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].title);
+  const [content, setContent] = useState(tabs[0].content);
+
+  const onTabSelected = (title: string, theContent: string) => {
+    if (title !== selectedTabId) {
+      setSelectedTabId(title);
+      setContent(theContent);
+    }
+
+    if (!title) {
+      setSelectedTabId(tabs[0].title);
+    }
+  };
 
   return (
+    <div className="section">
+    <h1 className="title">{`Selected tab is ${selectedTabId}`}</h1>
+
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
       <ul>
         {tabs.map((tab) => (
           <li
-            className={tab.title === activeTab ? "is-active" : ""}
+            className={tab.title === selectedTabId ? "is-active" : ""}
             data-cy="Tab"
             key={`#${tab.id}`}
           >
             <a
               data-cy="TabLink"
               href={`#${tab.id}`}
-              onClick={() => changeTab(tab.title, tab.content)}
+              onClick={() => onTabSelected(tab.title, tab.content)}
             >
               {tab.title}
             </a>
           </li>
         ))}
-      </ul>
+    </ul>
+        </div>
+      </div>
+        <div className="block" data-cy="TabContent" style={{marginTop: "20px"}}>
+            {content}
+        </div>
+      </div>
+
   );
 };
