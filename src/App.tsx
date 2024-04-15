7a652ce33b9be73dcd1a988e9dd6094dc60be9d1
spencerspe <spencerspe@gmail.com>
2024-04-15 15:36:28 +0100

    still trying to stop Dependency cycle

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.scss";
 import { Tabs } from "./components/Tabs";
-
-export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { tabs } from "./components/tabsData"
 
 export const App: React.FC = () => {
   const [selectedTabId, setSelectedTabId] = useState(tabs[0].title);
